5c4e3785e3150bd112df3ad25b9f731819d7f534
Adam Lutz <amlutz160@gmail.com>
2020-07-13 11:17:31 -0500

    removing debug

--- tests/conf/netlify-browserstack-plugin/index.js
+++ tests/conf/netlify-browserstack-plugin/index.js
@@ -3,8 +3,6 @@ module.exports = {
     constants,
     utils: { build, status, cache, run, git },
   }) => {
-    console.log(constants.PUBLISH_DIR);
-    console.log(process.env.DEPLOY_URL);
     await run.command("yarn e2e:browserstack");
   },
 };
