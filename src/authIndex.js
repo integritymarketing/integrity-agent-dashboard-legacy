c675fb79f52449c38acaa98714b72e2cb1443319
Adam Lutz <amlutz160@gmail.com>
2020-08-03 17:08:30 -0500

    adding sentry for auth app

--- src/authIndex.js
+++ src/authIndex.js
@@ -13,6 +13,15 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+// error logging disabled for netlify deploy-preview and branch-deploy builds
+// DSN only defined in production apps.  see netlify.toml
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    environment: process.env.REACT_APP_BUILD_ENV || "develop",
+  });
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
