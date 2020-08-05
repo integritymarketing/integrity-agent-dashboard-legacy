c64c03cd3050610f8e870883d3b022a3a6ce50db
Nick Stark <nick.stark@gokartlabs.com>
2020-08-05 15:02:33 -0500

    add scroll to top on route change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import AuthContext from "contexts/auth";
 import authService from "services/auth";
 import AppRoutes from "routes/app";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const App = () => {
   return (
     <AuthContext.Provider value={authService}>
       <Router>
+        <ScrollToTop />
         <div className="content-frame">
           <AppRoutes />
         </div>
--- src/AuthApp.js
+++ src/AuthApp.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
 import AuthAppRoutes from "routes/authApp";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const AuthApp = () => {
   return (
     <Router>
+      <ScrollToTop />
       <div className="content-frame">
         <AuthAppRoutes />
       </div>
