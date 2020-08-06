18ae705ec00790d53dc5ce42260fd17633174b84
Nick Stark <nick.stark@gokartlabs.com>
2020-08-06 08:38:40 -0500

    move routes back into app files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AuthContext from "contexts/auth";
 import authService from "services/auth";
-import AppRoutes from "routes/app";
+import DashboardPage from "pages/DashboardPage";
+import TrainingPage from "pages/TrainingPage";
+import LandingPage from "pages/LandingPage";
+import NotFoundPage from "pages/NotFound";
+import LogoutCallback from "components/auth/logoutCallback";
+import AuthSilentCallback from "components/auth/authSilentCallback";
+import AuthCallback from "components/auth/authCallback";
+import {
+  AuthenticatedRoute,
+  UnauthenticatedRoute,
+} from "components/auth/routes";
 
 const App = () => {
   return (
     <AuthContext.Provider value={authService}>
       <Router>
         <div className="content-frame">
-          <AppRoutes />
+          <Switch>
+            <Route
+              exact={true}
+              path="/signin-oidc-silent"
+              component={AuthSilentCallback}
+            />
+            <Route exact={true} path="/signin-oidc" component={AuthCallback} />
+            <Route
+              exact={true}
+              path="/signout-oidc"
+              component={LogoutCallback}
+            />
+            <UnauthenticatedRoute exact path="/">
+              <LandingPage />
+            </UnauthenticatedRoute>
+            <AuthenticatedRoute exact path="/dashboard">
+              <DashboardPage />
+            </AuthenticatedRoute>
+            <AuthenticatedRoute path="/training">
+              <TrainingPage />
+            </AuthenticatedRoute>
+            <Route path="*">
+              <NotFoundPage />
+            </Route>
+          </Switch>
         </div>
       </Router>
     </AuthContext.Provider>
