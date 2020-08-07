9e85c1ca60d77dea45ca56db0a9290ec2c2d43a4
Nick Stark <nick.stark@gokartlabs.com>
2020-08-07 15:06:24 -0500

    stub out resource page accordion

---
+++ src/components/ui/expandable-content.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import ExpandIcon from "components/icons/expand";
+import CollapseIcon from "components/icons/collapse";
+
+export default ({ header, sections = [], ...props }) => {
+  const [expandedItems, setExpandedItems] = useState([]);
+  const isExpanded = sections.length === expandedItems.length;
+  const renderProps = {
+    isExpanded,
+    toggleAll: () =>
+      setExpandedItems(isExpanded ? [] : sections.map(({ title }) => title)),
+  };
+
+  const toggleItem = (item, show) => {
+    if (show) {
+      setExpandedItems([...expandedItems, item.title]);
+    } else {
+      setExpandedItems(expandedItems.filter((title) => title !== item.title));
+    }
+  };
+  return (
+    <React.Fragment>
+      {header && header(renderProps)}
+      <ul className="mt-2">
+        {sections.map((item, idx) => {
+          const itemVisible = expandedItems.includes(item.title);
+          const Icon = itemVisible ? CollapseIcon : ExpandIcon;
+          const collapsedClasses =
+            idx === sections.length - 1
+              ? ""
+              : "mb-scale-2 pb-scale-2 border-bottom";
+
+          return (
+            <li
+              key={item.title}
+              className={itemVisible ? null : collapsedClasses}
+            >
+              <div className="toolbar">
+                <span className="hdg hdg--3">
+                  <span className="mr-1">{item.title}</span>{" "}
+                  <span className="text-bold">{item.numItems}</span>
+                </span>
+                <div className="toolbar__right">
+                  <button
+                    type="button"
+                    className="icon-btn"
+                    onClick={() => toggleItem(item, !itemVisible)}
+                  >
+                    <Icon />
+                  </button>
+                </div>
+              </div>
+              {itemVisible && item.renderItems(renderProps)}
+            </li>
+          );
+        })}
+      </ul>
+    </React.Fragment>
+  );
+};
--- src/pages/ResourcesPage.js
+++ src/pages/ResourcesPage.js
@@ -1,16 +1,152 @@
 import React from "react";
 import Container from "components/ui/container";
+import Card from "components/ui/card";
+import LineItem from "components/ui/line-item";
 import GlobalNav from "partials/global-nav";
 import GlobalFooter from "partials/global-footer";
+import ExpandableContent from "components/ui/expandable-content";
+import ComputerIcon from "components/icons/computer";
+import DownloadIcon from "components/icons/download";
 
 export default () => {
   return (
     <React.Fragment>
       <div className="bg-high-contrast">
         <GlobalNav />
+        <Container className="scaling-header">
+          <div className="hdg hdg--2">
+            Resources to keep your sales game strong.
+          </div>
+
+          <div className="hdg hdg--3 mt-1">
+            Check out your educational resources below.
+          </div>
+        </Container>
       </div>
       <Container className="mt-scale-3">
-        <div className="hdg hdg--3">Training</div>
+        <section>
+          <div className="hdg hdg--3">Recommended Reads</div>
+          <div className="card-grid mt-4">
+            <Card>
+              <div className="card__title">
+                <ComputerIcon className="card__icon" />
+                <span>Tips for Working Remotely</span>
+              </div>
+              <div className="card__body">
+                <p className="text-body">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor,
+                </p>
+              </div>
+              <div className="card__actions">
+                <button className="btn">Download</button>
+              </div>
+            </Card>
+            <Card>
+              <div className="card__title">
+                <ComputerIcon className="card__icon" />
+                <span>Tips for Managing Remote Workers</span>
+              </div>
+              <div className="card__body">
+                <p className="text-body">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor,
+                </p>
+              </div>
+              <div className="card__actions">
+                <button className="btn">Download</button>
+              </div>
+            </Card>
+            <Card>
+              <div className="card__title">
+                <ComputerIcon className="card__icon" />
+                <span>Tips for Managing Stress</span>
+              </div>
+              <div className="card__body">
+                <p className="text-body">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor,
+                </p>
+              </div>
+              <div className="card__actions">
+                <button className="btn">Download</button>
+              </div>
+            </Card>
+          </div>
+        </section>
+        <section className="mt-scale-4">
+          <ExpandableContent
+            header={({ isExpanded, toggleAll }) => (
+              <div className="toolbar mb-4">
+                <div className="hdg hdg--3">All Resources</div>
+                <div className="toolbar__right text-body sf-hide">
+                  <button className="link" onClick={toggleAll}>
+                    {isExpanded ? "Collapse All" : "Expand All"}
+                  </button>
+                </div>
+              </div>
+            )}
+            sections={[
+              {
+                title: "Working in a Remote World",
+                numItems: 2,
+                renderItems: () => (
+                  <ul className="divided-vlist mt-2 mb-5">
+                    <li>
+                      <LineItem
+                        href="#external"
+                        icon={<ComputerIcon />}
+                        actionIcon={<DownloadIcon />}
+                      >
+                        <div className="text-body text-bold mb-1">
+                          Tips for Working Remotely
+                        </div>
+                        <div className="text-body">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do eiusmod tempor,
+                        </div>
+                      </LineItem>
+                    </li>
+                    <LineItem
+                      href="#external"
+                      icon={<ComputerIcon />}
+                      actionIcon={<DownloadIcon />}
+                    >
+                      <div className="text-body text-bold mb-1">
+                        Tips for Working Remotely
+                      </div>
+                      <div className="text-body">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor,
+                      </div>
+                    </LineItem>
+                  </ul>
+                ),
+              },
+              {
+                title: "Sales Tips and Tricks",
+                numItems: 1,
+                renderItems: () => (
+                  <ul className="divided-vlist mt-2 mb-5">
+                    <LineItem
+                      href="#external"
+                      icon={<ComputerIcon />}
+                      actionIcon={<DownloadIcon />}
+                    >
+                      <div className="text-body text-bold mb-1">
+                        Tips for Working Remotely
+                      </div>
+                      <div className="text-body">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor,
+                      </div>
+                    </LineItem>
+                  </ul>
+                ),
+              },
+            ]}
+          />
+        </section>
       </Container>
       <GlobalFooter />
     </React.Fragment>
