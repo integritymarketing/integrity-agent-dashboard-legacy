f28e5c024a7566347dde5c15855478f59ae238d7
Adam Lutz <amlutz160@gmail.com>
2020-08-20 17:05:26 -0500

    adding password update integration

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -170,7 +170,7 @@ export default () => {
             <Formik
               initialValues={{
                 currentPassword: "",
-                password: "",
+                newPassword: "",
                 confirmPassword: "",
               }}
               validate={(values) => {
@@ -182,24 +182,59 @@ export default () => {
                       args: ["Current Password"],
                     },
                     {
-                      name: "password",
+                      name: "newPassword",
                       validator: validationService.validatePasswordCreation,
                       args: ["New Password"],
                     },
                     {
                       name: "confirmPassword",
                       validator: validationService.validateFieldMatch(
-                        values.password
+                        values.newPassword
                       ),
                     },
                   ],
                   values
                 );
               }}
-              onSubmit={async (values, { setSubmitting }) => {
+              onSubmit={async (values, { setErrors, setSubmitting }) => {
                 setSubmitting(true);
-                // TODO: hook up form submit
-                showMessage("Your password has been updated.");
+                loading.begin(0);
+
+                let user = await AuthService.getUser();
+                const response = await fetch(
+                  process.env.REACT_APP_AUTH_AUTHORITY_URL +
+                    "/api/account/updatepassword",
+                  {
+                    method: "PUT",
+                    headers: {
+                      Authorization: "Bearer " + user.access_token,
+                      "Content-Type": "application/json",
+                    },
+                    credentials: "include",
+                    body: JSON.stringify(values),
+                  }
+                );
+
+                if (response.status >= 200 && response.status < 300) {
+                  setSubmitting(false);
+                  loading.end();
+
+                  showMessage("Your password has been successfully updated.", {
+                    type: "success",
+                  });
+                } else {
+                  loading.end();
+                  if (response.status === 401) {
+                    // TODO handle expired token?
+                  } else {
+                    const errorsArr = await response.json();
+                    setErrors(
+                      validationService.formikErrorsFor(
+                        validationService.standardizeValidationKeys(errorsArr)
+                      )
+                    );
+                  }
+                }
               }}
             >
               {({
@@ -231,12 +266,12 @@ export default () => {
                       type="password"
                       label="Create New Password"
                       placeholder="Create a new password"
-                      name="password"
-                      value={values.password}
+                      name="newPassword"
+                      value={values.newPassword}
                       onChange={handleChange}
                       onBlur={handleBlur}
-                      error={touched.password && errors.password}
-                      success={touched.password && !errors.password}
+                      error={touched.newPassword && errors.newPassword}
+                      success={touched.newPassword && !errors.newPassword}
                       focusBanner={
                         <div className="form-tip">
                           <p>Your password must: </p>
