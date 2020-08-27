a68c840b8a779f3ce046aa523e02f8994d82f993
Nick Stark <nick.stark@gokartlabs.com>
2020-08-27 15:05:01 -0500

    remove bg from download icon

--- src/components/icons/download.js
+++ src/components/icons/download.js
@@ -4,8 +4,6 @@ export default (props) => {
   return (
     <svg width="14" height="17" xmlns="http://www.w3.org/2000/svg" {...props}>
       <g fill="none" fillRule="evenodd">
-        <path fill="#E8EAEF" d="M-1293-651H147v3689h-1440z" />
-        <path d="M-5-3h24v24H-5z" />
         <path d="M0 17h14v-2H0m14-9h-4V0H4v6H0l7 7 7-7z" fill="#236C9E" />
       </g>
     </svg>
