b73ef2e46c9f5d73fef07db14e905876e3b0252f
Nick Stark <nick.stark@gokartlabs.com>
2020-09-02 14:11:47 -0500

    clean up instances of router

---
+++ src/components/functional/router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import TrackPageviews from "components/functional/track-pageviews";
+import ScrollToTop from "components/functional/scroll-to-top";
+import { FlashProvider } from "contexts/flash";
+import FlashMessage from "partials/flash-message";
+
+export default ({ children, ...props }) => (
+  <Router {...props}>
+    <TrackPageviews />
+    <ScrollToTop />
+    <FlashProvider>
+      <FlashMessage />
+      {children}
+    </FlashProvider>
+  </Router>
+);
