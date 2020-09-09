33d40dc8f273ab3e950f480b2a991ac4a9a2ffad
Adam Lutz <amlutz160@gmail.com>
2020-09-09 16:51:32 -0500

    adding login button to stubbed welcome page

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+import Container from "components/ui/container";
+import GlobalNav from "partials/global-nav";
+import AuthContext from "contexts/auth";
+
+const LoginLink = (props) => {
+  const auth = useContext(AuthContext);
+
+  const login = () => {
+    auth.signinRedirect();
+  };
+
+  return (
+    <React.Fragment>
+      <button onClick={login} {...props}></button>
+    </React.Fragment>
+  );
+};
 
 export default () => {
   return (
-    <div>
-      <h1>Welcome page</h1>
-    </div>
+    <React.Fragment>
+      <div className="bg-photo text-invert">
+        <GlobalNav />
+        <Container className="scaling-header">
+          <div className="hdg hdg--2">Welcome</div>
+        </Container>
+      </div>
+      <Container className="mt-scale-3">
+        <LoginLink className="btn">Login</LoginLink>
+      </Container>
+    </React.Fragment>
   );
 };
