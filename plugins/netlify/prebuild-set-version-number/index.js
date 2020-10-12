250d5d3b18d81015473e3a297eaf02d7eb758cc8
Adam Lutz <amlutz160@gmail.com>
2020-10-12 11:27:02 -0500

    adding logging output for netlify build

--- plugins/netlify/prebuild-set-version-number/index.js
+++ plugins/netlify/prebuild-set-version-number/index.js
@@ -3,5 +3,8 @@ const { version } = require("../../../package.json");
 module.exports = {
   onPreBuild: () => {
     process.env["REACT_APP_VERSION"] = version;
+    console.log(
+      `Setting REACT_APP_VERSION to ${process.env.REACT_APP_VERSION}`
+    );
   },
 };
