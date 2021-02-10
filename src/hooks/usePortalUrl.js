80bc338770b5c6608b4173a458c846a6a25d7027
Adam Lutz <amlutz160@gmail.com>
2021-02-10 07:07:25 -0600

    default to env var if exists

--- src/hooks/usePortalUrl.js
+++ src/hooks/usePortalUrl.js
@@ -2,5 +2,5 @@ import Cookies from "universal-cookie";
 
 export default () => {
   const cookies = new Cookies();
-  return cookies.get("portal_url") || process.env.REACT_APP_PORTAL_URL;
+  return process.env.REACT_APP_PORTAL_URL || cookies.get("portal_url");
 };
