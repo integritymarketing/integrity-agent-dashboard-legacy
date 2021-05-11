3ebb0f81fe499a9b79ff0cc1b5839136abe53274
vishal marneni <vishal.marneni@integritymarketing.com>
2021-05-11 12:03:59 -0500

    formatted the phone number as per the design

--- src/utils/phones.js
+++ src/utils/phones.js
@@ -4,7 +4,7 @@ export const formatPhoneNumber = (phoneNumberString) => {
   const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
 
   if (match) {
-    return `(${match[1]})-${match[2]}-${match[3]}`;
+    return `(${match[1]}) ${match[2]}-${match[3]}`;
   }
 
   if (cleaned === "") {
