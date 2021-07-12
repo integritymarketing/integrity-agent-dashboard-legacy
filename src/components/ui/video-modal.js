0ec4ec99613fa020149c279c1cbb502c87769739
Adam Lutz <adamlutz@users.noreply.github.com>
2021-07-12 10:28:28 -0500

    Basic video modal and video line item components [4526] (#419)
    * wip stash
    
    * working video modal components + samples

---
+++ src/components/ui/video-modal.js
@@ -0,0 +1,37 @@
+import Modal from "components/ui/modal";
+import React from "react";
+
+export default ({
+  modalOpen,
+  handleCloseModal,
+  videoUrl,
+  testId,
+  ...props
+}) => {
+  return (
+    <React.Fragment>
+      <div {...props}></div>
+      {modalOpen && (
+        <Modal
+          open={true}
+          wide
+          onClose={() => handleCloseModal()}
+          labeledById="dialog_video_label"
+          descById="dialog_video_desc"
+          testId={testId}
+          isVideo={true}
+        >
+          <iframe
+            title="vimeo-player"
+            src={`${videoUrl}?autoplay=1`}
+            width="100%"
+            height="320"
+            frameBorder="0"
+            allow="autoplay"
+            allowFullScreen
+          />
+        </Modal>
+      )}
+    </React.Fragment>
+  );
+};
