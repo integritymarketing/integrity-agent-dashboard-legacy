e08085a3d342b30c011b7e52a71e10a53f4abb39
Adam Lutz <adamlutz@users.noreply.github.com>
2021-07-16 12:00:10 -0500

    Add global styles to storybook  (#424)
    * adding global _theme.scss to storybook config
    
    * adding typography

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { addDecorator } from "@storybook/react";
 import { MemoryRouter } from "react-router";
+import "!style-loader!css-loader!sass-loader!../src/scss/_theme.scss";
+import "!style-loader!css-loader!sass-loader!../src/scss/_typography.scss";
 
-addDecorator(story => <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>)
+addDecorator((story) => (
+  <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+));
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
