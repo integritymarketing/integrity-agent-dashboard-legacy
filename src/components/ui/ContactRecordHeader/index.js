1e36b8d975914a913ae10795525a76be19632e1b
Mike Bradford <48229313+mbradford-verys@users.noreply.github.com>
2021-08-06 08:25:38 -0700

    feature: Plans page for quoting | Contact Record Header (#438)
    * create contact record header
    
    * add ContactRecordHeader story
    
    * Pharmacy vs Pharmacies
    
    * formatting
    
    * fix import

---
+++ src/components/ui/ContactRecordHeader/index.js
@@ -0,0 +1,86 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import PropTypes from "prop-types";
+import "./index.scss";
+import { Button } from "components/ui/Button";
+import EditDetails from "components/icons/edit-details";
+import ArrowDown from "components/icons/arrow-down";
+
+export default function ContactRecordHeader({
+  contact,
+  providersCount,
+  prescriptionsCount,
+  pharmacyCount,
+  isMobile,
+}) {
+  const history = useHistory();
+  const fullName = `${contact.firstName} ${contact.middleName} ${contact.lastName}`;
+  const zip = contact.addresses[0].postalCode;
+  return (
+    <div className="contactRecordHeader">
+      <div className="back">
+        {!isMobile && (
+          <Button
+            icon={<ArrowDown />}
+            label="Back to Contact Record"
+            onClick={() => history.push(`/contact/${contact.leadsId}`)}
+            type="tertiary"
+          />
+        )}
+      </div>
+      <div className="details">
+        <h4 className="name">{fullName}</h4>
+        <div className="info">
+          <Link
+            to={{
+              pathname: `/contact/${contact.leadsId}`,
+              state: { display: "Details" },
+            }}
+          >
+            ZIP Code: {zip}
+          </Link>
+          <Link
+            to={{
+              pathname: `/contact/${contact.leadsId}`,
+              state: { display: "Providers" },
+            }}
+          >
+            Providers ({providersCount})
+          </Link>
+          <Link
+            to={{
+              pathname: `/contact/${contact.leadsId}`,
+              state: { display: "Prescriptions" },
+            }}
+          >
+            Prescriptions ({prescriptionsCount})
+          </Link>
+          <Link
+            to={{
+              pathname: `/contact/${contact.leadsId}`,
+              state: { display: "Pharmacies" },
+            }}
+          >
+            {pharmacyCount > 1 ? "Pharmacies" : "Pharmacy"} ({pharmacyCount})
+          </Link>
+        </div>
+      </div>
+      <div className="edit">
+        <Button
+          icon={<EditDetails />}
+          label="Edit Contact"
+          onClick={() => history.push(`/contact/${contact.leadsId}`)}
+          type="secondary"
+          iconOnly={isMobile}
+        />
+      </div>
+    </div>
+  );
+}
+
+ContactRecordHeader.propTypes = {
+  contact: PropTypes.object.isRequired,
+  providersCount: PropTypes.number.isRequired,
+  prescriptionsCount: PropTypes.number.isRequired,
+  pharmacyCount: PropTypes.number.isRequired,
+};
