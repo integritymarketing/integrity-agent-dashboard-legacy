823ca0f7a8980f239ee50dee6e732efe2a5366ac
vishal marneni <vishal.marneni@integritymarketing.com>
2021-09-29 15:02:21 -0500

    fixed pull request comments

--- src/pages/contacts/contactRecordInfo/modals/AddPrescription.js
+++ src/pages/contacts/contactRecordInfo/modals/AddPrescription.js
@@ -79,9 +79,7 @@ export default function AddPrescription({
     if (dosage) {
       const { commonUserQuantity, commonDaysOfSupply } = dosage;
       const packageOptions = (dosage?.packages || []).map((_package) => ({
-        label: `${_package?.packageSize} ${
-          _package?.PackageSizeUnitOfMeasure
-        } 'm'  ${_package?.packageDescription ?? ""}`,
+        label: `${_package?.packageSize}${_package?.PackageSizeUnitOfMeasure} ${_package?.packageDescription}`,
         value: _package,
       }));
 
