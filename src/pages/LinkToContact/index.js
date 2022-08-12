7ce01f3368e85232e292e6be024ae577b77e26aa
Vishal Marneni <vishal.marneni@integritymarketing.com>
2022-08-12 00:55:08 -0500

    Added caller id & tags on link to contact page

--- src/pages/LinkToContact/index.js
+++ src/pages/LinkToContact/index.js
@@ -1,23 +1,43 @@
+import * as Sentry from "@sentry/react";
+import AuthContext from "contexts/auth";
 import ContactSearch from "./ContactSearch";
 import DashboardHeaderSection from "pages/dashbaord/DashboardHeaderSection";
 import DescriptionIcon from "@mui/icons-material/Description";
 import Footer from "partials/global-footer";
 import GlobalNav from "partials/global-nav-v2";
 import Heading3 from "packages/Heading3";
-import React, { useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Tags from "packages/Tags/Tags";
 import clientService from "services/clientsService";
 import styles from "./styles.module.scss";
+import { CallScriptModal } from "packages/CallScriptModal";
 import { Helmet } from "react-helmet-async";
 import { TextButton } from "packages/Button";
 import { Typography } from "@mui/material";
+import { formatPhoneNumber } from "utils/phones";
 import { useHistory } from "react-router-dom";
-import { CallScriptModal } from "packages/CallScriptModal";
 
 export default function LinkToContact() {
   const history = useHistory();
+  const auth = useContext(AuthContext);
   const [modalOpen, setModalOpen] = useState(false);
   const [contacts, setContacts] = useState([]);
+  const [agentInformation, setAgentInformation] = useState({});
+
+  useEffect(() => {
+    const loadAsyncData = async () => {
+      try {
+        const user = await auth.getUser();
+        const agentId = user?.profile?.agentid;
+        const agentInfo = await clientService.getAgentAvailability(agentId);
+        setAgentInformation(agentInfo);
+      } catch (error) {
+        Sentry.captureException(error);
+      }
+    };
+
+    loadAsyncData();
+  }, [auth]);
 
   const getContacts = async (searchStr) => {
     const response = await clientService.getList(
@@ -34,7 +54,7 @@ export default function LinkToContact() {
   const bannerContent = () => {
     return (
       <>
-          <Heading3 text="Link to contact" />
+        <Heading3 text="Link to contact" />
         <div>
           <TextButton
             variant={"outlined"}
@@ -53,6 +73,8 @@ export default function LinkToContact() {
     history.push("/contact/add-new");
   };
 
+  const tags = agentInformation?.tags?.map((tag) => tag);
+
   return (
     <>
       <Helmet>
@@ -64,19 +86,25 @@ export default function LinkToContact() {
         justifyContent={"space-between"}
         padding={"0 15%"}
       />
-
       <div className={styles.outerContainer}>
         <div className={styles.innerContainer}>
           <div className={styles.medContent}>
             <Typography sx={{ mx: 1 }} variant={"h5"}>
-              Caller ID{" "}
+              {formatPhoneNumber(
+                agentInformation?.agentVirtualPhoneNumber,
+                true
+              )}
             </Typography>
           </div>
           <div className={styles.medContent}>
-            <Tags words={["CALL LEAD", "MAPD"]} flexDirection={"column"} />
+            <Tags words={tags} flexDirection={"column"} />
           </div>
           <div className={styles.medContent}>
-            <TextButton onClick={goToAddNewContactsPage}  variant={"outlined"} size={"large"}>
+            <TextButton
+              onClick={goToAddNewContactsPage}
+              variant={"outlined"}
+              size={"large"}
+            >
               Create new contact
             </TextButton>
           </div>
@@ -86,8 +114,13 @@ export default function LinkToContact() {
         </div>
       </div>
 
-    <CallScriptModal modalOpen={modalOpen} handleClose={()=>{setModalOpen(false)}}/>
-  <Footer />
+      <CallScriptModal
+        modalOpen={modalOpen}
+        handleClose={() => {
+          setModalOpen(false);
+        }}
+      />
+      <Footer />
     </>
   );
 }
