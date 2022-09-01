96467d7abe34ac005f10160e0ab8857b2508c755
RajM1249 <madhasuraja@gmail.com>
2022-09-01 20:45:59 +0530

    Added tags functionality  (#856)
    * Release 2.1.5 (#725)
    
    * updated the release version number (#697)
    
    Co-authored-by: Raja Madasu <raja.madasu@integritymarketing.com>
    
    * fixed showing correct SOA method and commented out showing Forgot NPN… (#700)
    
    * fixed showing correct SOA method and commented out showing Forgot NPN number
    
    * fixed Forgot NPN link issue
    
    Co-authored-by: Raja Madasu <raja.madasu@integritymarketing.com>
    
    * Remove 2022 from MedicareAPP on main page (#702)
    
    Co-authored-by: Raja Madasu <raja.madasu@integritymarketing.com>
    
    * Auth app/changes (#703)
    
    * auth-app test for external clients@1
    
    * auth-app test for external clients@2
    
    * auth-app test for external clients@3
    
    * auth-app test for external clients@3
    
    * fixed build failure
    
    * fixed build issue
    
    * fixed build failure
    
    * auth-app test for external clients@5
    
    * refactored the code
    
    * adding auth changes
    
    Co-authored-by: Raja Madasu <raja.madasu@integritymarketing.com>
    
    * AB#7767: changed document name as link name in share plans modal
    
    * Clicking Duplicate Contacts link doesn't show only duplicates
    
    * AB#7794: added descriptions and new item for carrier guides in learning center (#709)
    
    Co-authored-by: Vinodh Khurra <Vinodh.Khurra@integritymarketing.com>
    
    * AB#7794: updated title as per doc (#711)
    
    Co-authored-by: Vinodh Khurra <Vinodh.Khurra@integritymarketing.com>
    
    * AB#7854: auto completing state is completed (#712)
    
    Co-authored-by: Vinodh Khurra <Vinodh.Khurra@integritymarketing.com>
    
    * Update Delete Provider with new Enhancement to include Provider addressID (#710)
    
    * Adding NotificationService_Full AgentService_Full scope in the OIDC configuration
    
    * Sending plan toast message error
    
    * removed unused import
    
    * AB#7689: hiding address details while getting empty array (#713)
    
    Co-authored-by: Vinodh Khurra <Vinodh.Khurra@integritymarketing.com>
    
    * AB#7767: bug fix in documents name issue
    
    * AB#7916: auto populating state based on zip code (#718)
    
    * AB#8062: Changing radius when adding providers is not sticking (#719)
    
    * AB#8059: Lost Reason Modal Drop Down selection non functional
    
    * AB#8061: Going to edit user account doesn't have the correct colors until hoverover (#720)
    
    * Fixed 8067 and 8068
    
    * AB#8069: On MOBILE and DESKTOP Devices - Navigate to duplicate contact screen triggers the Agent Must add Phone number banner even if agent has a phone number (#722)
    
    * Fixed QA findouts (#724)
    
    Co-authored-by: Raja Madasu <raja.madasu@integritymarketing.com>
    
    Co-authored-by: Raja Madasu <raja.madasu@integritymarketing.com>
    Co-authored-by: Vinodh Khurra <Vinodh.Khurra@integritymarketing.com>
    Co-authored-by: vishalmarneni <79602980+vishalmarneni@users.noreply.github.com>
    Co-authored-by: vinodkumar <39404479+vinodkurra@users.noreply.github.com>
    Co-authored-by: Zaiba Ali <zaiba.ali@integritymarketing.com>
    Co-authored-by: ChristyChen0106 <100621243+ChristyChen0106@users.noreply.github.com>
    
    * Added tag functionality
    
    Co-authored-by: Raja Madasu <raja.madasu@integritymarketing.com>
    Co-authored-by: Vinodh Khurra <Vinodh.Khurra@integritymarketing.com>
    Co-authored-by: vishalmarneni <79602980+vishalmarneni@users.noreply.github.com>
    Co-authored-by: vinodkumar <39404479+vinodkurra@users.noreply.github.com>
    Co-authored-by: Zaiba Ali <zaiba.ali@integritymarketing.com>
    Co-authored-by: ChristyChen0106 <100621243+ChristyChen0106@users.noreply.github.com>
    Co-authored-by: Raja Madasu <rmadasu@SSC-5LZD593.ad.integritymarketing.com>

--- src/pages/contacts/contactRecordInfo/PersonalInfo.js
+++ src/pages/contacts/contactRecordInfo/PersonalInfo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import Container from "components/ui/container";
 import StageSelect from "./StageSelect";
 import { formatPhoneNumber } from "utils/phones";
@@ -7,12 +7,188 @@ import styles from "../ContactsPage.module.scss";
 import { formatAddress, getMapUrl } from "utils/address";
 import Editicon from "components/icons/edit-details";
 import CallScript from "components/icons/callScript";
+import TagIcon from "components/icons/tag-icon";
 import { CallScriptModal } from "packages/CallScriptModal";
 import PrimaryContactPhone from "pages/contacts/PrimaryContactPhone";
+import { Popover as TinyPopover } from "react-tiny-popover";
+import clientsService from "services/clientsService";
+import * as Sentry from "@sentry/react";
+import Check from "components/icons/check-blue";
+import { Button } from "components/ui/Button";
+import useToast from "hooks/useToast";
+import Close from "components/icons/close";
 
 const NOT_AVAILABLE = "N/A";
 
-export default ({ personalInfo, isEdit, setEdit, setDisplay, leadsId }) => {
+function TagsIcon({ leadsId, leadTags, onUpdateTags }) {
+  const [tagModalOpen, setTagModalOpen] = useState(false);
+  const [tagsByCategory, setTagsByCategory] = useState([]);
+  const initialState = leadTags?.map((st) => st.tag.tagId) || [];
+  const [selectedTagsIds, setSelectedTagsIds] = useState(initialState);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [expandedList, setExpandedList] = useState({});
+  const addToast = useToast();
+  function toggleTagSelection(e, tagId) {
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+    setSelectedTagsIds((selTagIds) => {
+      return selTagIds.includes(tagId)
+        ? selTagIds.filter((id) => id !== tagId)
+        : [...selTagIds, tagId];
+    });
+  }
+  function handleSaveTags() {
+    setIsProcessing(true);
+    clientsService
+      .updateLeadsTags(leadsId, selectedTagsIds)
+      .then((data) => {
+        setTagModalOpen(false);
+        setIsProcessing(false);
+        onUpdateTags();
+        addToast({
+          type: "success",
+          message: "Successfully Updated the Tags",
+        });
+      })
+      .catch((error) => {
+        setIsProcessing(false);
+        Sentry.captureException(error);
+        addToast({
+          type: "error",
+          message: "Error Updating the Tags",
+        });
+      });
+  }
+  function handleResetTags() {
+    setSelectedTagsIds([]);
+  }
+  useEffect(() => {
+    setExpandedList(
+      tagsByCategory.reduce((acc, cat) => {
+        acc[cat.tagCategoryId] = true;
+        return acc;
+      }, {})
+    );
+  }, [tagsByCategory]);
+  useEffect(() => {
+    clientsService
+      .getTagsGroupByCategory()
+      .then((data) => {
+        setTagsByCategory(data);
+      })
+      .catch((error) => {
+        Sentry.captureException(error);
+      });
+  }, []);
+  const totalTags = useMemo(() => {
+    return tagsByCategory?.reduce(
+      (acc, item) => acc + item.tags?.length || 0,
+      0
+    );
+  }, [tagsByCategory]);
+  function handleClose() {
+    if (isProcessing) return;
+    setSelectedTagsIds(initialState);
+    setTagModalOpen(false);
+  }
+  function handleToggleExpand(catId) {
+    setExpandedList((expList) => ({ ...expList, [catId]: !expList[catId] }));
+  }
+  return (
+    <TinyPopover
+      onClickOutside={handleClose}
+      isOpen={tagModalOpen}
+      positions={["bottom", "top"]}
+      content={
+        <div className={styles.tagsModal}>
+          <div
+            onClick={handleClose}
+            style={{ display: "flex", justifyContent: "flex-end" }}
+          >
+            <Close />
+          </div>
+          <div className={styles.tagsModalHeader}>
+            <h1 className={styles.tagHeader}>Manage Tags</h1>
+            <div className={styles.tagCount}>{totalTags}</div>
+          </div>
+          {tagsByCategory?.map((tg) => {
+            if (!tg.tags?.length) {
+              return null;
+            }
+            const isExpanded = expandedList[tg.tagCategoryId];
+            return (
+              <div>
+                <div className={styles.categoryContainer}>
+                  <div className={styles.categoryName}>
+                    {tg.tagCategoryName}
+                  </div>
+                  <div onClick={() => handleToggleExpand(tg.tagCategoryId)}>
+                    {isExpanded ? (
+                      <span className={styles.expandIcons}>-</span>
+                    ) : (
+                      <span className={styles.expandIcons}>+</span>
+                    )}
+                  </div>
+                </div>
+                {isExpanded ? (
+                  <div className={styles.tagCategoryId}>
+                    {tg.tags?.map((tag) => (
+                      <div
+                        className={styles.tagRow}
+                        onClick={(e) => toggleTagSelection(e, tag.tagId)}
+                      >
+                        <div className={styles.tagRowText}>
+                          <span className={styles.delSTyles}>
+                            <TagIcon />
+                          </span>
+                          <span>{tag.tagLabel}</span>
+                        </div>
+                        <div>
+                          {selectedTagsIds.includes(tag.tagId) && <Check />}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                ) : null}
+              </div>
+            );
+          })}
+          <div className={styles.tagFooter}>
+            <Button
+              label="Reset"
+              type={"secondary"}
+              onClick={handleResetTags}
+              disable={isProcessing}
+            />
+            <Button
+              label="Apply"
+              type={"primary"}
+              onClick={handleSaveTags}
+              disable={isProcessing}
+            />
+          </div>
+        </div>
+      }
+    >
+      <div
+        onClick={() => {
+          setTagModalOpen(true);
+        }}
+      >
+        <CallScript />
+      </div>
+    </TinyPopover>
+  );
+}
+
+export default ({
+  personalInfo,
+  isEdit,
+  setEdit,
+  setDisplay,
+  leadsId,
+  refreshContactDetails,
+}) => {
   const [modalOpen, setModalOpen] = useState(false);
 
   let {
@@ -26,6 +202,7 @@ export default ({ personalInfo, isEdit, setEdit, setDisplay, leadsId }) => {
     statusName = "",
     contactRecordType = "",
     contactPreferences,
+    leadTags,
   } = personalInfo;
 
   emails = emails.length > 0 ? emails[0].leadEmail : NOT_AVAILABLE;
@@ -41,7 +218,7 @@ export default ({ personalInfo, isEdit, setEdit, setDisplay, leadsId }) => {
     setDisplay("Details");
     setEdit(true);
   };
-  
+
   return (
     <div className="nameCard">
       <Container className={styles.container}>
@@ -69,6 +246,14 @@ export default ({ personalInfo, isEdit, setEdit, setDisplay, leadsId }) => {
           <div className=" customSelectbox personalInfo">
             <label className="text-bold">Stage</label>
             <StageSelect value={statusName} original={personalInfo} />
+          </div>
+          <div className="personalInfo personalInfoCallScriptIcon">
+            <label className="text-bold">Tag</label>
+            <TagsIcon
+              leadTags={leadTags}
+              leadsId={leadsId}
+              onUpdateTags={refreshContactDetails}
+            />
           </div>
           <div className="personalInfo personalInfoCallScriptIcon">
             <label className="text-bold">Call Script</label>
