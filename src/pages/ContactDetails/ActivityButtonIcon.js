691d0ebf028954c5e5e68aa45afb09944f7c0c31
vishalmarneni <79602980+vishalmarneni@users.noreply.github.com>
2022-09-07 08:57:25 +0530

    Added downlaod button for the outbound call recorded activity type (#913)

--- src/pages/ContactDetails/ActivityButtonIcon.js
+++ src/pages/ContactDetails/ActivityButtonIcon.js
@@ -13,6 +13,7 @@ export default function ActivityButtonIcon({ activitySubject }) {
       "Incoming Call": <LINK />,
       "Call Recording": <DOWNLOAD />,
       "Contact's new call log created": <DOWNLOAD />,
+      "Outbound Call Recorded": <DOWNLOAD />,
       "Scope of Appointment Signed": <SOACOMPLETE />,
       "Scope of Appointment Completed": <SOAVIEW />,
       "Plan Shared": <VIEWPLANS />,
