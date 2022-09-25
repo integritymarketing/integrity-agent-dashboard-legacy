c0a1c7a6b5887c104717673461190404741e9a98
Vishal Marneni <vishal.marneni@integritymarketing.com>
2022-09-25 01:45:57 -0500

    feat: help page updates

---
+++ src/pages/Help/index.js
@@ -0,0 +1,80 @@
+import CallIcon from "components/icons/callicon";
+import GlobalFooter from "partials/global-footer";
+import GlobalNav from "partials/global-nav-v2";
+import Heading2 from "packages/Heading2";
+import Heading3 from "packages/Heading3";
+import Heading4 from "packages/Heading4";
+import MailIcon from "components/icons/mailicon";
+import Media from "react-media";
+import Paragraph from "packages/Paragraph";
+import React, { useState } from "react";
+import styles from "./styles.module.scss";
+import { Helmet } from "react-helmet-async";
+import { Box } from "@material-ui/core";
+
+export default () => {
+  const [isMobile, setIsMobile] = useState(false);
+  let mainContentClassName = "container " + styles.headerLayout;
+  return (
+    <React.Fragment>
+      <Media
+        query={"(max-width: 500px)"}
+        onChange={(isMobile) => {
+          setIsMobile(isMobile);
+        }}
+      />
+      <Helmet>
+        <title>MedicareCENTER - Help</title>
+      </Helmet>
+      <GlobalNav />
+      <div className={styles.layout}>
+        {!isMobile && (
+          <div className={styles.headerLayoutContainer}>
+            <div id="main-content" className={mainContentClassName}>
+              <div>
+                <Heading2
+                  className={styles.headerLayoutText}
+                  text="Need Help?"
+                />
+              </div>
+            </div>
+          </div>
+        )}
+        <div className={styles.outerContainer}>
+          <div className={styles.centerContainer}>
+            <Box sx={{ py: "5px" }}>
+              {isMobile ? (
+                <Heading2 text={"Contact Support"} />
+              ) : (
+                <Heading3 text={"Contact Support"} />
+              )}
+            </Box>
+            <Box sx={{ py: "5px" }}>
+              <Heading4
+                text={
+                  "Call or email one of our support representatives to help resolve your issue."
+                }
+                className={styles.subHeadingOverride}
+              />
+            </Box>
+            <Box className={styles.sectionRoundedTop}>
+              <div className={styles.iconBg}>
+                <CallIcon />
+              </div>
+              <a className={styles.cta} href="tel:8888183760"><Paragraph text={"888-818-3760"} /></a>
+            </Box>
+            <Box className={styles.sectionRoundedBottom}>
+              <div className={styles.iconBg}>
+                <MailIcon />
+              </div>
+              <a className={styles.cta} href="mailto: support@medicarecenter.com">
+                <Paragraph text={"support@medicarecenter.com"} />
+              </a>
+            </Box>
+          </div>
+        </div>
+      </div>
+      <GlobalFooter className={styles.footer} />
+    </React.Fragment>
+  );
+};
