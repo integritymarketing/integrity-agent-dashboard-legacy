e6eaf8ded6fa63225bcd22ca3c46779fd77685a4
Raja Madasu <rmadasu@SSC-5LZD593.ad.integritymarketing.com>
2022-10-04 17:25:41 -0500

    sunifire redirect changes

--- src/components/functional/auth-signin-callback.js
+++ src/components/functional/auth-signin-callback.js
@@ -1,16 +1,27 @@
 import { useEffect, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import AuthContext from "contexts/auth";
+import useQueryParams from "hooks/useQueryParams";
 
 export default () => {
   const auth = useContext(AuthContext);
   const history = useHistory();
+  const params = useQueryParams();
 
-  useEffect(() => {    
-     auth.signinSilent().catch((error) => {
-       history.replace("/error?code=login_callback_error");
-     });
-  }, [auth, history]);
+  useEffect(() => {
+    const params1 = new URLSearchParams(
+      new URL(params.get("ReturnUrl")).search
+    );
+    let clientId = params1.get("client_id");
+    if (clientId === "AgentMobileSunfire") {
+      window.location.href =
+        "https://qa-sunfire.sunfirematrix.com/api/partner/sso/int";
+      return;
+    }
+    auth.signinSilent().catch((error) => {
+      history.replace("/error?code=login_callback_error");
+    });
+  }, [auth, history, params]);
 
   return "";
 };
