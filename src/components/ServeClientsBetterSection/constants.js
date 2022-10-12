4f83deae709a003d31078aea0f9c2b20ab436a74
vishalmarneni <79602980+vishalmarneni@users.noreply.github.com>
2022-10-13 01:08:59 +0530

    update the image on marketing page (#1020)

--- src/components/ServeClientsBetterSection/constants.js
+++ src/components/ServeClientsBetterSection/constants.js
@@ -2,7 +2,7 @@ const useConstants = () => {
     return {
         SERVE_CLIENTS_BETTER: 'Serve Clients Better',
         WITH_CONTACT_MANAGEMENT:
-        'With Contact Management in our user-friendly CRM, you can spend less time managing your clients and more time serving them, all while staying 100% compliant.'
+        'With Contact Management, our user-friendly CRM, you can spend less time managing your clients and more time serving them, all while staying 100% compliant.'
     };
 };
 
--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react';
 
-import image from './image.png';
+import image from './image.jpg';
 
 import styles from './styles.module.scss';
 
