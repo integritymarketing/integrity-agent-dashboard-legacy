28d18ddc478b28d6ff98c3e3e54cb3eddf44d78a
Raja Madasu <rmadasu@SSC-5LZD593.ad.integritymarketing.com>
2022-10-13 18:35:02 -0500

    Fixed outstanding issues test1

--- src/pages/dashbaord/DashboardActivityTable.js
+++ src/pages/dashbaord/DashboardActivityTable.js
@@ -372,13 +372,14 @@ export default function DashboardActivityTable({
   };
 
   const handleAddActivtyNotes = useCallback(
-    async (activity, activityBody) => {
+    async (activity, activityNote) => {
       const { activityBody, activitySubject, activityId } = activity;
       const leadsId = selectedLead.leadsId;
       const payload = {
         activityBody,
         activitySubject,
         activityId,
+        activityNote,
       };
       try {
         await clientsService.updateActivity(payload, leadsId);
