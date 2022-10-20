359784ae6cfccedf515cc9568479cee2e05a7375
vishalmarneni <79602980+vishalmarneni@users.noreply.github.com>
2022-10-20 20:42:05 +0530

    fix: Local Time Details do not show in Safari (#1046)

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -1,6 +1,28 @@
-import moment from 'moment'
+import moment from "moment";
+
+const months = {
+  Jan: "01",
+  Feb: "02",
+  Mar: "03",
+  Apr: "04",
+  May: "05",
+  Jun: "06",
+  Jul: "07",
+  Aug: "08",
+  Sep: "09",
+  Oct: "10",
+  Nov: "11",
+  Dec: "12",
+};
 
 export const dateFormatter = (date, format) => {
-  return moment(date || Date.now()).format(format);
+  if (date) {
+    date = date.split(" ");
+    return moment(
+      [date[3], months[date[1]], date[2]].join("-"),
+      "YYYY-MM-DD"
+    ).format(format);
+  } else {
+    return moment(Date.now()).format(format);
+  }
 };
-  
