3ab067cacafa26482d1e29e3933d61c9181d07d2
vishalmarneni <79602980+vishalmarneni@users.noreply.github.com>
2022-11-09 20:59:12 +0530

    Removed the duplicate key (#1089)

--- src/pages/contacts/contactRecordInfo/PersonalInfo.js
+++ src/pages/contacts/contactRecordInfo/PersonalInfo.js
@@ -190,7 +190,6 @@ function TagsIcon({ leadsId, leadTags, onUpdateTags }) {
         type: "error",
         message:
           "Tag length should be between 2 and 10, and only allow alphanumeric, single space, single hyphen(-), single underscore(_)",
-        time: 3000,
       });
       return Promise.reject();
     }
