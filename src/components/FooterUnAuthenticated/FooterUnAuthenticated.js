06c4e26253ea101ded092be97a39fb61bf8df192
vishalmarneni <79602980+vishalmarneni@users.noreply.github.com>
2023-02-07 01:51:28 +0530

    fix: add footer logo (#1198)

--- src/components/FooterUnAuthenticated/FooterUnAuthenticated.js
+++ src/components/FooterUnAuthenticated/FooterUnAuthenticated.js
@@ -3,31 +3,35 @@ import { Link } from "react-router-dom";
 import styles from "./FooterUnAuthenticated.module.scss";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { Typography } from "@mui/material";
+import Integrity from "./Integrity.svg";
 
 export const FooterUnAuthenticated = (props) => {
   return (
     <Grid
       alignItems={"center"}
-      justifyContent={{xs: "center", sm:"space-between"}}
+      justifyContent={{ xs: "center", sm: "space-between" }}
       className={styles.footerContainer}
       container
       px={{ xs: "0rem", sm: "2rem", md: "8.5rem" }}
     >
       <Grid container>
-      <Link to={`/terms`} className={styles.textContent}>
-        Terms of Use
-      </Link>
-      <Typography className={styles.textContent} px={"1rem"}>
-        |
-      </Typography>
-      <Link to={`/privacy`} className={styles.textContent}>
-        Privacy Policy
-      </Link>
+        <Link to={`/terms`} className={styles.textContent}>
+          Terms of Use
+        </Link>
+        <Typography className={styles.textContent} px={"1rem"}>
+          |
+        </Typography>
+        <Link to={`/privacy`} className={styles.textContent}>
+          Privacy Policy
+        </Link>
       </Grid>
       <Grid>
-      <Typography align="right" className={styles.textContent}>
-        &copy; {new Date().getFullYear()} Integrity. All rights reserved.{" "}
-      </Typography>
+        <Grid align="right" container>
+          <img alt="Integrity Logo" src={Integrity} />
+          <Typography ml="1rem" className={styles.textContent}>
+            &copy; {new Date().getFullYear()} Integrity. All rights reserved.{" "}
+          </Typography>
+        </Grid>
       </Grid>
     </Grid>
   );
