b50eb58f6fc57807b8b2b50cd62d72e41ab02ee0
vishalmarneni <79602980+vishalmarneni@users.noreply.github.com>
2023-02-09 00:58:54 +0530

    fix: update view plans icon (#1211)

--- src/components/icons/activities/ViewPlans.js
+++ src/components/icons/activities/ViewPlans.js
@@ -2,8 +2,7 @@ import * as React from "react";
 
 const VIEWPLANS = (props) => (
   <svg
-    id="SOA_View"
-    data-name="SOA View"
+    id="View"
     xmlns="http://www.w3.org/2000/svg"
     width="32"
     height="32"
@@ -12,9 +11,9 @@ const VIEWPLANS = (props) => (
     <defs>
       <clipPath id="clip-path">
         <rect
-          id="Rectangle_20849"
-          data-name="Rectangle 20849"
-          width="13"
+          id="Rectangle_20850"
+          data-name="Rectangle 20850"
+          width="16"
           height="16"
           fill="none"
         />
@@ -30,22 +29,12 @@ const VIEWPLANS = (props) => (
         fill="rgba(33,117,244,0.1)"
       />
     </g>
-    <g id="Group_3324" data-name="Group 3324" transform="translate(10 8)">
-      <g id="Group_3323" data-name="Group 3323" clip-path="url(#clip-path)">
+    <g id="Group_3326" data-name="Group 3326" transform="translate(8 8)">
+      <g id="Group_3325" data-name="Group 3325" clip-path="url(#clip-path)">
         <path
-          id="Path_3515"
-          data-name="Path 3515"
-          d="M3.658,4.413H.5L4.29.5V3.761A.642.642,0,0,1,3.658,4.413Z"
-          fill="none"
-          stroke="#0052ce"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="1"
-        />
-        <path
-          id="Path_3516"
-          data-name="Path 3516"
-          d="M.5,5.759v9.089a.642.642,0,0,0,.632.652H11.868a.642.642,0,0,0,.632-.652V1.153A.643.643,0,0,0,11.868.5H5.553"
+          id="Path_3517"
+          data-name="Path 3517"
+          d="M14.2,8.652v5.657A1.191,1.191,0,0,1,13.005,15.5H1.691A1.191,1.191,0,0,1,.5,14.309V2.995A1.191,1.191,0,0,1,1.691,1.8H7.348"
           fill="none"
           stroke="#0052ce"
           strokeLinecap="round"
@@ -53,10 +42,10 @@ const VIEWPLANS = (props) => (
           strokeWidth="1"
         />
         <line
-          id="Line_1045"
-          data-name="Line 1045"
-          y2="4.421"
-          transform="translate(6.5 6.79)"
+          id="Line_1047"
+          data-name="Line 1047"
+          x2="3.913"
+          transform="translate(11.587 0.5)"
           fill="none"
           stroke="#0052ce"
           strokeLinecap="round"
@@ -64,10 +53,22 @@ const VIEWPLANS = (props) => (
           strokeWidth="1"
         />
         <line
-          id="Line_1046"
-          data-name="Line 1046"
-          x1="4.421"
-          transform="translate(4.29 9.001)"
+          id="Line_1048"
+          data-name="Line 1048"
+          y1="3.913"
+          transform="translate(15.5 0.5)"
+          fill="none"
+          stroke="#0052ce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1"
+        />
+        <line
+          id="Line_1049"
+          data-name="Line 1049"
+          y1="7.174"
+          x2="7.174"
+          transform="translate(8.326 0.5)"
           fill="none"
           stroke="#0052ce"
           strokeLinecap="round"
