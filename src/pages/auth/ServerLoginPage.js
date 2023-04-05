945de4cc2a09328454328949ae9234df8b565e77
rishithainturi <121522091+rishithainturi@users.noreply.github.com>
2023-04-05 10:30:02 -0500

    [feature/284684] dismissible availability switch (#1329)
    * [feature/284684] dismissable availabilty switch
    
    * [feature/284684] fixed build issue

--- src/pages/auth/ServerLoginPage.js
+++ src/pages/auth/ServerLoginPage.js
@@ -18,7 +18,6 @@ import { FooterUnAuthenticated } from "components/FooterUnAuthenticated";
 import { ContainerUnAuthenticated } from "components/ContainerUnAuthenticated";
 import { Box } from "@mui/material";
 import { Button } from "packages/Button";
-import { PreventOrientation } from "prevent-orientation";
 
 export default () => {
   const loading = useLoading();
@@ -130,12 +129,7 @@ export default () => {
       }
     }
   };
-  new PreventOrientation({
-    text: "Kindly rotate your device to the portrait mode for using this site",
-    color: "white",
-    background: "#051D43",
-    fontSize: "1.2rem",
-  }).preventLandscape();
+
   return (
     <React.Fragment>
       <Helmet>
--- src/partials/global-nav-v2/microComponent/AvailabilityOverlay.js
+++ src/partials/global-nav-v2/microComponent/AvailabilityOverlay.js
@@ -2,12 +2,17 @@ import Styles from "./AvailabilityOverlay.module.scss";
 import React from "react";
 import Arrow from "./arrow.svg";
 import Box from "./box.svg";
+import ClickAwayListener from "react-click-away-listener";
 
 export default function AvailabilityOverlay({ hideModal }) {
   function onClick() {
     localStorage.setItem("isCheckInUpdateModalDismissed", true);
     hideModal();
   }
+  // Click away handle
+  const handleClickAway = () => {
+    onClick();
+  };
   return (
     <div className={Styles.wrapper}>
       <div className={Styles.arrowBox}>
@@ -15,33 +20,35 @@ export default function AvailabilityOverlay({ hideModal }) {
         <br />
         <img src={Arrow} className={Styles.box} alt="" />
       </div>
-      <div className={Styles.body}>
-        <div className={Styles.header}>“Check In” has been updated.</div>
-        <div className={Styles.content}>
-          Set your availability for access to{" "}
-          <a
-            href="https://learningcenter.tawebhost.com/MedicareCENTER-Real-Time-Leads-User-Guide.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={Styles.link}
-          >
-            real-time leads
-          </a>
-          . Availability Preferences are found on your{" "}
-          <a
-            href="/account"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={Styles.link}
-          >
-            Account Page
-          </a>
-          .
-          <button className={Styles.button} onClick={onClick}>
-            Dismiss
-          </button>
+      <ClickAwayListener onClickAway={handleClickAway}>
+        <div className={Styles.body}>
+          <div className={Styles.header}>“Check In” has been updated.</div>
+          <div className={Styles.content}>
+            Set your availability for access to{" "}
+            <a
+              href="https://learningcenter.tawebhost.com/MedicareCENTER-Real-Time-Leads-User-Guide.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={Styles.link}
+            >
+              real-time leads
+            </a>
+            . Availability Preferences are found on your{" "}
+            <a
+              href="/account"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={Styles.link}
+            >
+              Account Page
+            </a>
+            .
+            <button className={Styles.button} onClick={onClick}>
+              Dismiss
+            </button>
+          </div>
         </div>
-      </div>
+      </ClickAwayListener>
     </div>
   );
 }
