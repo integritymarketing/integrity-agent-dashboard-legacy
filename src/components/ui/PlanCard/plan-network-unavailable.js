a6c1fca2321406651fd361aef60eff97848402f5
Vinodh Khurra <Vinodh.Khurra@integritymarketing.com>
2023-04-26 23:08:34 +0530

    tool tip has updated in 500 plans handling

--- src/components/ui/PlanCard/plan-network-unavailable.js
+++ src/components/ui/PlanCard/plan-network-unavailable.js
@@ -2,16 +2,18 @@ import React from "react";
 import TooltipMUI from "packages/ToolTip";
 
 const PlanCoverageUnavailable = ({ title }) => {
+  let titleData =
+    title === "Pharmacy"
+      ? `Looks like you need to add a pharmacy, or there is a problem
+  with the Pharmacy Service provider. Please try again later.`
+      : `${title} service partner is not returning current status. Please try again later.`;
   return (
     <div className={"status-item"}>
       <div className={"status-text"}>
-        {title} Looks like you need to add a pharmacy, or there is a problem
-        with the Pharmacy Service provider. Please try again later.
+        {title} status temporarily unavailable
       </div>
       <div className="status-icon">
-        <TooltipMUI
-          title={`${title} service partner is not returning current status. Please try again later.`}
-        />
+        <TooltipMUI title={titleData} />
       </div>
     </div>
   );
