50150ec6cd99ef20dcaf0e1d2e8af2b683f27586
vinodkurra <[ugandharkurra1996@gmail.com]>
2023-06-15 20:00:55 +0530

    mobile view for task list has completed

--- src/components/TaskList/UnlinkedPolicies/index.js
+++ src/components/TaskList/UnlinkedPolicies/index.js
@@ -5,27 +5,27 @@ import TooltipMUI from "packages/ToolTip";
 import { Button } from "components/ui/Button";
 import Dialog from "packages/Dialog";
 import { ReactComponent as LinkContactCircle } from "pages/dashbaord/LinkContactCircle.svg";
-import NoUnlinkedPolicy from 'images/no-unlinked-policies.svg';
+import NoUnlinkedPolicy from "images/no-unlinked-policies.svg";
 
 import "./style.scss";
-// const mockData = [
-//   {
-//     policyName: "Humana HMO 2343",
-//     policyId: "252456",
-//     policyCarrier: "Humana",
-//     policyHolder: "Anne Polsen",
-//     policyStatus: "Started",
-//   },
-//   {
-//     policyName: "Humana HMO 2343",
-//     policyId: "252456",
-//     policyCarrier: "Humana",
-//     policyHolder: "Anne Polsen",
-//     policyStatus: "Started",
-//   },
-// ];
-
-const mockData = [];
+const mockData = [
+  {
+    policyName: "Humana HMO 2343",
+    policyId: "252456",
+    policyCarrier: "Humana",
+    lastName: "Polsen",
+    firstName: "Anne",
+    policyStatus: "Started",
+  },
+  {
+    policyName: "Humana HMO 2343",
+    policyId: "252456",
+    policyCarrier: "Humana",
+    lastName: "Polsen",
+    firstName: "Anne",
+    policyStatus: "Started",
+  },
+];
 
 const UnlinkedPolicyCard = ({ callData }) => {
   const [isMobile, setIsMobile] = useState(false);
@@ -38,34 +38,45 @@ const UnlinkedPolicyCard = ({ callData }) => {
           setIsMobile(isMobile);
         }}
       />
-      <Grid container spacing={2}>
-        <Grid item xs={6} alignSelf={"center"} md={5} sx={{ color: "#434A51" }}>
-          <p className="up-name">{callData.policyName}</p>
+      <Grid container className={"infoContainer"} spacing={2}>
+        <Grid
+          item
+          xs={6}
+          alignSelf={!isMobile ? "center" : ""}
+          md={5}
+          className="policy-mobile"
+        >
+          <p className="up-name">{callData?.policyName}</p>
           <p>
             <span className="up-label">Policy Id:</span>
-            <span className="up-info">{callData.policyId}</span>
+            <span className="up-info">{callData?.policyId}</span>
           </p>
           <p>
-            <span className="up-label">Policy Carrier:</span>{" "}
-            <span className="up-info"> {callData.policyCarrier}</span>
+            <span className="up-label">Policy Carrier:</span>
+            <span className="up-info"> {callData?.policyCarrier}</span>
           </p>
         </Grid>
 
-        <Grid item xs={6} md={3} alignSelf={"center"}>
-          <div className="up-label-bold">Policy Holder</div>{" "}
-          <div className="up-info-bold">{callData.policyHolder}</div>
+        <Grid
+          item
+          xs={6}
+          md={3}
+          alignSelf={!isMobile ? "center" : ""}
+          className="policy-holder-mobile"
+        >
+          <div className="up-label-bold">
+            Policy Holder {isMobile ? ":" : ""}
+          </div>
+          <div className="up-info-bold">
+            {callData?.firstName + callData?.lastName}
+          </div>
         </Grid>
         <Grid
           item
           xs={6}
           md={4}
           alignSelf={"center"}
-          sx={{
-            textAlign: "right",
-            display: "flex",
-            justifyContent: "center",
-            width: "30%",
-          }}
+          className="policy-button-mobile"
         >
           <Button
             icon={<LinkContactCircle />}
@@ -82,54 +93,71 @@ const UnlinkedPolicyCard = ({ callData }) => {
 };
 
 const UnlinkedPolicyList = ({ isError }) => {
-
   const [dialogOpen, setDialogOpen] = useState(false);
 
-  if(isError) {
-
+  if (isError) {
     return (
-        <div className="error-container">
-          <p className="error-text">Status Temporarily Unavailable</p>
-          <TooltipMUI 
-            titleData={"Service partner is not returning current status. Please try again later."}
-            onClick={() => setDialogOpen(true)}
-          />
-          <Dialog
-            title='ERROR'
-            open={dialogOpen}
-            onClose={() => setDialogOpen(false)}
-            titleWithIcon={false}
-          >
-            <p>Service partner is not returning current status. Please try again later.</p>
-          </Dialog>
-        </div>
+      <div className="error-container">
+        <p className="error-text">Status Temporarily Unavailable</p>
+        <TooltipMUI
+          titleData={
+            "Service partner is not returning current status. Please try again later."
+          }
+          onClick={() => setDialogOpen(true)}
+        />
+        <Dialog
+          title="ERROR"
+          open={dialogOpen}
+          onClose={() => setDialogOpen(false)}
+          titleWithIcon={false}
+        >
+          <p>
+            Service partner is not returning current status. Please try again
+            later.
+          </p>
+        </Dialog>
+      </div>
     );
-  } else if(mockData.length === 0) {
-
+  } else if (mockData.length === 0) {
     return (
-
       <div className="no-data-container">
-            <div className="no-data-icon-container">
-              <img src={NoUnlinkedPolicy} className="no-data-icon" alt="No policy Data" />
-            </div>
-            <div className="no-data-text-container">
-              <p className="no-data-text-heading">There are no unlinked policies at this time.</p>
-              <p className="no-data-text-desc">To learn more about how you can receive leads through consumer callback requests, 
-              <a href="/MedicareCENTER-Requested-Callbacks-Guide.pdf" className="click-here-link">click here.</a></p>
-            </div>
-          </div>
-    )
+        <div className="no-data-icon-container">
+          <img
+            src={NoUnlinkedPolicy}
+            className="no-data-icon"
+            alt="No policy Data"
+          />
+        </div>
+        <div className="no-data-text-container">
+          <p className="no-data-text-heading">
+            There are no unlinked policies at this time.
+          </p>
+          <p className="no-data-text-desc">
+            To learn more about how you can receive leads through consumer
+            callback requests,
+            <a
+              href="/MedicareCENTER-Requested-Callbacks-Guide.pdf"
+              className="click-here-link"
+            >
+              click here.
+            </a>
+          </p>
+        </div>
+      </div>
+    );
   }
   return (
     <>
       <div className="up-card-container">
-        {mockData.map((data) => {
+        {mockData?.map((data) => {
           return <UnlinkedPolicyCard callData={data} />;
         })}
       </div>
-      <div className="jumpList-card">
-        <Button type="tertiary" label="Jump to List" className="jumpList-btn" />
-      </div>
+      {mockData?.length > 5 && (
+        <div className="jumpList-card">
+          <Button type="tertiary" label="Show More" className="jumpList-btn" />
+        </div>
+      )}
     </>
   );
 };
