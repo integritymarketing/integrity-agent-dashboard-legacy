0bdb595bd029fd385804246ca405c635851a2f27
vinodkurra <[ugandharkurra1996@gmail.com]>
2023-06-22 20:14:14 +0530

    Updated calculations

--- src/components/ui/PlanDetailsTable/shared/prescriptions-compare-table.js
+++ src/components/ui/PlanDetailsTable/shared/prescriptions-compare-table.js
@@ -63,9 +63,7 @@ function buildPrescription({ planData, monthsRemaining }) {
   }
 
   if (pharmacyCost.monthlyCosts && Array.isArray(pharmacyCost.monthlyCosts)) {
-    var monthlyCostLength = pharmacyCost.monthlyCosts.length;
-    var startingMonth = monthlyCostLength - monthsRemaining;
-    for (var i = startingMonth - 1; i < pharmacyCost.monthlyCosts.length; i++) {
+    for (var i = 0; i < monthsRemaining; i++) {
       var monthlyCost = pharmacyCost.monthlyCosts[i];
       if (monthlyCost.costDetail && Array.isArray(monthlyCost.costDetail)) {
         for (var k = 0; k < monthlyCost.costDetail.length; k++) {
@@ -108,6 +106,14 @@ export function PrescriptionsCompareTable({
   const effectiveDateString = `${effectiveStartDate.toLocaleString("default", {
     month: "long",
   })} ${effectiveStartDate.getFullYear()} `;
+
+  console.log(
+    "TESTING CALCULATIONS",
+    effectiveEndDate.getMonth(),
+    effectiveEndDate,
+    effectiveStartDate.getMonth(),
+    effectiveStartDate
+  );
 
   const columns = useMemo(
     () => [
