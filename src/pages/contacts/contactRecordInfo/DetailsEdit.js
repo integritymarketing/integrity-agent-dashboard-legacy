9ad2118669911099262c7fbdffcd7bab41393b00
vinodkurra <ugandharkurra1996@gmail.com>
2023-07-28 20:26:19 +0530

    update in edit details page

--- src/pages/contacts/contactRecordInfo/DetailsEdit.js
+++ src/pages/contacts/contactRecordInfo/DetailsEdit.js
@@ -714,7 +714,7 @@ export default (props) => {
                     </label>
 
                     <DatePickerMUI
-                      value={values.partB}
+                      value={values.partB === null ? "" : values.partB}
                       onChange={(value) => {
                         setFieldValue("partB", formatDate(value, "yyyy-MM-dd"));
                       }}
