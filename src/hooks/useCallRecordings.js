1760176ce54f479b0fa702e0580d85f7f288b014
vishalmarneni <vishal.marneni@integritymarketing.com>
2023-09-07 14:05:38 -0500

    fix: call recordings api call

--- src/hooks/useCallRecordings.js
+++ src/hooks/useCallRecordings.js
@@ -7,39 +7,51 @@ import useUserProfile from "hooks/useUserProfile";
 export default function useCallRecordings() {
   const [callRecordings, setCallRecordings] = useState([]);
   const { agentId } = useUserProfile();
+  const [fetchTimeoutId, setFetchTimeoutId] = useState(null);
+
+  async function fetchCallRecordings() {
+    try {
+      const recordings =
+        await callRecordingsService.getAllCallRecordingsByAgent();
+
+      if (recordings.length > 0) {
+        const sortedRecordings = recordings.sort(
+          (a, b) => new Date(b.callStartTime) - new Date(a.callStartTime)
+        );
+        setCallRecordings(sortedRecordings);
+      } else {
+        setCallRecordings([]);
+      }
+    } catch (error) {
+      Sentry.captureException(error);
+    }
+  }
 
   useEffect(() => {
-    async function fetchCallRecordings() {
-      try {
-        const recordings =
-          await callRecordingsService.getAllCallRecordingsByAgent();
-
-        if (recordings.length > 0) {
-          const sortedRecordings = recordings.sort(
-            (a, b) => new Date(b.callStartTime) - new Date(a.callStartTime)
-          );
-          setCallRecordings(sortedRecordings);
-        } else {
-          setCallRecordings([]);
-        }
-      } catch (error) {
-        Sentry.captureException(error);
+    function handleActiveCall(status) {
+      if (status) {
+        setFetchTimeoutId(setTimeout(fetchCallRecordings, 5000));
+      } else {
+        setFetchTimeoutId(setTimeout(fetchCallRecordings, 15000));
       }
     }
 
-    fetchCallRecordings();
+    if (agentId) {
+      const connection = getSignalRConnection(agentId);
 
-    const connection = getSignalRConnection(agentId);
+      connection.on("ActiveCall", handleActiveCall);
 
-    function handleActiveCall(status) {
-      setTimeout(fetchCallRecordings, status ? 5000 : 15000);
+      return () => {
+        connection.off("ActiveCall", handleActiveCall);
+        clearTimeout(fetchTimeoutId);
+      };
     }
+  }, [agentId, fetchTimeoutId]);
 
-    connection.on("ActiveCall", handleActiveCall);
-
-    return () => {
-      connection.off("ActiveCall", handleActiveCall);
-    };
+  useEffect(() => {
+    if (agentId) {
+      fetchCallRecordings();
+    }
   }, [agentId]);
 
   return callRecordings;
