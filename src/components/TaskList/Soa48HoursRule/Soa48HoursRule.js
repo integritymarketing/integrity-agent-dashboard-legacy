903e40e2ea51cb6f068629f2349eac295c0384cf
rishitha inturi <Rishitha.Inturi@integritymarketing.com>
2023-09-10 01:16:59 -0500

    [284963] added Prop Types

--- src/components/TaskList/Soa48HoursRule/Soa48HoursRule.js
+++ src/components/TaskList/Soa48HoursRule/Soa48HoursRule.js
@@ -1,5 +1,6 @@
 import React from "react";
-import styles from "./Soa48HoursRule.module.scss";
+import PropTypes from "prop-types";
+import { useHistory } from "react-router-dom";
 import {
   formatDate,
   convertToLocalDateTime,
@@ -7,9 +8,9 @@ import {
 } from "utils/dates";
 import { Button } from "components/ui/Button";
 import OpenIcon from "components/icons/open";
-import { useHistory } from "react-router-dom";
+import styles from "./Soa48HoursRule.module.scss";
 
-function Soa48HoursRule({ taskList, isMobile, refreshData }) {
+const Soa48HoursRule = ({ taskList, isMobile, refreshData }) => {
   const history = useHistory();
 
   const getDateTime = (dateString) => {
@@ -29,67 +30,71 @@ function Soa48HoursRule({ taskList, isMobile, refreshData }) {
 
   return (
     <div className={styles.container}>
-      {taskList.map((item, index) => {
-        return (
-          <div className={styles.item}>
-            <div className={styles.section}>
-              <div className={styles.title1}>
-                Soa sent {getDateTime(item.sentDate).date} to
-              </div>
-              <div
-                className={styles.title2}
-              >{`${item.firstName} ${item.lastName}`}</div>
-              <div className={styles.title3}>
-                {item.phoneNumber || item.sentTo}
-              </div>
+      {taskList.map((item) => (
+        <div className={styles.item} key={item.id}>
+          <div className={styles.section}>
+            <div className={styles.title1}>
+              Soa sent {getDateTime(item.sentDate).date} to
             </div>
-            <div className={styles.section}>
-              <div className={styles.dateContainer}>
-                <div className={styles.title4}>Date Signed: </div>
-                <div className={styles.title1}>
-                  {getDateTime(item.signedDate).date}
-                </div>
-              </div>
-              <div className={styles.dateContainer}>
-                <div className={styles.title4}>Time Signed: </div>
-                <div className={styles.title1}>
-                  {getDateTime(item.signedDate).time}
-                </div>
-              </div>
+            <div className={styles.title2}>
+              {`${item.firstName} ${item.lastName}`}
             </div>
-            <div className={`${styles.section} ${styles.contact}`}>
-              <div className={styles.title4}>Contact After</div>
-              <div className={styles.dateContainer}>
-                <div className={styles.dateItem}>
-                  {getDateTime(item.contactAfterDate).date}
-                </div>
-                <div className={styles.title1}>at</div>
-                <div className={styles.dateItem}>
-                  {getDateTime(item.contactAfterDate).time}
-                </div>
-              </div>
-            </div>
-            <div
-              className={`${styles.section} ${styles.action} ${
-                isMobile ? styles.mobileIcon : ""
-              }`}
-            >
-              <Button
-                className={`${styles.completeBtn} ${
-                  isWithinTwoDays(item.contactAfterDate) ? styles.disabled : ""
-                }`}
-                label={isMobile ? "" : "Complete"}
-                onClick={() => navigateToConfirmSOA(item)}
-                type="primary"
-                icon={<OpenIcon />}
-                iconPosition="right"
-              />
+            <div className={styles.title3}>
+              {item.phoneNumber || item.sentTo}
             </div>
           </div>
-        );
-      })}
+          <div className={styles.section}>
+            <div className={styles.dateContainer}>
+              <div className={styles.title4}>Date Signed: </div>
+              <div className={styles.title1}>
+                {getDateTime(item.signedDate).date}
+              </div>
+            </div>
+            <div className={styles.dateContainer}>
+              <div className={styles.title4}>Time Signed: </div>
+              <div className={styles.title1}>
+                {getDateTime(item.signedDate).time}
+              </div>
+            </div>
+          </div>
+          <div className={`${styles.section} ${styles.contact}`}>
+            <div className={styles.title4}>Contact After</div>
+            <div className={styles.dateContainer}>
+              <div className={styles.dateItem}>
+                {getDateTime(item.contactAfterDate).date}
+              </div>
+              <div className={styles.title1}>at</div>
+              <div className={styles.dateItem}>
+                {getDateTime(item.contactAfterDate).time}
+              </div>
+            </div>
+          </div>
+          <div
+            className={`${styles.section} ${styles.action} ${
+              isMobile ? styles.mobileIcon : ""
+            }`}
+          >
+            <Button
+              className={`${styles.completeBtn} ${
+                isWithinTwoDays(item.contactAfterDate) ? styles.disabled : ""
+              }`}
+              label={isMobile ? "" : "Complete"}
+              onClick={() => navigateToConfirmSOA(item)}
+              type="primary"
+              icon={<OpenIcon />}
+              iconPosition="right"
+            />
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
+
+Soa48HoursRule.propTypes = {
+  taskList: PropTypes.array.isRequired,
+  isMobile: PropTypes.bool.isRequired,
+  refreshData: PropTypes.func.isRequired,
+};
 
 export default Soa48HoursRule;
