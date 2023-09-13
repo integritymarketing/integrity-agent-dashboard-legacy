ef1115458859d84cccbb131b8ef64550588afd54
vishalmarneni <vishal.marneni@integritymarketing.com>
2023-09-12 22:09:31 -0500

    fix: removed unused variables

--- src/pages/contacts/contactRecordInfo/viewAvailablePlans/steps/ReviewProviders.js
+++ src/pages/contacts/contactRecordInfo/viewAvailablePlans/steps/ReviewProviders.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect, useRef } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useHistory } from "react-router-dom";
 import { useRecoilState } from "recoil";
