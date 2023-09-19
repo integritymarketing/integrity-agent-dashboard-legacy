dc883639ae63459f0a3cad1cd3e608d2830681dc
vinodkurra <ugandharkurra1996@gmail.com>
2023-09-19 14:27:14 +0530

    Added logic for profile linking

---
+++ src/components/icons/activities/LocationIcon.js
@@ -0,0 +1,78 @@
+import * as React from "react";
+
+const LOCATION = () => (
+  <svg
+    data-name="LOCATION-UPDATED"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <rect
+      id="Rectangle_13650"
+      data-name="Rectangle 13650"
+      width="24"
+      height="24"
+      rx="4"
+      fill="rgba(255,255,255,0)"
+    />
+    <g id="Costs" transform="translate(3 3)">
+      <path
+        id="Path_21217"
+        data-name="Path 21217"
+        d="M1,9A8,8,0,1,0,3.343,3.343,8,8,0,0,0,1,9Z"
+        transform="translate(0 0)"
+        fill="#fff"
+        stroke="#0052ce"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1"
+      />
+      <g
+        id="Group_6462"
+        data-name="Group 6462"
+        transform="translate(7.607 6.217)"
+      >
+        <path
+          id="Path_21218"
+          data-name="Path 21218"
+          d="M4.169,1.435H2.679A1.29,1.29,0,0,0,2.1,3.878l1.356.679A1.29,1.29,0,0,1,2.882,7h-1.5"
+          transform="translate(-1.387 -1.435)"
+          fill="none"
+          stroke="#0052ce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1"
+        />
+      </g>
+      <g id="Group_6463" data-name="Group 6463" transform="translate(9 5.183)">
+        <path
+          id="Path_21219"
+          data-name="Path 21219"
+          d="M1.348,1.87V2.913"
+          transform="translate(-1.348 -1.87)"
+          fill="none"
+          stroke="#0052ce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1"
+        />
+      </g>
+      <g id="Group_6464" data-name="Group 6464" transform="translate(9 11.885)">
+        <path
+          id="Path_21220"
+          data-name="Path 21220"
+          d="M1.652,1.478V2.522"
+          transform="translate(-1.652 -1.478)"
+          fill="none"
+          stroke="#0052ce"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1"
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export default LOCATION;
