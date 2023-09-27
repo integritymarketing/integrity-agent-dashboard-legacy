21ac74c511518a35493a98fb2db4fe24c93c5d03
rishitha inturi <Rishitha.Inturi@integritymarketing.com>
2023-09-27 17:55:46 -0500

    [call-script] updated the text from carrier to orgnisations

--- src/packages/CallScriptModal/index.js
+++ src/packages/CallScriptModal/index.js
@@ -1,21 +1,20 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import PropTypes from 'prop-types';
-import Modal from 'components/Modal';
-import useFetch from 'hooks/useFetch';
-import styles from './styles.module.scss';
-import { DEFAULT_EFFECTIVE_YEAR } from 'utils/dates';
+import React, { useState, useEffect, useCallback } from "react";
+import PropTypes from "prop-types";
+import Modal from "components/Modal";
+import useFetch from "hooks/useFetch";
+import styles from "./styles.module.scss";
+import { DEFAULT_EFFECTIVE_YEAR } from "utils/dates";
 
 const DEFAULT_CARRIER_COUNT = 78;
-const DEFAULT_PLAN_COUNT = '2,613';
+const DEFAULT_PLAN_COUNT = "2,613";
 
-export const CallScriptModal = ({ 
-  modalOpen, 
-  handleClose, 
-  leadId, 
-  countyFips, 
-  postalCode 
+export const CallScriptModal = ({
+  modalOpen,
+  handleClose,
+  leadId,
+  countyFips,
+  postalCode,
 }) => {
-  
   const [carrierProductData, setCarrierProductData] = useState(null);
   const { Get: fetchCarrierProductData } = useFetch(
     `${process.env.REACT_APP_QUOTE_URL}/api/v2.0/Lead/${leadId}/Plan/Carrier/Count?Zip=${postalCode}&Fips=${countyFips}&Year=${DEFAULT_EFFECTIVE_YEAR}`
@@ -32,27 +31,40 @@ export const CallScriptModal = ({
     fetchCounts();
   }, [fetchCounts]);
 
-  let carrierCount = countyFips 
-    ? carrierProductData?.carrierCount || DEFAULT_CARRIER_COUNT 
+  let carrierCount = countyFips
+    ? carrierProductData?.carrierCount || DEFAULT_CARRIER_COUNT
     : `0-${carrierProductData?.carrierCount || DEFAULT_CARRIER_COUNT}`;
 
-  let productCount = countyFips 
-    ? carrierProductData?.planCount || DEFAULT_PLAN_COUNT 
+  let productCount = countyFips
+    ? carrierProductData?.planCount || DEFAULT_PLAN_COUNT
     : `0-${carrierProductData?.planCount || DEFAULT_PLAN_COUNT}`;
 
-  const renderModalContent = useCallback(() => (
-    <>
-      <div className={styles.cmsComplianceSection}>
-        To be in compliance with CMS guidelines, please read this script before every call.
-      </div>
-      <div className={styles.planInformationSection}>
-        We do not offer every plan available in your area. Currently, we represent {carrierCount} carriers which offer {productCount} products in your area. Please contact medicare.gov, 1-800-MEDICARE, or your local State Health Insurance Program (SHIP) to get information on all of your options.
-      </div>
-    </>
-  ), [carrierCount, productCount]);
+  const renderModalContent = useCallback(
+    () => (
+      <>
+        <div className={styles.cmsComplianceSection}>
+          To be in compliance with CMS guidelines, please read this script
+          before every call.
+        </div>
+        <div className={styles.planInformationSection}>
+          We do not offer every plan available in your area. Currently, we
+          represent {carrierCount} organizations which offer {productCount}{" "}
+          products in your area. Please contact medicare.gov, 1-800-MEDICARE, or
+          your local State Health Insurance Program (SHIP) to get information on
+          all of your options.
+        </div>
+      </>
+    ),
+    [carrierCount, productCount]
+  );
 
   return (
-    <Modal open={modalOpen} onClose={handleClose} title="Recorded Call Script" hideFooter>
+    <Modal
+      open={modalOpen}
+      onClose={handleClose}
+      title="Recorded Call Script"
+      hideFooter
+    >
       {renderModalContent()}
     </Modal>
   );
