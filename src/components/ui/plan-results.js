10d961896d815e83dcfeb62f832b0124c40c5415
Duy Truong <Duy.Truong@integritymarketing.com>
2023-09-28 13:18:37 -0500

    Fix zipcode issue

--- src/components/ui/plan-results.js
+++ src/components/ui/plan-results.js
@@ -37,7 +37,7 @@ const PlanResults = ({
     for (const plan of plans) {
       cards.push(
         <PlanCard
-          contact
+          contact={contact}
           key={key++}
           planData={plan}
           effectiveDate={effectiveDate}
