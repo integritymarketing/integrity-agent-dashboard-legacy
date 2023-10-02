343dc2e7e62c1524c3e8eb81ab5a7a99d4a00de2
vishalmarneni <vishal.marneni@integritymarketing.com>
2023-10-02 14:14:45 -0500

    ask integrity feedback with cleaned code

---
+++ src/components/WebChat/AskIntegrityInfoContainer/Components/assets/Cross.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Cross() {
+    return <svg id="IconCircleBG" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+    <circle id="Ellipse_299" data-name="Ellipse 299" cx="16" cy="16" r="16" fill="#375582"/>
+  </svg>  
+}
+
+export default Cross
