45577f02568f5210cc72b5eaa122c9c333ab6f3c
ims7inc <ugandharkurra1996@gmail.com>
2023-10-27 16:22:22 +0530

    Moved minimum version values to environment variables

--- src/hooks/useMobileVersionCheck.js
+++ src/hooks/useMobileVersionCheck.js
@@ -1,55 +1,47 @@
 import useDeviceInfo, { DEVICES } from "hooks/useDeviceInfo";
 import useQueryParams from "hooks/useQueryParams";
 
-const MIN_VERSIONS = {
-  ANDROID: "1.6.2",
-  IOS: "1.8.2",
-};
+const IOS_MINIMUM_APP_VERSION = process.env.REACT_APP_IOS_MINIMUM_APP_VERSION;
+const ANDROID_MINIMUM_APP_VERSION = process.env.REACT_APP_ANDROID_MINIMUM_APP_VERSION;
 
 // Function to get URL parameters by name
 function getURLParameter(name, url) {
-  const urlParams = new URLSearchParams(url);
-  return urlParams.get(name);
+    const urlParams = new URLSearchParams(url);
+    return urlParams.get(name);
 }
 
 function isAppVersionOlderThan(version, targetVersion) {
-  // Remove any number between parentheses from the end of version and targetVersion
-  version = version.replace(/\(\d+\)$/, '');
-  targetVersion = targetVersion.replace(/\(\d+\)$/, '');
+    // Remove any number between parentheses from the end of version and targetVersion
+    version = version.replace(/\(\d+\)$/, "");
+    targetVersion = targetVersion.replace(/\(\d+\)$/, "");
 
-  const versionParts = version.split(".").map(Number);
-  const targetVersionParts = targetVersion.split(".").map(Number);
+    const versionParts = version.split(".").map(Number);
+    const targetVersionParts = targetVersion.split(".").map(Number);
 
-  for (
-    let i = 0;
-    i < Math.min(versionParts.length, targetVersionParts.length);
-    i++
-  ) {
-    if (versionParts[i] < targetVersionParts[i]) {
-      return true; // The app version is older
-    } else if (versionParts[i] > targetVersionParts[i]) {
-      return false; // The app version is newer
+    for (let i = 0; i < Math.min(versionParts.length, targetVersionParts.length); i++) {
+        if (versionParts[i] < targetVersionParts[i]) {
+            return true; // The app version is older
+        } else if (versionParts[i] > targetVersionParts[i]) {
+            return false; // The app version is newer
+        }
     }
-  }
 
-  return versionParts.length < targetVersionParts.length;
+    return versionParts.length < targetVersionParts.length;
 }
 
 const useMobileVersionCheck = () => {
-  const device = useDeviceInfo();
-  const params = useQueryParams();
-  const url = new URL(params.get("ReturnUrl")).search;
-  const versionParam = getURLParameter("Version", url);
+    const device = useDeviceInfo();
+    const params = useQueryParams();
+    const url = new URL(params.get("ReturnUrl")).search;
+    const versionParam = getURLParameter("Version", url);
 
-  if (versionParam) {
-    const minVersion =
-      device === DEVICES.IOS || device === DEVICES.IPHONE
-        ? MIN_VERSIONS.IOS
-        : MIN_VERSIONS.ANDROID;
-    return isAppVersionOlderThan(versionParam, minVersion);
-  }
+    if (versionParam) {
+        const minVersion =
+            device === DEVICES.IOS || device === DEVICES.IPHONE ? IOS_MINIMUM_APP_VERSION : ANDROID_MINIMUM_APP_VERSION;
+        return isAppVersionOlderThan(versionParam, minVersion);
+    }
 
-  return true;
+    return true;
 };
 
 export default useMobileVersionCheck;
