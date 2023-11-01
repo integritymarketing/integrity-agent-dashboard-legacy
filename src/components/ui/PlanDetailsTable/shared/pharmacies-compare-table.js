2ad4cd79a36f1835104e128a31ac5f817e34c6b4
vinodkumar <39404479+vinodkurra@users.noreply.github.com>
2023-11-01 23:38:50 +0530

    update in in networks code (#1852)

--- src/components/ui/PlanDetailsTable/shared/pharmacies-compare-table.js
+++ src/components/ui/PlanDetailsTable/shared/pharmacies-compare-table.js
@@ -5,115 +5,106 @@ import InNetworkCheck from "../../../icons/in-network-check";
 import APIFail from "./APIFail/index";
 
 export function PharmaciesCompareTable({ plans, pharmacies, apiError }) {
-  const clonedPlans = useMemo(() => {
-    const copyPlans = [...plans];
-    if (plans.length < 3) {
-      copyPlans.push(null);
-    }
-    return copyPlans;
-  }, [plans]);
+    const clonedPlans = useMemo(() => {
+        const copyPlans = [...plans];
+        if (plans.length < 3) {
+            copyPlans.push(null);
+        }
+        return copyPlans;
+    }, [plans]);
 
-  const isEmpty =
-    (pharmacies?.filter((pharmacy) => pharmacy?.name)?.length > 0
-      ? false
-      : true) &&
-    pharmacies !== null &&
-    pharmacies?.length > 0;
-  const columns = useMemo(
-    () => [
-      {
-        Header: "Pharmacies",
-        columns: [
-          {
-            hideHeader: true,
-            accessor: "name",
-            Cell({ value }, original) {
-              return (
-                <div>
-                  <span>{value}</span>
-                  <div>{original.address}</div>
-                </div>
-              );
+    const isEmpty =
+        (pharmacies?.filter((pharmacy) => pharmacy?.name)?.length > 0 ? false : true) &&
+        pharmacies !== null &&
+        pharmacies?.length > 0;
+    const columns = useMemo(
+        () => [
+            {
+                Header: "Pharmacies",
+                columns: [
+                    {
+                        hideHeader: true,
+                        accessor: "name",
+                        Cell({ value }, original) {
+                            return (
+                                <div>
+                                    <span>{value}</span>
+                                    <div>{original.address}</div>
+                                </div>
+                            );
+                        },
+                    },
+                    ...clonedPlans.map((plan, index) => ({
+                        hideHeader: true,
+                        accessor: `plan-${index}`,
+                        Cell({ value }) {
+                            if (!plan) {
+                                return "-";
+                            }
+                            return value ? (
+                                <span className="pr-network">
+                                    <InNetworkCheck /> <span className="pr-network-text">In Network</span>
+                                </span>
+                            ) : (
+                                <span className="pr-network">
+                                    <OutNetworkX />
+                                    <span className="pr-network-text">Not In Network</span>
+                                </span>
+                            );
+                        },
+                    })),
+                ],
             },
-          },
-          ...clonedPlans.map((plan, index) => ({
-            hideHeader: true,
-            accessor: `plan-${index}`,
-            Cell({ value }) {
-              if (!plan) {
-                return "-";
-              }
-              return value ? (
-                <span className="pr-network">
-                  <InNetworkCheck />{" "}
-                  <span className="pr-network-text">In Network</span>
-                </span>
-              ) : (
-                <span>
-                  <OutNetworkX />
-                  <span className="pr-network-text">Not In Network</span>
-                </span>
-              );
-            },
-          })),
         ],
-      },
-    ],
-    [clonedPlans]
-  );
-  const data = pharmacies.map((document) => ({
-    name: <span className="label">{document.name}</span>,
-    address: document.address,
-    [`plan-0`]: !!plans[0]?.pharmacyCosts?.find(
-      (pr) => pr.pharmacyID === document.pharmacyID
-    ),
-    [`plan-1`]: !!plans[1]?.pharmacyCosts?.find(
-      (pr) => pr.pharmacyID === document.pharmacyID
-    ),
-    [`plan-2`]: !!plans[2]?.pharmacyCosts?.find(
-      (pr) => pr.pharmacyID === document.pharmacyID
-    ),
-  }));
-  const columnsData = [
-    {
-      Header: "Pharmacies",
-      columns: [
+        [clonedPlans]
+    );
+    const data = pharmacies.map((document) => ({
+        name: <span className="label">{document.name}</span>,
+        address: document.address,
+        [`plan-0`]: !!plans[0]?.pharmacyCosts?.find((pr) => pr.isNetwork),
+        [`plan-1`]: !!plans[1]?.pharmacyCosts?.find((pr) => pr.isNetwork),
+        [`plan-2`]: !!plans[2]?.pharmacyCosts?.find((pr) => pr.isNetwork),
+    }));
+    const columnsData = [
         {
-          hideHeader: true,
-          accessor: "unAvailable",
+            Header: "Pharmacies",
+            columns: [
+                {
+                    hideHeader: true,
+                    accessor: "unAvailable",
+                },
+            ],
         },
-      ],
-    },
-  ];
+    ];
 
-  const emptyColumnsData = [
-    {
-      Header: "Pharmacies",
-      columns: [
+    const emptyColumnsData = [
         {
-          hideHeader: true,
-          accessor: "empty",
+            Header: "Pharmacies",
+            columns: [
+                {
+                    hideHeader: true,
+                    accessor: "empty",
+                },
+            ],
         },
-      ],
-    },
-  ];
+    ];
 
-  const rowData = [
-    {
-      unAvailable: <APIFail title={"Pharmacy"} />,
-    },
-  ];
+    const rowData = [
+        {
+            unAvailable: <APIFail title={"Pharmacy"} />,
+        },
+    ];
 
-  const emptyRowData = [];
+    const emptyRowData = [];
 
-  return (
-    <>
-      <PlanDetailsTableWithCollapse
-        columns={apiError ? columnsData : isEmpty ? emptyColumnsData : columns}
-        data={apiError ? rowData : isEmpty ? emptyRowData : data}
-        compareTable={true}
-        header={"Pharmacies"}
-      />
-    </>
-  );
+    return (
+        <>
+            <PlanDetailsTableWithCollapse
+                columns={apiError ? columnsData : isEmpty ? emptyColumnsData : columns}
+                data={apiError ? rowData : isEmpty ? emptyRowData : data}
+                compareTable={true}
+                header={"Pharmacies"}
+            />
+        </>
+    );
 }
