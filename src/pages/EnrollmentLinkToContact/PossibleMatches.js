4bd885234ffbbdcb1c1bb4b43cae83e343ecd40e
Duy Truong <50502501+duytruong0916@users.noreply.github.com>
2023-11-02 16:12:12 -0500

    fix  Policy SnapShot Link/Relinking page is broken (#1854)

--- src/pages/EnrollmentLinkToContact/PossibleMatches.js
+++ src/pages/EnrollmentLinkToContact/PossibleMatches.js
@@ -1,138 +1,126 @@
-import React, { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import * as Sentry from "@sentry/react";
+import styles from "./styles.module.scss";
 import clientsService from "services/clientsService";
 import enrollPlansService from "services/enrollPlansService";
 import useToast from "hooks/useToast";
-import styles from "./styles.module.scss";
 
-export default function PossibleMatches({
-  phone, policyHolder, state
-}) {
+// eslint-disable-next-line max-lines-per-function
+export default function PossibleMatches({ phone, policyHolder, state }) {
+    const [matches, setMatches] = useState([]);
+    const { callFrom } = useParams();
+    const navigate = useNavigate();
+    const showToast = useToast();
 
-  const [matches, setMatches] = useState([]);
-  const { callFrom } = useParams();
-  const navigate = useNavigate();
-  const showToast = useToast();
-
-  useEffect(() => {
-    const getContacts = async () => {
-      try {
-        const response = await clientsService.getList(
-          1,
-          25,
-          ["Activities.CreateDate:desc"],
-          policyHolder
-        );
-        if (response && response?.result.length > 0) {
-          const sorted = response?.result?.sort((a, b) =>
-            a.firstName.localeCompare(b.firstName)
-          );
-          setMatches(sorted);
-        }
-      } catch (error) {
-        Sentry.captureException(error);
-      }
-    };
-    getContacts();
-  }, [phone, policyHolder, state]);
-
-
-  const updatePrimaryContact = useCallback(
-    (contact) => {
-      return clientsService.updateLeadPhone(contact, callFrom);
-    },
-    [callFrom]
-  );
-
-  const onClickHandler = useCallback(
-    async (contact) => {
-      try {
-        const hasPhone = contact?.phones?.slice().reverse()[0]?.leadPhone;
-        if (!hasPhone) {
-          await updatePrimaryContact(contact);
-        }
-
-        const {
-          policyEffectiveDate,
-          planId,
-          submittedDate,
-          policyId,
-          agentNpn,
-          carrier,
-          consumerSource,
-          policyStatus,
-          hasPlanDetails,
-          confirmationNumber,
-          policyHolder,
-          sourceId,
-          linkingType,
-        } = state;
-
-        const leadDate = contact.emails[0]?.createDate;
-
-        const updateBusinessBookPayload = {
-          agentNpn,
-          leadId: contact?.leadsId?.toString(),
-          policyNumber: policyId,
-          plan: planId,
-          carrier,
-          policyStatus,
-          consumerSource,
-          confirmationNumber,
-          policyHolder,
-          policyEffectiveDate,
-          appSubmitDate: submittedDate,
-          hasPlanDetails,
-          sourceId,
-          leadDate,
-          leadStatus: "",
-          linkingType,
+    useEffect(() => {
+        const getContacts = async () => {
+            try {
+                const response = await clientsService.getList(1, 25, ["Activities.CreateDate:desc"], policyHolder);
+                if (response && response?.result.length > 0) {
+                    const sorted = response?.result?.sort((a, b) => a.firstName.localeCompare(b.firstName));
+                    setMatches(sorted);
+                }
+            } catch (error) {
+                Sentry.captureException(error);
+            }
         };
+        getContacts();
+    }, [phone, policyHolder, state]);
 
-        const response = await enrollPlansService.updateBookOfBusiness(
-          updateBusinessBookPayload
-        );
-
-        if (response) {
-          showToast({
-            message: "Contact linked successfully",
-          });
-          navigate(`/contact/${contact.leadsId}`);
-        } else {
-          showToast({
-            type: "error",
-            message: `${response}`,
-          });
-        }
-      } catch (error) {
-        showToast({
-          type: "error",
-          message: `${error.message}`,
-        });
-      }
-    },
-    [navigate, showToast, updatePrimaryContact, state]
-  );
-
-  if (matches?.length > 0) {
-    return (
-      <div className={styles.possibleMatch}>
-        <div className={styles.title}>Possible Matches</div>
-        <div className={styles.matchList}>
-          {matches.map((contact, index) => (
-            <div
-              className={styles.matchItem}
-              key={`matchItem-${index}`}
-              onClick={() => onClickHandler(contact)}
-            >
-              {`${contact?.firstName} ${contact.lastName}`}
-            </div>
-          ))}
-        </div>
-      </div>
+    const updatePrimaryContact = useCallback(
+        (contact) => {
+            return clientsService.updateLeadPhone(contact, callFrom);
+        },
+        [callFrom]
     );
-  } else {
-    return null;
-  }
+
+    const onClickHandler = useCallback(
+        async (contact) => {
+            try {
+                const hasPhone = contact?.phones?.slice().reverse()[0]?.leadPhone;
+                if (!hasPhone) {
+                    await updatePrimaryContact(contact);
+                }
+
+                const {
+                    policyEffectiveDate,
+                    planId,
+                    submittedDate,
+                    policyId,
+                    agentNpn,
+                    carrier,
+                    consumerSource,
+                    policyStatus,
+                    hasPlanDetails,
+                    confirmationNumber,
+                    policyHolder,
+                    sourceId,
+                    linkingType,
+                } = state;
+
+                const leadDate = contact.emails[0]?.createDate;
+
+                const updateBusinessBookPayload = {
+                    agentNpn,
+                    leadId: contact?.leadsId?.toString(),
+                    policyNumber: policyId,
+                    plan: planId,
+                    carrier,
+                    policyStatus,
+                    consumerSource,
+                    confirmationNumber,
+                    policyHolder,
+                    policyEffectiveDate,
+                    appSubmitDate: submittedDate,
+                    hasPlanDetails,
+                    sourceId,
+                    leadDate,
+                    leadStatus: "",
+                    linkingType,
+                };
+
+                const response = await enrollPlansService.updateBookOfBusiness(updateBusinessBookPayload);
+
+                if (response) {
+                    showToast({
+                        message: "Contact linked successfully",
+                    });
+                    navigate(`/contact/${contact.leadsId}`);
+                } else {
+                    showToast({
+                        type: "error",
+                        message: `${response}`,
+                    });
+                }
+            } catch (error) {
+                showToast({
+                    type: "error",
+                    message: `${error.message}`,
+                });
+            }
+        },
+        [navigate, showToast, updatePrimaryContact, state]
+    );
+
+    if (matches?.length > 0) {
+        return (
+            <div className={styles.possibleMatch}>
+                <div className={styles.title}>Possible Matches</div>
+                <div className={styles.matchList}>
+                    {matches.map((contact, index) => (
+                        <div
+                            className={styles.matchItem}
+                            key={`matchItem-${index}`}
+                            onClick={() => onClickHandler(contact)}
+                        >
+                            {`${contact?.firstName} ${contact.lastName}`}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    } else {
+        return null;
+    }
 }
