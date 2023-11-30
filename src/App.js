2affe6bdc48614fa3a15a7fbae26063a9c61ccef
Duy Truong <50502501+duytruong0916@users.noreply.github.com>
2023-11-30 10:51:32 -0600

    Remove Duplicate contacts filter when agent navigates to different page (#1924)

--- src/App.js
+++ src/App.js
@@ -1,59 +1,58 @@
-import React, { lazy } from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { lazy } from "react";
 import Media from "react-media";
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import {
-  UnProtectedRoute,
-  ProtectedRoute,
-} from "components/functional/auth-routes";
-import { appRoutes, appProtectedRoutes } from "routeConfigs/AppRouteConfig";
+import { appProtectedRoutes, appRoutes } from "routeConfigs/AppRouteConfig";
+
+import useRemoveDuplicateIdsOnRouteChange from "hooks/useRemoveDuplicateIdsOnRouteChange";
+
+import { ProtectedRoute, UnProtectedRoute } from "components/functional/auth-routes";
 
 const LandingPage = lazy(() => import("mobile/landing/LandingPage"));
 const MaintenancePage = lazy(() => import("pages/MaintenancePage"));
 const Welcome = lazy(() => import("pages/welcome"));
 
 const App = () => {
-  const isMaintainanceMode = process.env.REACT_APP_MAINTENANCE_MODE;
+    useRemoveDuplicateIdsOnRouteChange();
+    const isMaintainanceMode = process.env.REACT_APP_MAINTENANCE_MODE;
 
-  if (isMaintainanceMode) {
+    if (isMaintainanceMode) {
+        return (
+            <Routes>
+                <Route path="/maintenance" element={<MaintenancePage />} />
+                <Route path="*" element={<Navigate to="/maintenance" />} />
+            </Routes>
+        );
+    }
+
     return (
-      <Routes>
-        <Route path="/maintenance" element={<MaintenancePage />} />
-        <Route path="*" element={<Navigate to="/maintenance" />} />
-      </Routes>
+        <Routes>
+            <Route
+                path="/welcome"
+                element={
+                    <UnProtectedRoute redirectPath="/">
+                        <Media
+                            queries={{
+                                small: "(max-width: 767px)",
+                            }}
+                        >
+                            {(matches) => (matches.small ? <LandingPage /> : <Welcome />)}
+                        </Media>
+                    </UnProtectedRoute>
+                }
+            />
+            {appProtectedRoutes.map((route) => (
+                <Route
+                    key={route.path}
+                    path={route.path}
+                    element={<ProtectedRoute redirectPath="/">{route.component}</ProtectedRoute>}
+                />
+            ))}
+            {appRoutes.map((route) => (
+                <Route key={route.path} path={route.path} element={route.component} />
+            ))}
+        </Routes>
     );
-  }
-
-  return (
-    <Routes>
-      <Route
-        path="/welcome"
-        element={
-          <UnProtectedRoute redirectPath="/">
-            <Media
-              queries={{
-                small: "(max-width: 767px)",
-              }}
-            >
-              {(matches) => (matches.small ? <LandingPage /> : <Welcome />)}
-            </Media>
-          </UnProtectedRoute>
-        }
-      />
-      {appProtectedRoutes.map((route) => (
-        <Route
-          key={route.path}
-          path={route.path}
-          element={
-            <ProtectedRoute redirectPath="/">{route.component}</ProtectedRoute>
-          }
-        />
-      ))}
-      {appRoutes.map((route) => (
-        <Route key={route.path} path={route.path} element={route.component} />
-      ))}
-    </Routes>
-  );
 };
 
 export default App;
