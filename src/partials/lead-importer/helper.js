a3b5b5aa3415b75a5ac7499d3cad86aad1cf88bc
Duy Truong <50502501+duytruong0916@users.noreply.github.com>
2023-12-04 09:58:07 -0600

    Add Health Information fields to Client Import (#1934)

--- src/partials/lead-importer/helper.js
+++ src/partials/lead-importer/helper.js
@@ -1,18 +1,21 @@
+import moment from "moment";
+
 import { formatPhoneNumber } from "utils/phones";
 
-export const getFormattedData = ({ phone, isTobaccoUser, height, weight, ...data }) => {
+export const getFormattedData = ({ phone, isTobaccoUser, height, weight, partB, partA, birthdate, ...data }) => {
     const formattedData = {
         ...data,
-        // middleName: "string",
-        // leadSource: "string",
-        // leadSourceName: "string",
-        // guid: "string",
         phone: String(formatPhoneNumber(phone)).replace(/\D/g, ""),
         ...(isTobaccoUser !== null && isTobaccoUser !== undefined
             ? { isTobaccoUser: isTobaccoUser.toLowerCase() === "yes" }
             : {}),
         ...(height !== null && height !== undefined ? { height: Number(height) } : {}),
         ...(weight !== null && weight !== undefined ? { weight: Number(weight) } : {}),
+        ...(birthdate !== null && birthdate !== undefined
+            ? { birthdate: moment(birthdate, "MM/DD/YYYY").toDate() }
+            : {}),
+        ...(partA !== null && partA !== undefined ? { partA: moment(partA, "MM/DD/YYYY").toDate() } : {}),
+        ...(partB !== null && partB !== undefined ? { partB: moment(partB, "MM/DD/YYYY").toDate() } : {}),
     };
 
     return formattedData;
