e87feb9168beab17bbca15ffeaf295c6f0e0e3a3
vishalmarneni <vishal.marneni@integritymarketing.com>
2023-12-09 13:51:17 -0600

    fix: client notes bugs

--- src/components/ContactDetailsContainer/OverviewContainer/ClientNotesContainer/ClientNotes.js
+++ src/components/ContactDetailsContainer/OverviewContainer/ClientNotesContainer/ClientNotes.js
@@ -1,99 +1,80 @@
-import React, { useState, useEffect } from "react";
-import Box from "@mui/material/Box";
-import { useLeadDetails } from "providers/ContactDetails";
-import ContactSectionCard from "packages/ContactSectionCard";
-import { Button } from "components/ui/Button";
-import EditIcon from "components/icons/icon-edit";
-import { TextField } from "@mui/material";
-
-import styles from "./ClientNotes.module.scss";
+import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import ContactSectionCard from 'packages/ContactSectionCard';
+import { Button } from 'components/ui/Button';
+import EditIcon from 'components/icons/icon-edit';
+import styles from './ClientNotes.module.scss';
+import { useLeadDetails } from 'providers/ContactDetails';
 
 export const ClientNotes = () => {
+  const { leadDetails, updateClientNotes } = useLeadDetails();
+  const [notes, setNotes] = useState('');
+  const [isEditing, setIsEditing] = useState(false);
 
-    const { leadDetails, updateClientNotes } = useLeadDetails();
-    const [notes, setNotes] = useState("");
-    const [isEdit, setIsEdit] = useState(false);
-
-    useEffect(() => {
-        setNotes(leadDetails?.notes || "");
-    }, [leadDetails]);
-
-    const saveNotes = () => {
-        const payload = {
-            ...leadDetails,
-            notes: notes,
-            primaryContact: "phone",
-        }
-
-        updateClientNotes(leadDetails, payload);
-        setIsEdit(false);
+  useEffect(() => {
+    if (leadDetails) {
+      setNotes(leadDetails.notes || '');
     }
+  }, [leadDetails]);
 
-    return (
-        <>
-            <ContactSectionCard
-                title="Client Notes"
-                className={styles.clientNotesContainer}
-                isDashboard
-                contentClassName={styles.clientNotesContainer_content}
+  const handleSaveNotes = () => {
+    const updatedDetails = { ...leadDetails, notes, primaryContact: 'phone' };
+    updateClientNotes(leadDetails, updatedDetails);
+    setIsEditing(false);
+  };
 
-            >
-                <Box className={styles.noteInputContainer}>
-
-                    <Box width="80%">
-                        <TextField
-                            id="outlined-basic"
-                            placeholder="Add a note"
-                            variant="outlined"
-                            value={notes}
-                            fullWidth
-                            onChange={(e) => setNotes(e.target.value || "")}
-                            multiline
-                            rows={3}
-                            className={styles.notes}
-                            disabled={!isEdit}
-                        />
-                    </Box>
-                    <Box width="20%" className={styles.buttonContainer}>
-                        {!isEdit ? (
-                            <Button
-                                icon={<EditIcon color="#ffffff" />}
-                                label={"Edit"}
-                                className={styles.editButton}
-                                onClick={() => setIsEdit(true)}
-                                type="tertiary"
-                                iconPosition="right"
-                            />) : (
-                            <>
-
-                                <Button
-                                    label={"Cancel"}
-                                    className={styles.deleteButton}
-                                    type="tertiary"
-                                    onClick={() => setIsEdit(false)}
-                                />
-                                <Button
-                                    label={"Save"}
-                                    className={styles.editButton}
-                                    onClick={saveNotes}
-                                    type="tertiary"
-                                    disabled={!notes}
-                                />
-
-                            </>
-                        )}
-
-
-
-
-                    </Box>
-
-                </Box>
-            </ContactSectionCard>
-
-        </>
-    );
+  return (
+    <ContactSectionCard
+      title="Client Notes"
+      className={styles.clientNotesContainer}
+      isDashboard
+      contentClassName={styles.clientNotesContainer_content}
+    >
+      <Box className={styles.noteInputContainer}>
+        <Box width="80%">
+          <TextField
+            id="outlined-basic"
+            placeholder="Add a note"
+            variant="outlined"
+            value={notes}
+            fullWidth
+            onChange={(e) => setNotes(e.target.value || '')}
+            multiline
+            rows={3}
+            className={styles.notes}
+            disabled={!isEditing}
+          />
+        </Box>
+        <Box width="20%" className={styles.buttonContainer}>
+          {isEditing ? (
+            <>
+              <Button
+                label="Cancel"
+                className={styles.deleteButton}
+                type="tertiary"
+                onClick={() => setIsEditing(false)}
+              />
+              <Button
+                label="Save"
+                className={styles.editButton}
+                onClick={handleSaveNotes}
+                type="tertiary"
+                disabled={!notes}
+              />
+            </>
+          ) : (
+            <Button
+              icon={<EditIcon color="#ffffff" />}
+              label="Edit"
+              className={styles.editButton}
+              onClick={() => setIsEditing(true)}
+              type="tertiary"
+              iconPosition="right"
+            />
+          )}
+        </Box>
+      </Box>
+    </ContactSectionCard>
+  );
 };
-
-
-
--- src/providers/ContactDetails/LeadDetailsProvider.js
+++ src/providers/ContactDetails/LeadDetailsProvider.js
@@ -6,178 +6,196 @@ import useToast from "hooks/useToast";
 import { getFormattedData } from "utilities/formatData";
 import { parseDate, formatServerDate } from "utils/dates";
 
-
 const flattenMBI = (mbi) => {
-    if (!mbi) return null;
-    return mbi.replace(/-/g, "");
+  if (!mbi) return null;
+  return mbi.replace(/-/g, "");
 };
 
 const getFormattedPhone = (phone) =>
-    phone ? ("" + phone).replace(/\D/g, "") : null;
+  phone ? ("" + phone).replace(/\D/g, "") : null;
 
 export const LeadDetailsContext = createContext();
 
 export const LeadDetailsProvider = ({ children }) => {
-    const leadsApiUrl = `${process.env.REACT_APP_LEADS_URL}/api/v2.0/Leads`;
+  const leadsApiUrl = `${process.env.REACT_APP_LEADS_URL}/api/v2.0/Leads`;
 
-    const { Get: fetchLeadDetails, loading: isLoadingLeadDetails, error: leadDetailsError, Put: editLeadDetails, Delete: deleteContact } = useFetch(leadsApiUrl);
+  const {
+    Get: fetchLeadDetails,
+    loading: isLoadingLeadDetails,
+    error: leadDetailsError,
+    Put: editLeadDetails,
+    Delete: deleteContact,
+  } = useFetch(leadsApiUrl);
 
-    const showToast = useToast();
+  const showToast = useToast();
 
-    // selectedTab  state can be one of ["overview", "scopeOfAppointment", "policies", "health"]
-    const [selectedTab, setSelectedTab] = useState("overview");
-    const [leadDetails, setLeadDetails] = useState(null);
-    const [leadDetailsloading, setLeadDetailsloading] = useState(false);
+  // selectedTab  state can be one of ["overview", "scopeOfAppointment", "policies", "health"]
+  const [selectedTab, setSelectedTab] = useState("overview");
+  const [leadDetails, setLeadDetails] = useState(null);
+  const [leadDetailsloading, setLeadDetailsloading] = useState(false);
 
-    const getLeadDetails = useCallback(
-        async (leadId) => {
-            try {
-                const data = await fetchLeadDetails(null, false, leadId);
-                if (data) setLeadDetails(data);
-                else setLeadDetails(null);
-            } catch (error) {
-                console.error("Error fetching Lead details:", error);
-                // Handle error appropriately here
-            }
+  const getLeadDetails = useCallback(
+    async (leadId) => {
+      try {
+        const data = await fetchLeadDetails(null, false, leadId);
+        if (data) setLeadDetails(data);
+        else setLeadDetails(null);
+      } catch (error) {
+        console.error("Error fetching Lead details:", error);
+        // Handle error appropriately here
+      }
+    },
+    [fetchLeadDetails]
+  );
+
+  const updateLeadDetails = async (newPayload) => {
+    const {
+      firstName,
+      middleName,
+      lastName,
+      birthdate,
+      email,
+      phones,
+      address,
+      primaryCommunication,
+      contactRecordType,
+      leadsId,
+      leadStatusId,
+      emailID,
+      phoneId,
+      leadAddressId,
+      notes,
+      medicareBeneficiaryID,
+      partA,
+      partB,
+    } = newPayload;
+    const reqData = {
+      leadsId,
+      firstName,
+      middleName: middleName?.toUpperCase(),
+      lastName,
+      birthdate: birthdate ? formatServerDate(parseDate(birthdate)) : null,
+      leadStatusId,
+      primaryCommunication,
+      contactRecordType,
+      notes,
+    };
+    if (medicareBeneficiaryID) {
+      reqData.medicareBeneficiaryID = flattenMBI(medicareBeneficiaryID);
+    }
+    if (partA) {
+      reqData.partA = formatServerDate(partA);
+    }
+    if (partB) {
+      reqData.partB = formatServerDate(partB);
+    }
+    reqData.emails = [];
+    if (email !== null && email !== undefined) {
+      reqData.emails = [
+        {
+          emailID: emailID,
+          leadEmail: email,
         },
-        [fetchLeadDetails]
+      ];
+    }
+
+    reqData.phones = [
+      {
+        phoneId: phoneId,
+        ...phones,
+        leadPhone: getFormattedPhone(phones.leadPhone),
+      },
+    ];
+
+    reqData.addresses = [
+      {
+        leadAddressId: leadAddressId,
+        ...address,
+      },
+    ];
+    await performAsyncOperation(
+      () => editLeadDetails(reqData, false, newPayload.leadsId),
+      setLeadDetailsloading,
+      async () => {
+        await getLeadDetails(newPayload?.leadsId);
+        showToast({
+          message: `Lead updated successfully`,
+        });
+      },
+      (err) =>
+        showToast({
+          type: "error",
+          message: `Failed to update lead`,
+        })
     );
+  };
 
-    const updateLeadDetails = async (newPayload) => {
-        const {
-            firstName,
-            middleName,
-            lastName,
-            birthdate,
-            email,
-            phones,
-            address,
-            primaryCommunication,
-            contactRecordType,
-            leadsId,
-            leadStatusId,
-            emailID,
-            phoneId,
-            leadAddressId,
-            notes,
-            medicareBeneficiaryID,
-            partA,
-            partB,
-        } = newPayload;
-        const reqData = {
-            leadsId,
-            firstName,
-            middleName: middleName?.toUpperCase(),
-            lastName,
-            birthdate: birthdate ? formatServerDate(parseDate(birthdate)) : null,
-            leadStatusId,
-            primaryCommunication,
-            contactRecordType,
-            notes,
-        };
-        if (medicareBeneficiaryID) {
-            reqData.medicareBeneficiaryID = flattenMBI(medicareBeneficiaryID);
-        }
-        if (partA) {
-            reqData.partA = formatServerDate(partA);
-        }
-        if (partB) {
-            reqData.partB = formatServerDate(partB);
-        }
-        reqData.emails = [];
-        if (email !== null && email !== undefined) {
-            reqData.emails = [
-                {
-                    emailID: emailID,
-                    leadEmail: email,
-                },
-            ];
-        }
-
-        reqData.phones = [
-            {
-                phoneId: phoneId,
-                ...phones,
-                leadPhone: getFormattedPhone(phones.leadPhone),
-            },
-        ];
-
-        reqData.addresses = [
-            {
-                leadAddressId: leadAddressId,
-                ...address,
-            },
-        ];
-        await performAsyncOperation(
-            () => editLeadDetails(reqData, false, newPayload.leadsId),
-            setLeadDetailsloading,
-            async () => {
-                await getLeadDetails(newPayload?.leadsId);
-                showToast({
-                    message: `Lead updated successfully`,
-                });
-            },
-            (err) =>
-                showToast({
-                    type: "error",
-                    message: `Failed to update lead`,
-                })
-        );
-    };
-
-
-    const updateClientNotes = async (oldPayload, newPayload) => {
-        const formattedData = getFormattedData(newPayload, oldPayload);
-        await performAsyncOperation(
-            () => editLeadDetails(formattedData, false, newPayload.leadsId),
-            setLeadDetailsloading,
-            async () => {
-                await getLeadDetails(newPayload?.leadsId);
-                showToast({
-                    message: `Lead updated successfully`,
-                });
-            },
-            (err) =>
-                showToast({
-                    type: "error",
-                    message: `Failed to update lead`,
-                })
-        );
-    };
-
-    const removeContact = async (leadId, callBack) => {
-        await performAsyncOperation(
-            () => deleteContact(null, false, leadId),
-            setLeadDetailsloading,
-            () => {
-                callBack();
-            },
-            (err) =>
-                showToast({
-                    type: "error",
-                    message: `Failed to delete lead`,
-                })
-        );
-    };
-
-    const contextValue = useMemo(
-        () => ({
-            getLeadDetails,
-            leadDetails,
-            leadDetailsError,
-            isLoadingLeadDetails,
-            selectedTab,
-            setSelectedTab,
-            updateLeadDetails,
-            removeContact,
-            updateClientNotes
-        }),
-        [getLeadDetails, leadDetails, leadDetailsError, isLoadingLeadDetails, selectedTab, setSelectedTab, removeContact, updateLeadDetails, updateClientNotes]
+  const updateClientNotes = async (oldPayload, newPayload) => {
+    const formattedData = getFormattedData(newPayload, oldPayload);
+    await performAsyncOperation(
+      () => editLeadDetails(formattedData, false, newPayload.leadsId),
+      setLeadDetailsloading,
+      async () => {
+        await getLeadDetails(newPayload?.leadsId);
+        showToast({
+          message: `Client notes successfully Updated.                      `,
+        });
+      },
+      (err) =>
+        showToast({
+          type: "error",
+          message: `Failed to update lead`,
+        })
     );
+  };
 
-    return <LeadDetailsContext.Provider value={contextValue}>{children}</LeadDetailsContext.Provider>;
+  const removeContact = async (leadId, callBack) => {
+    await performAsyncOperation(
+      () => deleteContact(null, false, leadId),
+      setLeadDetailsloading,
+      () => {
+        callBack();
+      },
+      (err) =>
+        showToast({
+          type: "error",
+          message: `Failed to delete lead`,
+        })
+    );
+  };
+
+  const contextValue = useMemo(
+    () => ({
+      getLeadDetails,
+      leadDetails,
+      leadDetailsError,
+      isLoadingLeadDetails,
+      selectedTab,
+      setSelectedTab,
+      updateLeadDetails,
+      removeContact,
+      updateClientNotes,
+    }),
+    [
+      getLeadDetails,
+      leadDetails,
+      leadDetailsError,
+      isLoadingLeadDetails,
+      selectedTab,
+      setSelectedTab,
+      removeContact,
+      updateLeadDetails,
+      updateClientNotes,
+    ]
+  );
+
+  return (
+    <LeadDetailsContext.Provider value={contextValue}>
+      {children}
+    </LeadDetailsContext.Provider>
+  );
 };
 
 LeadDetailsProvider.propTypes = {
-    children: PropTypes.node.isRequired, // PropTypes.node for validating React node types
+  children: PropTypes.node.isRequired, // PropTypes.node for validating React node types
 };
