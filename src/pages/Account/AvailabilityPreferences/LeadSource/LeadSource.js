95d670adf3f59ffef4573180241124a70300a579
Duy Truong <50502501+duytruong0916@users.noreply.github.com>
2024-01-16 15:17:38 -0600

    Available toggle issue fix (#2075)

--- src/pages/Account/AvailabilityPreferences/LeadSource/LeadSource.js
+++ src/pages/Account/AvailabilityPreferences/LeadSource/LeadSource.js
@@ -1,17 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Box from "@mui/material/Box";
+
+import { useAgentAccountContext } from "providers/AgentAccountProvider";
+
+import { useAgentAvailability } from "hooks/useAgentAvailability";
+import useUserProfile from "hooks/useUserProfile";
 
 import Dialog from "packages/Dialog";
 
 import { HealthLead } from "./HealthLead";
 import { LifeLead } from "./LifeLead";
 import { PlanEnroll } from "./PlanEnroll";
+import clientsService from "services/clientsService";
 
 import styles from "./styles.module.scss";
 
 function LeadSource() {
     const [openDialog, setOpenDialog] = useState(false);
+    const { agentId } = useUserProfile();
+    const { leadPreference, agentAvailability } = useAgentAccountContext();
+    const [isAvailable, setIsAvailable] = useAgentAvailability();
+
+    useEffect(() => {
+        const hasActiveLifeCallCampaign = agentAvailability?.activeCampaign?.hasActiveLifeCallCampaign;
+        const hasActiveHealthCallCampaign = agentAvailability?.activeCampaign?.hasActiveHealthCallCampaign;
+
+        const isLifeCheck = leadPreference?.leadCenterLife && hasActiveLifeCallCampaign;
+        const isHealthChecked = hasActiveHealthCallCampaign ? leadPreference?.leadCenter : false;
+        const isPlanEnrollChecked = leadPreference?.medicareEnrollPurl;
+        const shouldChangeStatus = !isLifeCheck && !isHealthChecked && !isPlanEnrollChecked;
+
+        if (shouldChangeStatus && isAvailable) {
+            setIsAvailable(false);
+            clientsService.updateAgentAvailability({
+                agentID: agentId,
+                availability: false,
+            });
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <Box className={styles.leadCenter}>
