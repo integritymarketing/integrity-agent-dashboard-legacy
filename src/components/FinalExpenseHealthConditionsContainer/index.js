c2bec06d3ff5948b84a71bdfd6a5575dae3b1e2c
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-01-20 11:22:33 -0600

    fix: import of useffect

--- src/components/FinalExpenseHealthConditionsContainer/index.js
+++ src/components/FinalExpenseHealthConditionsContainer/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useEffect, useNavigate, useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 import useAnalytics from "hooks/useAnalytics";
 
