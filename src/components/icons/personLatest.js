b093756f904666f2cd32e625a6d61d871367e0a4
duytruong0916 <duytruong169@gmail.com>
2024-01-28 15:19:02 -0600

    Update contact person icon

--- src/components/icons/personLatest.js
+++ src/components/icons/personLatest.js
@@ -1,4 +1,4 @@
-const PersonLatestIcon = () => {
+const PersonLatestIcon = ({ color = "#ffffff" }) => {
     return (
         <svg id="Contacts" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <g id="Button_-_BG" data-name="Button - BG">
@@ -11,7 +11,7 @@ const PersonLatestIcon = () => {
                     d="M17.655,4.617a2.623,2.623,0,0,1-5.247,0V3.12a2.623,2.623,0,0,1,5.247,0Z"
                     transform="translate(-4.214 -0.25)"
                     fill="none"
-                    stroke="#4178ff"
+                    stroke={color}
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="1"
@@ -22,7 +22,7 @@ const PersonLatestIcon = () => {
                     d="M14.412,11.5h0a6.917,6.917,0,0,0-6.926,5.779.859.859,0,0,0,.874.957h12.1a.859.859,0,0,0,.874-.957A6.917,6.917,0,0,0,14.412,11.5Z"
                     transform="translate(-3.595 -1.987)"
                     fill="none"
-                    stroke="#4178ff"
+                    stroke={color}
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="1"
