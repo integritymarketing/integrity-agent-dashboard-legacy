7a20df75921dbc35af5e022f54191a9a67e142f5
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-02-05 15:45:20 -0600

    fix: legacy safeguard url

--- src/components/ContactDetailsContainer/OverviewContainer/LegacySafeGuard/index.js
+++ src/components/ContactDetailsContainer/OverviewContainer/LegacySafeGuard/index.js
@@ -1,11 +1,11 @@
 import React, { useMemo } from "react";
 import PropTypes from "prop-types";
+import moment from 'moment';
 import Box from "@mui/material/Box";
 import { Button } from "components/ui/Button";
 import styles from "./styles.module.scss";
 import LegacySafeGuardCardImage from "images/LegacySafeGuardCard.png";
 import { Arrow } from "../Icons";
-import { formatDate } from "utils/dates";
 import useUserProfile from "hooks/useUserProfile";
 
 /**
@@ -28,12 +28,12 @@ const LegacySafeGuard = ({ leadDetails }) => {
     } = leadDetails;
 
     const leadPhone = useMemo(() => phones?.[0]?.leadPhone || "", [phones]);
-    const leadEmail = useMemo(() => emails?.[0]?.leadEmail || "", [emails]);
-    const leadAddress = useMemo(() => addresses?.[0]?.address1?.replace(/ /g, "%20") || "", [addresses]);
+    const leadEmail = useMemo(() => emails?.[0]?.leadEmail || '', [emails]);
+    const leadAddress = useMemo(() => addresses?.[0]?.address1 || '', [addresses]);
     const leadCity = useMemo(() => addresses?.[0]?.city || "", [addresses]);
     const leadState = useMemo(() => addresses?.[0]?.state || "", [addresses]);
     const leadZip = useMemo(() => addresses?.[0]?.postalCode || "", [addresses]);
-    const formattedBirthdate = useMemo(() => (birthdate ? formatDate(birthdate) : ""), [birthdate]);
+    const formattedBirthdate = useMemo(() => (birthdate ? moment(birthdate).format('MM-DD-YYYY') : ""), [birthdate]);
     const formattedGender = useMemo(() => (gender === "Male" ? "M" : gender === "Female" ? "F" : ""), [gender]);
     const isLSGTagPresent = useMemo(() => leadTags?.some((tag) => tag?.tag?.tagLabel === "LS USER"), [leadTags]);
 
@@ -52,7 +52,7 @@ const LegacySafeGuard = ({ leadDetails }) => {
             ...(leadZip && { mc_zip: leadZip }),
             ...(leadState && { mc_state: leadState }),
             ...(leadPhone && { mc_phone: leadPhone }),
-            ...(leadEmail && { mc_email: `%20${leadEmail}%20` }),
+            ...(leadEmail && { mc_email: leadEmail }),
             ...(formattedGender && { gender: formattedGender }),
             ...(formattedBirthdate && { mc_dob: formattedBirthdate }),
         }).toString();
@@ -64,14 +64,14 @@ const LegacySafeGuard = ({ leadDetails }) => {
      * Handles click on the card, redirecting to the constructed URL.
      */
     const handleCardClick = () => {
-        if (!userNpn || !leadsId) return;
+        if (!userNpn || !leadsId) {return;}
 
         const url = constructLegacySafeGuardUrl();
         window.open(url, "_blank");
     };
 
     // Render nothing if the LS USER tag is present
-    if (isLSGTagPresent) return null;
+    if (isLSGTagPresent) {return null;}
 
     return (
         <Box className={styles.legacySafeGuardCardContainer}>
