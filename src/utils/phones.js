da680282df19e26217be45721adc040b8627c729
rishithainturi <121522091+rishithainturi@users.noreply.github.com>
2024-02-13 12:34:40 -0600

    [SPW-285675] changed the format of phone number as per new designs across application (#2183)

--- src/utils/phones.js
+++ src/utils/phones.js
@@ -1,19 +1,21 @@
-export const formatPhoneNumber = (phoneNumberString, countryCode = false) => {
-  let phoneNumber = phoneNumberString;
-  if (countryCode) {
-    phoneNumber = phoneNumber?.replace(/^\+1/, "");
-  }
-  const originalInput = phoneNumber;
-  const cleaned = ("" + phoneNumber).replace(/\D/g, "");
-  const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
-
-  if (match) {
-    return `(${match[1]}) ${match[2]}-${match[3]}`;
-  }
-
-  if (cleaned === "") {
-    return null;
+/**
+ * Formats a given phone number string to a standard format.
+ *
+ * @param {string} phoneNumberString - The phone number to format.
+ * @param {boolean} [includeCountryCode=false] - Flag to determine if country code should be included in the format.
+ * @returns {string|null} The formatted phone number or null if input is invalid.
+ */
+export const formatPhoneNumber = (phoneNumberString, includeCountryCode = false) => {
+  let originalInput = phoneNumberString;
+  if (includeCountryCode) {
+    originalInput = originalInput?.replace(/^\+1/, "");
   }
 
-  return originalInput;
+
+  const digitsOnly = originalInput.replace(/\D/g, "");
+
+
+  const partsMatch = digitsOnly.match(/^(\d{3})(\d{3})(\d{4})$/);
+
+  return partsMatch ? `${partsMatch[1]}-${partsMatch[2]}-${partsMatch[3]}` : (digitsOnly === "" ? null : originalInput);
 };
