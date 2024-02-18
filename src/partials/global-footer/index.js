ec35cb8dd7a3c924399530af8ea385dc74f56cb3
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-02-18 16:47:02 -0600

    fix: remove medicare app and medicare link from footer

--- src/partials/global-footer/index.js
+++ src/partials/global-footer/index.js
@@ -71,7 +71,7 @@ const GlobalFooter = ({ className = "", hideMedicareIcon = false, ...props }) =>
                                         ) : null
                                     }
                                 </Media>
-                                <li>
+                                {/* <li>
                                     <a
                                         href={process.env.REACT_APP_AUTH_AUTHORITY_URL + "/external/SamlLogin/2023"}
                                         rel="noopener noreferrer"
@@ -90,7 +90,7 @@ const GlobalFooter = ({ className = "", hideMedicareIcon = false, ...props }) =>
                                     >
                                         MedicareLINK
                                     </a>
-                                </li>
+                                </li> */}
                                 <li>
                                     <a
                                         href={`${portalUrl || ""}/terms`}
