e1dd839039b4ab5966e4062f766543bcaf580d20
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-02-20 19:46:18 -0600

    feat: for testing only

--- src/pages/auth/ServerLogoutPage.js
+++ src/pages/auth/ServerLogoutPage.js
@@ -7,6 +7,7 @@ import usePortalUrl from "hooks/usePortalUrl";
 const ServerLogoutPage = () => {
   const params = useQueryParams();
   const portal_url = usePortalUrl();
+  debugger
 
   useEffect(() => {
     const handleLogout = async () => {
@@ -22,7 +23,7 @@ const ServerLogoutPage = () => {
             level: "warning",
           }
         );
-        window.location = portal_url + "/signout-oidc";
+        window.location = `${portal_url}/signout-oidc`;
       }
     };
 
