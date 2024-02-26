ed160e61a2e87b5842fb373c086ed4cf918f6b19
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-02-26 11:50:34 -0600

    SPW-285322: The Integrity Logo showing on IOS device when scrolling.

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,18 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types'; 
+import { Link } from 'react-router-dom';
 
-import { altText } from "./constants";
+import { altText } from './constants';
 
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss';
 
-const Logo = ({ className = "", color = "blue" }) => {
-    const image = `/images/logo-${color}.svg`;
+const Logo = ({ className, color }) => {
+    const imageSrc = `/images/logo-${color}.svg`;
 
     return (
         <Link className={styles.logo} to="/">
-            <img alt={altText} className={`${className} ${styles.image}`} src={image} />
+            <img alt={altText} className={`${className} ${styles.image}`} src={imageSrc} />
         </Link>
     );
+};
+
+Logo.propTypes = {
+    className: PropTypes.string,
+    color: PropTypes.string,
+};
+
+Logo.defaultProps = {
+    className: '',
+    color: 'blue', 
 };
 
 export default Logo;
