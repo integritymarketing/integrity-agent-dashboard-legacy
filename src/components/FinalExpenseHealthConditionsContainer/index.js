2de515812508efe848bd9c14b6f16ef91b76b0c9
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-02-27 12:11:23 -0600

    fix: amplitude issue with Health Conditions Page Viewed

--- src/components/FinalExpenseHealthConditionsContainer/index.js
+++ src/components/FinalExpenseHealthConditionsContainer/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import useAnalytics from "hooks/useAnalytics";
@@ -20,21 +20,20 @@ const FinalExpenseHealthConditionsContainer = () => {
     const { contactId } = useParams();
     const navigate = useNavigate();
     const { fireEvent } = useAnalytics();
+    const eventFiredRef = useRef(false);
 
     const onClickViewQuote = () => {
         navigate(`/finalexpenses/plans/${contactId}`);
     };
 
     useEffect(() => {
-        const timer = setTimeout(() => {
+        if (!eventFiredRef.current) {
             fireEvent("Health Conditions Page Viewed", {
                 leadid: contactId,
                 flow: "final_expense",
             });
-        }, 2000);
-
-        
-        return () => clearTimeout(timer);
+            eventFiredRef.current = true;
+        }
     }, [contactId, fireEvent]);
 
     return (
