d5c03cccafadd0ddf79d0faa4e0db7bee191bf63
rishithainturi <121522091+rishithainturi@users.noreply.github.com>
2024-03-05 12:27:35 -0600

    [285777] Disable Apply on Ineligible Life Products (#2262)

--- src/components/FinalExpensePlansContainer/FinalExpensePlansResultContainer/PlanDetailsContainer/PlanCard.js
+++ src/components/FinalExpensePlansContainer/FinalExpensePlansResultContainer/PlanDetailsContainer/PlanCard.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-lines-per-function */
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import PropTypes from "prop-types";
 import useAgentInformationByID from "hooks/useAgentInformationByID";
 import { useLeadDetails } from "providers/ContactDetails";
@@ -93,10 +93,10 @@ export const PlanCard = ({
                 isShowExcludedProducts && isMyAppointedProducts
                     ? ["My Appointed Products", "Show Excluded Products"]
                     : isMyAppointedProducts
-                    ? ["My Appointed Products"]
-                    : isShowExcludedProducts
-                    ? ["Show Excluded Products"]
-                    : [],
+                        ? ["My Appointed Products"]
+                        : isShowExcludedProducts
+                            ? ["Show Excluded Products"]
+                            : [],
             coverage_vs_premium: selectedTab === COVERAGE_AMOUNT ? "coverage" : "premium",
             quote_coverage_amount: selectedTab === COVERAGE_AMOUNT ? coverageAmount : null,
             quote_monthly_premium: selectedTab === MONTHLY_PREMIUM ? monthlyPremium : null,
@@ -124,6 +124,9 @@ export const PlanCard = ({
             success,
         });
     };
+    const isPlanExcluded = useMemo(() => {
+        return reason != null;
+    }, [reason]);
 
     const onPreApply = async () => {
         lifeQuoteEvent("Life Apply CTA Clicked");
@@ -262,12 +265,12 @@ export const PlanCard = ({
             <div className={styles.applyCTA}>
                 <Button
                     label={APPLY}
-                    disabled={!isHaveCarriers}
+                    disabled={!isHaveCarriers || isPlanExcluded}
                     onClick={onPreApply}
                     type="primary"
                     icon={<ButtonCircleArrow />}
                     iconPosition="right"
-                    className={`${styles.applyButton} ${!isHaveCarriers ? styles.disabled : ""}`}
+                    className={`${styles.applyButton} ${!isHaveCarriers || isPlanExcluded ? styles.disabled : ""}`}
                 />
             </div>
         </div>
