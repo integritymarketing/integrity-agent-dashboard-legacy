fa7191d97d152bfa75cd79254a43d1d62518aa5b
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-03-11 15:32:12 -0500

    fix: loadmore issue with rts life

--- src/pages/Account/ActivePermissions/ActivePermissionsLife/APLifeTable/APLifeTable.js
+++ src/pages/Account/ActivePermissions/ActivePermissionsLife/APLifeTable/APLifeTable.js
@@ -37,7 +37,7 @@ const APLifeTable = () => {
             ) : (
                 <Table data={visibleItems} />
             )}
-            {hasMore && <LoadMoreButton onClick={loadMore} />}
+            {hasMore && <LoadMoreButton loadMore={loadMore} />}
         </Box>
     );
 };
