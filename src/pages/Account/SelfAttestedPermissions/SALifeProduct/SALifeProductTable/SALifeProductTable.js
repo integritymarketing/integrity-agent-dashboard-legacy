bda497308a53f88df05cfb41ee7b205ba486b20c
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-03-15 12:13:40 -0500

    [285814] Mobile layout for self attestaion Life

--- src/pages/Account/SelfAttestedPermissions/SALifeProduct/SALifeProductTable/SALifeProductTable.js
+++ src/pages/Account/SelfAttestedPermissions/SALifeProduct/SALifeProductTable/SALifeProductTable.js
@@ -1,3 +1,4 @@
+import { useWindowSize } from "hooks/useWindowSize";
 import Box from "@mui/material/Box";
 
 import useLoadMore from "../../hooks/useLoadMore";
@@ -7,16 +8,18 @@ import styles from "./styles.module.scss";
 
 import { LoadMoreButton } from "../LoadMoreButton";
 import { useSALifeProductContext } from "../providers/SALifeProductProvider";
+import MobileSALife from "./MobileSALife";
 
 const ITEM_PER_PAGE = 5;
 
 function SALifeProductTable() {
     const { tableData } = useSALifeProductContext();
+    const { width: windowWidth } = useWindowSize();
     const { visibleItems, loadMore, hasMore } = useLoadMore(tableData, ITEM_PER_PAGE);
-
+    const isMobile = windowWidth <= 784;
     return (
         <Box className={styles.tableWrapper}>
-            <Table data={visibleItems} />
+            {isMobile ? <MobileSALife items={visibleItems} /> : <Table data={visibleItems} />}
             {hasMore && <LoadMoreButton loadMore={loadMore} />}
         </Box>
     );
