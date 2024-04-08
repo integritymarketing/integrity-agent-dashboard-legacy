c7d9e1a7d8400b4b39c464358eae619f304f5299
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-04-08 12:36:19 -0500

    fix: reset the redirect URI

--- src/services/authService.js
+++ src/services/authService.js
@@ -75,7 +75,7 @@ class authService {
       client_id: process.env.REACT_APP_AUTH_CLIENT_ID,
       response_type: process.env.REACT_APP_AUTH_RESPONSE_TYPE,
       scope: process.env.REACT_APP_AUTH_SCOPES,
-      redirect_uri: `${portal_url}/signin-oidc?client_id=${process.env.REACT_APP_AUTH_CLIENT_ID}`,
+      redirect_uri: `${portal_url}/signin-oidc`,
       post_logout_redirect_uri: `${portal_url}/signout-oidc`,
       silent_redirect_uri: `${portal_url}/signin-oidc-silent`,
     };
