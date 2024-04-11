62d3c692633ce954e43693e7f47a4922d9141907
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-04-11 16:51:05 -0500

    remove campaign prefix for category name

--- src/pages/ContactsList/CampaignModal/CampaignModal.js
+++ src/pages/ContactsList/CampaignModal/CampaignModal.js
@@ -1,12 +1,12 @@
-import { useCallback } from 'react';
-import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import { useNavigate } from 'react-router-dom';
+import { useCallback } from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
 import useDeviceType from "hooks/useDeviceType";
-import ArrowRightIcon from 'components/icons/version-2/ArrowRight';
-import CampaignStatusIcon from 'components/icons/version-2/CampaignStatus';
-import Modal from 'components/Modal';
-import Styles from './CampaignModal.module.scss';
+import ArrowRightIcon from "components/icons/version-2/ArrowRight";
+import CampaignStatusIcon from "components/icons/version-2/CampaignStatus";
+import Modal from "components/Modal";
+import Styles from "./CampaignModal.module.scss";
 import { toSentenceCase } from "utils/toSentenceCase";
 
 const CampaignModal = ({ open, onClose, campaignList, leadData }) => {
@@ -15,7 +15,10 @@ const CampaignModal = ({ open, onClose, campaignList, leadData }) => {
     const handleViewContact = useCallback(() => {
         navigate(`/contact/${leadData.leadsId}/overview`);
     }, [leadData.leadsId, navigate]);
-    const fullName = `${leadData?.firstName ?? ''} ${leadData?.middleName ?? ''} ${leadData?.lastName ?? ''}`;
+    const fullName = `${leadData?.firstName ?? ""} ${leadData?.middleName ?? ""} ${leadData?.lastName ?? ""}`;
+    const removePrefix = (str) => {
+        return str?.replace("Campaign ", "");
+    };
 
     return (
         <Modal
@@ -39,20 +42,28 @@ const CampaignModal = ({ open, onClose, campaignList, leadData }) => {
                 </Box>
             </Box>
             <Box>
-                {campaignList?.map((campaign, index) => (
-                    <Box key={index} className={Styles.campaignCard}>
-                        <Box className={Styles.campaignInfo}>
-                            <CampaignStatusIcon />
-                            <Box className={Styles.tagInfo}>
-                                <Box display="flex" alignItems="center">
-                                <Box className={Styles.tagCategoryName}>{toSentenceCase(campaign?.tag?.tagCategory?.tagCategoryName)}:</Box>
-                                <Box className={Styles.tagName}>{toSentenceCase(campaign?.tag?.tagLabel)}</Box>
+                {campaignList?.map((campaign, index) => {
+                    const { tag } = campaign;
+                    const { tagCategory, tagLabel, metadata } = tag || {};
+                    const { tagCategoryName } = tagCategory || {};
+
+                    return (
+                        <Box key={index} className={Styles.campaignCard}>
+                            <Box className={Styles.campaignInfo}>
+                                <CampaignStatusIcon />
+                                <Box className={Styles.tagInfo}>
+                                    <Box display="flex" alignItems="center">
+                                        <Box className={Styles.tagCategoryName}>
+                                            {toSentenceCase(removePrefix(tagCategoryName))}:
+                                        </Box>
+                                        <Box className={Styles.tagName}>{toSentenceCase(tagLabel)}</Box>
+                                    </Box>
+                                    <Box className={Styles.tagMetaData}>{metadata}</Box>
                                 </Box>
-                                <Box className={Styles.tagMetaData}>{campaign?.tag?.metadata}</Box>
                             </Box>
                         </Box>
-                    </Box>
-                ))}
+                    );
+                })}
             </Box>
         </Modal>
     );
@@ -61,17 +72,21 @@ const CampaignModal = ({ open, onClose, campaignList, leadData }) => {
 CampaignModal.propTypes = {
     open: PropTypes.bool.isRequired, // Indicates if the modal is currently open or not.
     onClose: PropTypes.func.isRequired, // Function to call when the modal needs to be closed.
-    campaignList: PropTypes.arrayOf(PropTypes.shape({ // List of campaign information to display in the modal.
-        tag: PropTypes.shape({
-            tagLabel: PropTypes.string, // Label of the campaign tag.
-            metadata: PropTypes.string // Metadata associated with the campaign tag.
+    campaignList: PropTypes.arrayOf(
+        PropTypes.shape({
+            // List of campaign information to display in the modal.
+            tag: PropTypes.shape({
+                tagLabel: PropTypes.string, // Label of the campaign tag.
+                metadata: PropTypes.string, // Metadata associated with the campaign tag.
+            }),
         })
-    })),
-    leadData: PropTypes.shape({ // Information about the lead related to the campaigns.
+    ),
+    leadData: PropTypes.shape({
+        // Information about the lead related to the campaigns.
         leadsId: PropTypes.string.isRequired, // Unique identifier for the lead.
         firstName: PropTypes.string, // First name of the lead.
         middleName: PropTypes.string, // Middle name of the lead.
-        lastName: PropTypes.string // Last name of the lead.
+        lastName: PropTypes.string, // Last name of the lead.
     }).isRequired,
 };
 
