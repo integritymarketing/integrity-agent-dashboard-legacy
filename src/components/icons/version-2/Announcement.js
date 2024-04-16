29989d874a32b4ddebef37c0f5af3e73a9475ebe
rishithainturi <121522091+rishithainturi@users.noreply.github.com>
2024-04-16 14:17:56 -0500

    [campaign] camapaign icon with blue bg (#2339)

--- src/components/icons/version-2/Announcement.js
+++ src/components/icons/version-2/Announcement.js
@@ -1,55 +1,38 @@
 const Announcement = () => {
     return (
-        <svg id="Campaign" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <g id="Button_-_BG" data-name="Button - BG">
-                <rect id="bg" width="24" height="24" fill="#ff1717" opacity="0" />
+        <svg id="Campaigns_-_Status" data-name="Campaigns - Status" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
+            <g id="Campaign">
+                <g id="Button_-_BG" data-name="Button - BG">
+                    <rect id="bg" width="32" height="32" fill="#ff1717" opacity="0" />
+                </g>
+                <g id="Group_8589" data-name="Group 8589">
+                    <circle id="Ellipse_725" data-name="Ellipse 725" cx="20" cy="20" r="20" fill="#f1faff" />
+                </g>
             </g>
-            <g id="Group_8718" data-name="Group 8718" transform="translate(0.5 2.5)">
-                <path
-                    id="Path_21980"
-                    data-name="Path 21980"
-                    d="M19.5,16.312l-16-3.8v-7.6l16-3.8Z"
-                    fill="#fff"
-                    stroke="#4178ff"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1"
-                />
-                <path
-                    id="Path_21981"
-                    data-name="Path 21981"
-                    d="M3.5,13.113h-2a1,1,0,0,1-1-1v-6a1,1,0,0,1,1-1h2Z"
-                    transform="translate(0 -0.355)"
-                    fill="#fff"
-                    stroke="#4178ff"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1"
-                />
-                <path
-                    id="Path_21982"
-                    data-name="Path 21982"
-                    d="M20.5,16.34l1.684.535a.993.993,0,0,0,1.316-.9V1.453a.993.993,0,0,0-1.316-.9L20.5,1.084Z"
-                    transform="translate(-1 0)"
-                    fill="#fff"
-                    stroke="#4178ff"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1"
-                />
-                <path
-                    id="Path_21983"
-                    data-name="Path 21983"
-                    d="M6.5,14.113l.833,2.5a1,1,0,0,0,.674.645L13.7,18.885a1,1,0,0,0,1.169-.514l.629-2.258"
-                    transform="translate(-0.429 -0.801)"
-                    fill="none"
-                    stroke="#4178ff"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1"
-                />
+            <g id="Campaign_Source" data-name="Campaign Source" transform="translate(8 8)">
+                <g id="Campaign-2" data-name="Campaign">
+                    <g id="Button_-_BG-2" data-name="Button - BG">
+                        <rect id="bg-2" data-name="bg" width="24" height="24" fill="#ff1717" opacity="0" />
+                    </g>
+                    <g id="Group_8589-2" data-name="Group 8589" transform="translate(-480 -1155)">
+                        <g id="Ellipse_725-2" data-name="Ellipse 725" transform="translate(480 1155)" fill="#fff" stroke="#4178ff" stroke-width="1">
+                            <circle cx="12" cy="12" r="12" stroke="none" />
+                            <circle cx="12" cy="12" r="11.5" fill="none" />
+                        </g>
+                    </g>
+                </g>
+                <g id="Button_-_BG-3" data-name="Button - BG">
+                    <rect id="bg-3" data-name="bg" width="24" height="24" fill="#ff1717" opacity="0" />
+                </g>
+                <g id="Group_8718" data-name="Group 8718" transform="translate(4.5 6.5)">
+                    <path id="Path_21980" data-name="Path 21980" d="M13.682,11.463,3.5,8.876V3.7L13.682,1.113Z" transform="translate(-1.091 -0.196)" fill="#fff" stroke="#4178ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" />
+                    <path id="Path_21981" data-name="Path 21981" d="M2.409,10.561H1.136A.66.66,0,0,1,.5,9.88V5.794a.66.66,0,0,1,.636-.681H2.409Z" transform="translate(0 -1.713)" fill="#fff" stroke="#4178ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" />
+                    <path id="Path_21982" data-name="Path 21982" d="M20.5,11.286l1.072.364a.638.638,0,0,0,.837-.616V1.149a.638.638,0,0,0-.837-.616L20.5.9Z" transform="translate(-7.909 0)" fill="#fff" stroke="#4178ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" />
+                    <path id="Path_21983" data-name="Path 21983" d="M6.5,14.113l.53,1.7a.659.659,0,0,0,.429.439l3.624,1.108a.622.622,0,0,0,.744-.35l.4-1.538" transform="translate(-2.455 -4.889)" fill="none" stroke="#4178ff" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" />
+                </g>
             </g>
         </svg>
+
     );
 };
 
