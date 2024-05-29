9f7cf313f7561e218cf0c77f23a98922a99dadea
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-05-29 11:05:27 -0500

    chore: Update environment variable name in generate-redirects.js

--- generate-redirects.js
+++ generate-redirects.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.REACT_APP_BUILD_ENV === "production") {
     fs.writeFileSync("_redirects", "/welcome https://integrity.com 301\n/* /index.html 200");
 }
