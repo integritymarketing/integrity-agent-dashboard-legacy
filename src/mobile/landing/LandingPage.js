d12df66d5a0ecf440a59d7e064918616eb3b0ad8
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-05-30 10:27:38 -0500

    update the redirect uri for login

--- src/mobile/landing/LandingPage.js
+++ src/mobile/landing/LandingPage.js
@@ -14,7 +14,7 @@ const LandingPage = () => {
         try {
             await loginWithRedirect({
                 authorizationParams: {
-                    redirect_uri: `${window.location.origin}/dashboard`,
+                    redirect_uri: `${process.env.REACT_APP_PORTAL_URL}/dashboard`,
                 },
             });
         } catch (e) {
