4899df01f83e28c1cbdb5d52da6d3acc9328e82d
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-06-10 10:57:07 -0500

    fix: clean up code

--- src/utils/greetings.js
+++ src/utils/greetings.js
@@ -1,10 +1,14 @@
 export const greetings = () => {
-  var myDate = new Date();
-  var hours = myDate.getHours();
-  var greet;
+    const myDate = new Date();
+    const hours = myDate.getHours();
+    let greet;
 
-  if (hours < 12) greet = "Morning";
-  else if (hours >= 12 && hours <= 17) greet = "Afternoon";
-  else if (hours >= 17 && hours <= 24) greet = "Evening";
-  return greet;
+    if (hours < 12) {
+        greet = "Morning";
+    } else if (hours >= 12 && hours <= 17) {
+        greet = "Afternoon";
+    } else if (hours >= 17 && hours <= 24) {
+        greet = "Evening";
+    }
+    return greet;
 };
