8424bac6469bfb4d7146b801095cc1c3d4e2c3b5
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-07-20 01:28:47 -0500

    feat: remove existing mui theme config

--- src/components/ui/version-2/Checkbox/Checkbox.js
+++ src/components/ui/version-2/Checkbox/Checkbox.js
@@ -5,15 +5,9 @@ const BpIcon = styled("span")(({ theme }) => ({
     borderRadius: 3,
     width: 16,
     height: 16,
-    boxShadow:
-        theme.palette.mode === "dark"
-            ? "0 0 0 1px rgb(16 22 26 / 40%)"
-            : "inset 0 0 0 1px #dddddd, inset 0 -1px 0 #dddddd",
+    boxShadow: "inset 0 0 0 1px #dddddd, inset 0 -1px 0 #dddddd",
     backgroundColor: "transparent",
-    backgroundImage:
-        theme.palette.mode === "dark"
-            ? "linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))"
-            : "linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))",
+    backgroundImage: "linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))",
     ".Mui-focusVisible &": {
         outline: "2px auto rgba(19,124,189,.6)",
         outlineOffset: 2,
--- src/packages/Dialog/index.js
+++ src/packages/Dialog/index.js
@@ -12,103 +12,96 @@ import { styled } from "@mui/system";
 import { Divider, Box } from "@mui/material";
 
 const StyledDialogTitle = styled(DialogTitle)(({ theme }) => ({
-  color: theme.palette.dialog.color,
-  fontSize: "24px",
-  fontFamily: "lato",
-  fontWeight: "bold",
+    color: "#002D72",
+    fontSize: "24px",
+    fontFamily: "lato",
+    fontWeight: "bold",
 }));
 
 const StyledDialog = styled(MUIDialog)(({ theme }) => ({
-  borderRadius: "8px",
+    borderRadius: "8px",
 }));
 
 const StyledDialogActions = styled(DialogActions)(({ theme }) => ({
-  padding: "1rem",
-  backgroundColor: "#F4F8FB",
+    padding: "1rem",
+    backgroundColor: "#F4F8FB",
 }));
 
 const StyledDialogContent = styled(DialogContent)(({ theme }) => ({
-  backgroundColor: "#F4F8FB",
+    backgroundColor: "#F4F8FB",
 }));
 
 export default function Dialog(props) {
-  const {
-    title,
-    saveText,
-    cancelText,
-    onSave,
-    onCancel,
-    open,
-    children,
-    onClose,
-    fullWidth,
-    maxWidth = "xs",
-    disabled = false,
-    titleWithIcon = true,
-  } = props;
+    const {
+        title,
+        saveText,
+        cancelText,
+        onSave,
+        onCancel,
+        open,
+        children,
+        onClose,
+        fullWidth,
+        maxWidth = "xs",
+        disabled = false,
+        titleWithIcon = true,
+    } = props;
 
-  return (
-    <StyledDialog
-      onClose={onClose}
-      open={open}
-      fullWidth={fullWidth}
-      maxWidth={maxWidth}
-    >
-      {title && (
-        <StyledDialogTitle>
-          <Box
-            sx={{
-              display: "flex",
-            }}
-          >
-            {titleWithIcon && (
-              <ActivitySubjectWithIcon activitySubject={title} />
+    return (
+        <StyledDialog onClose={onClose} open={open} fullWidth={fullWidth} maxWidth={maxWidth}>
+            {title && (
+                <StyledDialogTitle>
+                    <Box
+                        sx={{
+                            display: "flex",
+                        }}
+                    >
+                        {titleWithIcon && <ActivitySubjectWithIcon activitySubject={title} />}
+                        {title}
+                    </Box>
+
+                    {onClose ? (
+                        <IconButton
+                            aria-label="close"
+                            onClick={onClose}
+                            sx={{
+                                position: "absolute",
+                                right: 8,
+                                top: 8,
+                                color: "#0052CE",
+                            }}
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    ) : null}
+                </StyledDialogTitle>
             )}
-            {title}
-          </Box>
-
-          {onClose ? (
-            <IconButton
-              aria-label="close"
-              onClick={onClose}
-              sx={{
-                position: "absolute",
-                right: 8,
-                top: 8,
-                color: (theme) => theme.palette.primary.main,
-              }}
-            >
-              <CloseIcon />
-            </IconButton>
-          ) : null}
-        </StyledDialogTitle>
-      )}
-      <Divider />
-      <StyledDialogContent>{children}</StyledDialogContent>
-      <StyledDialogActions>
-        {cancelText && (
-          <Button size={"medium"} onClick={onCancel}>
-            {cancelText}
-          </Button>
-        )}
-        {(onSave || saveText) && (
-          <Button disabled={disabled} size={"medium"} onClick={onSave}>
-            {saveText}
-          </Button>
-        )}
-      </StyledDialogActions>
-    </StyledDialog>
-  );
+            <Divider />
+            <StyledDialogContent>{children}</StyledDialogContent>
+            <StyledDialogActions>
+                {cancelText && (
+                    <Button size={"medium"} onClick={onCancel}>
+                        {cancelText}
+                    </Button>
+                )}
+                {(onSave || saveText) && (
+                    <Button disabled={disabled} size={"medium"} onClick={onSave}>
+                        {saveText}
+                    </Button>
+                )}
+            </StyledDialogActions>
+        </StyledDialog>
+    );
 }
 
 Dialog.propTypes = {
-  onSave: PropTypes.func,
-  onCancel: PropTypes.func,
-  open: PropTypes.bool.isRequired,
-  children: PropTypes.element.isRequired,
-  saveText: PropTypes.string,
-  cancelText: PropTypes.string,
-  title: PropTypes.string,
-  maxWidth: PropTypes.oneOf(["xs", "sm", "md", "lg", "xl"]),
-  disabled: PropTypes.bool,
+    onSave: PropTypes.func,
+    onCancel: PropTypes.func,
+    open: PropTypes.bool.isRequired,
+    children: PropTypes.element.isRequired,
+    saveText: PropTypes.string,
+    cancelText: PropTypes.string,
+    title: PropTypes.string,
+    maxWidth: PropTypes.oneOf(["xs", "sm", "md", "lg", "xl"]),
+    disabled: PropTypes.bool,
 };
