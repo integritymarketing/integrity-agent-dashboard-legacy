ae76f5b77b0356b225a4c33b2b39a935cb53a454
jaime-lynn <jaime.madsen@gmail.com>
2024-07-26 09:09:09 -0600

    Remove MedicareLINK and MedicareAPP links from navigation menu (#2455)
    Co-authored-by: Jaime Jones <jaime.jones@RIM-M316-jaime.jones>

--- src/partials/global-nav-v2/MobileMenu/index.js
+++ src/partials/global-nav-v2/MobileMenu/index.js
@@ -90,23 +90,6 @@ const MobileMenu = ({ onClose }) => {
             },
         },
         {
-            label: "MedicareAPP",
-            action: () => {
-                window.open(
-                    `${process.env.REACT_APP_CONNECTURE_LINK}/${npn}/${process.env.REACT_APP_CURRENT_PLAN_YEAR}`,
-                    "_blank"
-                );
-                onClose();
-            },
-        },
-        {
-            label: "MedicareLINK",
-            action: () => {
-                window.open(process.env.REACT_APP_SUNFIRE_SSO_URL, "_blank");
-                onClose();
-            },
-        },
-        {
             label: "CSG App",
             action: () => {
                 getAccessTokenSilently().then((token) => {
