51f92fbea904e35fad821ee7f511a2d0d8f5fb37
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-09-23 16:55:55 -0500

    Refactor CampaignsFilterBar component to remove unused code and improve readability

--- src/pages/ContactsList/CampaignsFilterBar/index.js
+++ src/pages/ContactsList/CampaignsFilterBar/index.js
@@ -1,8 +1,6 @@
 import Box from "@mui/material/Box";
-
 import useCampaignLeadIds from "../hooks/useCampaignLeadIds";
 import ClearFilterButton from "components/CampaignInvitationContainer/InvitationCountBar/ClearFilterButton";
-
 import { useContactsListContext } from "../providers/ContactsListProvider";
 import styles from "./styles.module.scss";
 
@@ -17,7 +15,7 @@ function CampaignsFilterBar() {
     };
 
     if (count === 0) {
-        return <></>;
+        return null;
     }
 
     return (
@@ -26,23 +24,21 @@ function CampaignsFilterBar() {
             <Box>
                 {filteredInfo?.status === "Delivered" ? (
                     <>
-                        Sending to <span className={styles.count}> {count}</span> contact{count > 1 && "s"}
+                        Sending to <span className={styles.count}>{count}</span> contact{count > 1 ? "s" : ""}
                     </>
                 ) : (
                     <>
-                        Sending to <span className={styles.count}> {count}</span> of{" "}
-                        <span className={styles.count}> {filteredInfo?.totalCount}</span> contacts
+                        Sending to <span className={styles.count}>{count}</span> of{" "}
+                        <span className={styles.count}>{filteredInfo?.totalCount}</span> contacts
                     </>
                 )}
             </Box>
             <Box className={styles.divider} />
-
             <Box className={styles.filteredContent}>
                 <span className={styles.status}>{filteredInfo?.status}</span>
                 <span className={styles.italicText}> from</span>
                 <span className={styles.campaignName}> {filteredInfo?.campaignName}</span>
             </Box>
-
             <Box className={styles.bannerFilter}>
                 <ClearFilterButton onClear={onClickHandle} />
             </Box>
