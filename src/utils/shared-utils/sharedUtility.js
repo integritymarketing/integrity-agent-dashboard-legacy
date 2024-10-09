7091b0be651449bb93548d17b9790fc24e1e6194
vishalmarneni <vishal.marneni@integritymarketing.com>
2024-10-09 14:29:07 -0500

    fix: bug wsod

--- src/utils/shared-utils/sharedUtility.js
+++ src/utils/shared-utils/sharedUtility.js
@@ -1,6 +1,7 @@
-import React from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
+import * as Sentry from "@sentry/react";
+
 /**
  * Renders a color option with styling and click handler.
  *
