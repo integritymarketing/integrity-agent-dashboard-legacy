81716407149967b8f3950b4ee51d7ea705e1b345
vishalmarneni <79602980+vishalmarneni@users.noreply.github.com>
2024-10-26 22:53:57 +0530

    Feature/ask integrity (#2729)
    * Refactor WebChat.js and add webChatStyleOptions.js
    
    * Refactor AskIntegrityFeedback component and update imports

--- src/components/WebChat/WebChat.js
+++ src/components/WebChat/WebChat.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useMemo, useRef, useCallback } from "react";
+/* eslint-disable max-lines-per-function */
+import { useEffect, useState, useMemo, useRef, useCallback } from "react";
 import ReactWebChat, { createDirectLine, createStore } from "botframework-webchat";
 import * as Sentry from "@sentry/react";
 import cx from "classnames";
@@ -15,19 +16,21 @@ import closeAudio from "./close.mp3";
 import Info from "components/icons/info-blue";
 import useDeviceType from "hooks/useDeviceType";
 import AskIntegrityFeedback from "./AskIntegrityInfoContainer/AskIntegrityFeedback";
+import styleOptions from "./webChatStyleOptions";
 
 const WebChatComponent = () => {
     const { getAccessTokenSilently } = useAuth0();
     const { npn, fullName } = useUserProfile();
     const showToast = useToast();
-    const [directLineToken, setDirectLineToken] = useState(null);
-    const [showAskIntegrityFeedback, setShowAskIntegrityFeedback] = useState(false);
-    const [isChatActive, setIsChatActive] = useState(false);
     const audioRefOpen = useRef(null);
     const audioRefClose = useRef(null);
     const chatRef = useRef(null);
     const { fireEvent } = useAnalytics();
     const { isDesktop } = useDeviceType();
+    const [directLineToken, setDirectLineToken] = useState(null);
+    const [showAskIntegrityFeedback, setShowAskIntegrityFeedback] = useState(false);
+    const [isChatActive, setIsChatActive] = useState(false);
+    const [skipFeedbackInfo, setSkipFeedbackInfo] = useState(false);
 
     const fetchDirectLineToken = useCallback(async () => {
         try {
@@ -57,12 +60,11 @@ const WebChatComponent = () => {
         await fetchDirectLineToken();
     }, [fetchDirectLineToken]);
 
-    const clearChat = useCallback(async () => {
+    const clearChat = useCallback(() => {
         const container = document.querySelector(".webchat__basic-transcript__transcript");
-        if (!container) {
-            return;
+        if (container) {
+            container.innerHTML = "";
         }
-        container.innerHTML = "";
     }, []);
 
     const closeChat = useCallback(() => {
@@ -91,8 +93,10 @@ const WebChatComponent = () => {
     }, [closeChat]);
 
     useEffect(() => {
+        let intervalId;
+
         if (isChatActive) {
-            const intervalId = setInterval(() => {
+            intervalId = setInterval(() => {
                 const inputElement = document.querySelector('[data-id="webchat-sendbox-input"]');
                 if (inputElement) {
                     inputElement.setAttribute("maxLength", "100");
@@ -102,9 +106,9 @@ const WebChatComponent = () => {
                     clearInterval(intervalId);
                 }
             }, 500);
-
-            return () => clearInterval(intervalId);
         }
+
+        return () => clearInterval(intervalId);
     }, [isChatActive, isDesktop]);
 
     useEffect(() => {
@@ -117,43 +121,6 @@ const WebChatComponent = () => {
     }, [isChatActive]);
 
     const directLine = useMemo(() => createDirectLine({ token: directLineToken }), [directLineToken]);
-
-    const styleOptions = {
-        autoScrollSnapOnPage: true,
-        backgroundColor: "#1D3E71",
-        bubbleBackground: "#375582",
-        bubbleFromUserBackground: "#375582",
-        bubbleBorderWidth: 0,
-        bubbleFromUserBorderWidth: 0,
-        bubbleTextColor: "#fff",
-        bubbleFromUserTextColor: "#fff",
-        sendBoxBackground: "#052A63",
-        sendBoxButtonColor: "#fff",
-        sendBoxTextColor: "#fff",
-        sendBoxHeight: 80,
-        hideUploadButton: true,
-        sendBoxPlaceholderColor: "#ffffffcc",
-        sendBoxButtonShadeColorOnActive: "transparent",
-        sendBoxButtonShadeColorOnFocus: "transparent",
-        sendBoxButtonShadeColorOnHover: "transparent",
-        timestampColor: "#fff",
-        sendBoxBorderTop: 0,
-        fontTypes: {
-            default: {
-                fontFamily: "'Lato', sans-serif",
-                fontSizes: {
-                    small: 12,
-                    default: 16,
-                    medium: 16,
-                    large: 24,
-                    extraLarge: 26,
-                },
-            },
-        },
-        spacing: {
-            small: 2,
-        },
-    };
 
     const overrideLocalizedStrings = {
         TEXT_INPUT_PLACEHOLDER: "Ask Integrity",
@@ -182,139 +149,169 @@ const WebChatComponent = () => {
         [clearChat],
     );
 
+    const handlePostActivity = useCallback(
+        async (action, dispatch) => {
+            if (action?.meta === "imBack") {
+                fireEvent("AI - Ask Integrity Playback Received", {
+                    intent_name: action?.payload?.activity?.text,
+                    message_card_id: action?.payload?.activity?.text,
+                });
+            }
+
+            if (action?.payload?.activity?.value?.leadId) {
+                fireEvent("AI - Ask Integrity CTA Clicked", {
+                    leadid: action?.payload?.activity?.value?.leadId,
+                    cta_name: action?.payload?.activity?.value?.name,
+                    intent_name: action?.payload?.activity?.value?.dialogId,
+                    message_card_id: action?.payload?.activity?.value?.dialogId,
+                });
+            }
+
+            if (
+                action?.payload?.activity?.name !== "webchat/join" &&
+                action?.payload?.activity?.value?.data?.dialogId
+            ) {
+                fireEvent("AI - Ask Integrity Playback Received", {
+                    leadid: action?.payload?.activity?.value?.data?.leadId,
+                    message_card_id: action?.payload?.activity?.value?.data?.dialogId,
+                });
+            }
+
+            const accessToken = await getAccessTokenSilently();
+            const activity = action.payload.activity;
+
+            if (activity.type === "message") {
+                handleMessageActivity(activity, accessToken, dispatch);
+            } else if (activity.type === "event") {
+                handleEventActivity(activity);
+            }
+        },
+        [fireEvent, getAccessTokenSilently],
+    );
+
+    const handleMessageActivity = useCallback(
+        (activity, accessToken, dispatch) => {
+            const message = activity.text || activity.value?.dropDownInfo || activity.value?.text;
+            activity.text = message;
+
+            const channelData = activity.channelData || {};
+            channelData.authToken = `Bearer ${accessToken}`;
+            channelData.data = channelData.data || [];
+            activity.channelData = channelData;
+
+            const activityValue = activity.value;
+            if (activityValue) {
+                activity.channelData.postBack = false;
+                if (activityValue.name === "mc_View_Contact") {
+                    goToContactDetailPage(activityValue.leadId);
+                } else if (activityValue.name === "mc_Redirect_Page") {
+                    window.location.href = activityValue.url;
+                } else if (activityValue.name === "mc_Leave_Feedback") {
+                    setShowAskIntegrityFeedback(true);
+                    setSkipFeedbackInfo(true);
+                    setIsChatActive(false);
+                }
+            }
+
+            if (
+                activityValue &&
+                [
+                    "mc_View_Call_Summary",
+                    "mc_View_Transcript",
+                    "mc_View_Contact",
+                    "mc_Ask_Something_Else",
+                    "mc_Search_Contact_Call",
+                ].includes(activityValue.name)
+            ) {
+                activity.channelData.postBack = true;
+            }
+
+            if (
+                activityValue &&
+                ["mc_Contact_Selected", "mc_Call_Selected", "mc_Search_Contact_Call"].includes(activityValue.name)
+            ) {
+                dispatch({
+                    type: "WEB_CHAT/SEND_EVENT",
+                    payload: {
+                        name: activityValue.name,
+                        value: {
+                            data: activityValue,
+                            appId: "mcweb",
+                        },
+                    },
+                });
+            }
+        },
+        [goToContactDetailPage],
+    );
+
+    const handleEventActivity = useCallback((activity) => {
+        const activityValue = activity.value;
+        if (activity.name === "mc_View_Contact") {
+            window.open(`${window.location.origin}/contact/${activityValue.leadId}`);
+        }
+    }, []);
+
+    const handleIncomingActivity = useCallback(
+        (action) => {
+            const activity = action.payload.activity;
+
+            if (activity?.attachments?.[0]) {
+                fireEvent("AI - Ask Integrity Playback Received", {
+                    leadid: activity.attachments[0]?.content?.actions?.[0]?.data?.leadId,
+                    message_card_id: activity.attachments[0]?.content?.id,
+                });
+            }
+
+            if (activity.type === "event" && activity.name === "mc_View_Contact") {
+                goToContactDetailPage(activity.value.leadId);
+            }
+        },
+        [fireEvent, goToContactDetailPage],
+    );
+
     const store = useMemo(
         () =>
             createStore({}, ({ dispatch }) => (next) => async (action) => {
-                if (action.type === "WEB_CHAT/SUBMIT_SEND_BOX") {
-                    fireEvent("AI - Ask Integrity Input Sent", { input_type: "text" });
-                }
-                if (action.type === "DIRECT_LINE/CONNECT_FULFILLED") {
-                    const accessToken = await getAccessTokenSilently();
-                    dispatch({
-                        type: "WEB_CHAT/SEND_EVENT",
-                        payload: {
-                            name: "webchat/join",
-                            value: {
-                                language: "en-US",
-                                marketerName: fullName,
-                                marketerId: npn,
-                                authorization: `Bearer ${accessToken}`,
-                                host: process.env.REACT_APP_BOT_HOST_NAME,
-                                hostUrl: process.env.REACT_APP_HOST_URL,
-                                appId: "mcweb",
-                            },
-                        },
-                    });
-                } else if (action.type === "DIRECT_LINE/POST_ACTIVITY") {
-                    console.log("inside post activity", action);
-                    if (action?.meta === "imBack") {
-                        console.log("inside intents", action);
-                        fireEvent("AI - Ask Integrity Playback Received", {
-                            intent_name: action?.payload?.activity?.text,
-                            message_card_id: action?.payload?.activity?.text,
-                        });
-                    }
-                    if (action?.payload?.activity?.value?.leadId) {
-                        console.log("inside AI - Ask Integrity CTA Clicked", action);
-                        fireEvent("AI - Ask Integrity CTA Clicked", {
-                            leadid: action?.payload?.activity?.value?.leadId,
-                            cta_name: action?.payload?.activity?.value?.name,
-                            intent_name: action?.payload?.activity?.value?.dialogId,
-                            message_card_id: action?.payload?.activity?.value?.dialogId,
-                        });
-                    }
-                    if (
-                        action?.payload?.activity?.name !== "webchat/join" &&
-                        action?.payload?.activity?.value?.data?.dialogId
-                    ) {
-                        console.log("inside AI - Ask Integrity Playback Received", action);
-                        fireEvent("AI - Ask Integrity Playback Received", {
-                            leadid: action?.payload?.activity?.value?.data?.leadId,
-                            message_card_id: action?.payload?.activity?.value?.data?.dialogId,
-                        });
-                    }
-                    const accessToken = await getAccessTokenSilently();
-                    if (action.payload.activity.type === "message") {
-                        let message = action.payload.activity.text
-                            ? action.payload.activity.text
-                            : action.payload.activity.value.dropDownInfo;
-                        const activityValue = action.payload.activity.value;
-                        if (!message) {
-                            message = activityValue ? activityValue.text : message;
-                            action.payload.activity.text = message;
-                        }
-                        const channelData = action.payload.activity.channelData;
-                        const data = [];
-                        if (channelData) {
-                            channelData.authToken = `Bearer ${accessToken}`;
-                            channelData.data = data;
-                            action.payload.activity.channelData = channelData;
-                        } else {
-                            action.payload.activity.channelData = {
-                                authToken: `Bearer ${accessToken}`,
-                                data: data,
-                            };
-                        }
-                        if (activityValue != null) {
-                            action.payload.activity.channelData.postBack = false;
-                            if (activityValue.name === "mc_View_Contact") {
-                                const leadId = activityValue.leadId;
-                                goToContactDetailPage(leadId);
-                            }
-                        }
-                        if (
-                            activityValue != null &&
-                            (activityValue.name === "mc_View_Call_Summary" ||
-                                activityValue.name === "mc_View_Transcript" ||
-                                activityValue.name === "mc_View_Contact" ||
-                                activityValue.name === "mc_Ask_Something_Else" ||
-                                activityValue.name === "mc_Search_Contact_Call")
-                        ) {
-                            action.payload.activity.channelData.postBack = true;
-                        }
+                switch (action.type) {
+                    case "WEB_CHAT/SUBMIT_SEND_BOX":
+                        fireEvent("AI - Ask Integrity Input Sent", { input_type: "text" });
+                        break;
 
-                        if (
-                            activityValue != null &&
-                            (activityValue.name === "mc_Contact_Selected" ||
-                                activityValue.name === "mc_Call_Selected" ||
-                                activityValue.name === "mc_Search_Contact_Call")
-                        ) {
-                            return dispatch({
-                                type: "WEB_CHAT/SEND_EVENT",
-                                payload: {
-                                    name: activityValue.name,
-                                    value: {
-                                        data: activityValue,
-                                        appId: "mcweb",
-                                    },
+                    case "DIRECT_LINE/CONNECT_FULFILLED": {
+                        const accessToken = await getAccessTokenSilently();
+                        dispatch({
+                            type: "WEB_CHAT/SEND_EVENT",
+                            payload: {
+                                name: "webchat/join",
+                                value: {
+                                    language: "en-US",
+                                    marketerName: fullName,
+                                    marketerId: npn,
+                                    authorization: `Bearer ${accessToken}`,
+                                    host: process.env.REACT_APP_BOT_HOST_NAME,
+                                    hostUrl: process.env.REACT_APP_HOST_URL,
+                                    appId: "mcweb",
                                 },
-                            });
-                        }
-                    } else if (action.payload.activity.type === "event") {
-                        const activityValue = action.payload.activity.value;
-                        if (action.payload.activity.name === "mc_View_Contact") {
-                            const leadId = activityValue.leadId;
-                            window.open(`${window.location.origin}/contact/${leadId}`);
-                        }
-                    }
-                } else if (action.type === "DIRECT_LINE/INCOMING_ACTIVITY") {
-                    if (action?.payload?.activity?.attachments?.[0]) {
-                        fireEvent("AI - Ask Integrity Playback Received", {
-                            leadid: action?.payload?.activity?.attachments?.[0]?.content?.actions?.[0]?.data?.leadId,
-                            message_card_id: action?.payload?.activity?.attachments?.[0]?.content?.id,
+                            },
                         });
+                        break;
                     }
-                    if (action.payload.activity.type === "event") {
-                        const activityValue = action.payload.activity.value;
-                        if (activityValue !== null && action.payload.activity.name === "mc_View_Contact") {
-                            const leadId = activityValue.leadId;
-                            goToContactDetailPage(leadId);
-                        }
-                    }
+
+                    case "DIRECT_LINE/POST_ACTIVITY":
+                        handlePostActivity(action, dispatch);
+                        break;
+
+                    case "DIRECT_LINE/INCOMING_ACTIVITY":
+                        handleIncomingActivity(action);
+                        break;
+
+                    default:
+                        break;
                 }
                 return next(action);
             }),
-        [fullName, npn, fireEvent, goToContactDetailPage],
+        [fullName, npn, fireEvent, getAccessTokenSilently, handlePostActivity, handleIncomingActivity],
     );
 
     const handleOpenAskIntegrityFeedback = () => {
@@ -337,6 +334,7 @@ const WebChatComponent = () => {
                             setShowAskIntegrityFeedback(false);
                             setIsChatActive(false);
                         }}
+                        skipFeedbackInfo={skipFeedbackInfo}
                     />
                 </div>
             </div>
---
+++ src/components/WebChat/webChatStyleOptions.js
@@ -0,0 +1,38 @@
+const styleOptions = {
+    autoScrollSnapOnPage: true,
+    backgroundColor: "#1D3E71",
+    bubbleBackground: "#375582",
+    bubbleFromUserBackground: "#375582",
+    bubbleBorderWidth: 0,
+    bubbleFromUserBorderWidth: 0,
+    bubbleTextColor: "#fff",
+    bubbleFromUserTextColor: "#fff",
+    sendBoxBackground: "#052A63",
+    sendBoxButtonColor: "#fff",
+    sendBoxTextColor: "#fff",
+    sendBoxHeight: 80,
+    hideUploadButton: true,
+    sendBoxPlaceholderColor: "#ffffffcc",
+    sendBoxButtonShadeColorOnActive: "transparent",
+    sendBoxButtonShadeColorOnFocus: "transparent",
+    sendBoxButtonShadeColorOnHover: "transparent",
+    timestampColor: "#fff",
+    sendBoxBorderTop: 0,
+    fontTypes: {
+        default: {
+            fontFamily: "'Lato', sans-serif",
+            fontSizes: {
+                small: 12,
+                default: 16,
+                medium: 16,
+                large: 24,
+                extraLarge: 26,
+            },
+        },
+    },
+    spacing: {
+        small: 2,
+    },
+};
+
+export default styleOptions;
