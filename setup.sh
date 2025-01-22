2656424d6fe70fb5692a8fedf59e8ba06eec2702
unstephenk <unstephenk@gmail.com>
2025-01-21 22:55:00 -0600

    added sonar to netlify

--- setup.sh
+++ setup.sh
@@ -11,8 +11,5 @@ fi
 sdk install java 17.0.6-zulu
 sdk use java 17.0.6-zulu
 
-export JAVA_HOME=$HOME/.sdkman/candidates/java/current
-export PATH=$JAVA_HOME/bin:$PATH
-
 # Print the Java version to verify
 java -version
