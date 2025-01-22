374b56461379402a4bada72a130804246d0175fb
unstephenk <unstephenk@gmail.com>
2025-01-21 22:17:25 -0600

    added sonar to netlify

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install SDKMAN if not already installed
+if ! command -v sdk &> /dev/null
+then
+    curl -s "https://get.sdkman.io" | bash
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
+
+# Install and set the desired Java version
+sdk install java 17.0.6-zulu
+sdk use java 17.0.6-zulu
