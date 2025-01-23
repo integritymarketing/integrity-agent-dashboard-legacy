bdf76d617c3919aa54b6fb52b6e87f83d87a66f0
unstephenk <unstephenk@gmail.com>
2025-01-23 11:20:07 -0600

    sonar updates

--- plugins/netlify/prebuild-set-version-number/index.js
+++ plugins/netlify/prebuild-set-version-number/index.js
@@ -2,7 +2,9 @@ const { version } = require("../../../package.json");
 
 module.exports = {
   onPreBuild: () => {
-    process.env["VITE_VERSION"] = version;
-    console.log(`Setting VITE_VERSION to ${import.meta.env.VITE_VERSION}`);
+    process.env["REACT_APP_VERSION"] = version;
+    console.log(
+      `Setting REACT_APP_VERSION to ${process.env.REACT_APP_VERSION}`
+    );
   },
 };
