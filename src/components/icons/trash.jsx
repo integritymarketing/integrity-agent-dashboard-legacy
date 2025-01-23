a69ab03f6155601e2b4e2d7e21ef8aef277e64e7
unstephenk <unstephenk@gmail.com>
2025-01-23 12:36:48 -0600

    Add blameable content to missing jsx file

--- src/components/icons/trash.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default () => {
-  return (
-    <?xml version="1.0" encoding="UTF-8"?><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 24"><defs><style>.e{fill:none;stroke:#0052ce;strokeLinecap:round;strokeLinejoin:round;}</style></defs><g id="a"/><g id="b"><g id="c"><g id="d"><line class="e" x1=".5" y1="3.5" x2="19.5" y2="3.5"/><path class="e" d="M8.31,.5h3.38c1.27,0,2.31,1.03,2.31,2.31v.69H6v-.69c0-1.27,1.03-2.31,2.31-2.31Z"/><line class="e" x1="10" y1="7.5" x2="10" y2="19.5"/><line class="e" x1="6" y1="7.5" x2="6" y2="19.5"/><line class="e" x1="14" y1="7.5" x2="14" y2="19.5"/><path class="e" d="M14.15,23.5H5.85c-1.6,0-2.92-1.25-3-2.85L2,3.5H18l-.86,17.15c-.08,1.6-1.4,2.85-3,2.85Z"/></g></g></g></svg>  );
-};
