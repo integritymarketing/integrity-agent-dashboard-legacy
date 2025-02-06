ff2ef060d5c44ff54069ebb88593bf92628ced93
unstephenk <unstephenk@gmail.com>
2025-02-06 15:06:49 -0600

    preparation for sonarqube

--- setup.sh
+++ setup.sh
@@ -10,10 +10,21 @@
    echo "Cleaning up the current directory..."
    rm -rf ./* ./.??*
 
-   # Clone the repository using authenticated Git
-   echo "Cloning the repository using authenticated Git..."
-   GIT_REPO_URL="https://$GH_TOKEN@github.com/integritymarketing/ae-agent-portal.git" # Use the token from environment variables
-   GIT_BRANCH="SPW-286840-Integrate-with-SonarCloud" # Branch to clone
+   # Add SSH key for the deploy
+   echo "$SSH_KEY" > ~/.ssh/deploy_key
+   chmod 600 ~/.ssh/deploy_key
+   eval "$(ssh-agent -s)"
+   ssh-add ~/.ssh/deploy_key
+
+   # Prevent SSH host key verification issues
+   mkdir -p ~/.ssh
+   touch ~/.ssh/known_hosts
+   ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+
+   # Clone the repository using SSH
+   echo "Cloning the repository using SSH..."
+   GIT_REPO_URL="git@github.com:integritymarketing/ae-agent-portal.git"
+   GIT_BRANCH="SPW-286840-Integrate-with-SonarCloud"
 
    git clone --branch "$GIT_BRANCH" --single-branch "$GIT_REPO_URL" .
 
@@ -28,11 +39,9 @@
    # Debug Git state
    echo "Current Git commit: $(git rev-parse HEAD || echo 'No Git commit found')"
 
-   # Ensure Java installation for SonarQube is compatible
+   # Ensure Java installation for SonarQube
    mise use -g java@openjdk-21
-
-   # Verify Java installation
    java -version
 
-   # Run SonarQube analysis with debug mode
+   # Run SonarQube analysis
    yarn sonar -X
