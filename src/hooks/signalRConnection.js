4a9c7c699802ce9c4b9c399446b99352b7e5b232
unstephenk <unstephenk@gmail.com>
2025-02-10 14:04:13 -0600

    updating env sample

--- src/hooks/signalRConnection.js
+++ src/hooks/signalRConnection.js
@@ -1,15 +1,15 @@
-import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import {HubConnectionBuilder, LogLevel} from "@microsoft/signalr";
 
 export const getSignalRConnection = (profileAgentId) => {
-  if (window.connection) {
-    return window.connection;
-  }
-  const toggleURI = `${import.meta.env.VITE_AGENT_MANAGMENT_SIGNALR}/api/?agentid=${profileAgentId}`;
-  const connection = new HubConnectionBuilder()
-    .withUrl(toggleURI)
-    .configureLogging(LogLevel.Information)
-    .build();
-  connection.start().catch(console.error);
-  window.connection = connection;
-  return connection;
+    if (window.connection) {
+        return window.connection;
+    }
+    const toggleURI = `${import.meta.env.VITE_AGENT_MANAGEMENT_SIGNALR}/api/?agentid=${profileAgentId}`;
+    const connection = new HubConnectionBuilder()
+        .withUrl(toggleURI)
+        .configureLogging(LogLevel.Information)
+        .build();
+    connection.start().catch(console.error);
+    window.connection = connection;
+    return connection;
 };
