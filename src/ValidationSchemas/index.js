c34ec7ad493427f496d25af5e9eac8ec59de1bec
vinodkurra <ugandharkurra1996@gmail.com>
2025-02-12 20:37:41 +0530

    refactor: update ContactForm to use genericTextFieldSchema and improve formatting

--- src/ValidationSchemas/index.js
+++ src/ValidationSchemas/index.js
@@ -2,3 +2,4 @@ export * from "./birthDateSchema";
 export * from "./addressSchema";
 export * from "./medicareBeneficiaryIDSchema";
 export * from "./emailOrPhonePrimaryCommunication";
+export * from "./genericTextFieldSchema";
