aa3060999edcae0c58c850e3ad86e69268808619
vishalmarneni <79602980+vishalmarneni@users.noreply.github.com>
2025-02-12 14:09:19 -0600

    Update phoneSchema.js

--- src/ValidationSchemas/phoneSchema.js
+++ src/ValidationSchemas/phoneSchema.js
@@ -1,9 +1,14 @@
-import * as yup from 'yup';
-
-export const getPhoneSchema = () => (
-  yup.object({
-    phoneNumber: yup.string("Enter your phone number")
-      .required("Phone number is required")
-      .matches(/^[0-9]{10}$/, 'Phone number must be 10 digits')
-  })
-)
+import * as yup from "yup";
+ 
+export const getPhoneSchema = () =>
+    yup.object({
+        phone: yup
+            .string("Enter your phone number")
+            .required("Phone number is required")
+            .test(
+                "len",
+                "Phone number must be 10 digits",
+                (val) => val && val.replace(/\D/g, "").length === 10
+            )
+            .matches(/^\(\d{3}\) \d{3}-\d{4}$/, "Phone number is not valid"),
+    });
