33ba080400e17b9383d484aaed3ed47793282d73
rishithainturi <121522091+rishithainturi@users.noreply.github.com>
2025-03-05 20:14:52 +0530

    text change and logic update for expired conditions (#3169)

--- src/components/HealthConditionsPageContainer/HealthConditionContainer.constants.js
+++ src/components/HealthConditionsPageContainer/HealthConditionContainer.constants.js
@@ -2,7 +2,7 @@ import { QUOTES_API_VERSION } from "services/clientsService";
 export const HEALTH_CONDITION_API = `${import.meta.env.VITE_QUOTE_URL}/api/${QUOTES_API_VERSION}/HealthCondition/Lead/`;
 export const DISCLAIMER_TEXT =
     "Underwriting prescreening is not a determination of eligibility. Quote eligibility will be determined by the carrier at time of application.";
-export const CARD_TITLE = "Please add health conditions to prescreen for coverage eligibility.";
+export const CARD_TITLE = "Add your health conditions using your prescriptions or by searching directly for a condition for coverage eligibility.";
 export const ADD_NEW = "Add New";
 export const COMPLETED = "Completed";
 export const INCOMPLETE = "Incomplete";
@@ -13,8 +13,7 @@ export const CONDITIONS = "Conditions";
 export const CONTINUE_TO_QUOTE = "Continue to Quote";
 export const MEDICATIONS = "Medications";
 export const SIMPLIFIED_IUL_TITLE = "Simplified IUL";
-export const HEALTH_CONDITION_SEARCH_API = `${
-    import.meta.env.VITE_QUOTE_URL
-}/api/${QUOTES_API_VERSION}/Underwriting/healthcondition/search/HEALTH`;
+export const HEALTH_CONDITION_SEARCH_API = `${import.meta.env.VITE_QUOTE_URL
+    }/api/${QUOTES_API_VERSION}/Underwriting/healthcondition/search/HEALTH`;
 export const PRESCRIPTION_TOOLTIP =
     "The prescriptions listed were entered from a previous visit. Let us know what condition you take these medications for to get the most accurate quote.";
--- src/components/HealthConditionsPageContainer/HealthConditionsTable/ConditionListItem.jsx
+++ src/components/HealthConditionsPageContainer/HealthConditionsTable/ConditionListItem.jsx
@@ -1,81 +1,99 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Box, Button, Grid, Stack, Typography } from "@mui/material";
-import { ActionsRename } from "@integritymarketing/icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleXmark, faCircleCheck } from "@awesome.me/kit-7ab3488df1/icons/classic/light";
-import { faCheckIncomplete } from "@awesome.me/kit-7ab3488df1/icons/kit/custom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box, Button, Grid, Stack, Typography } from '@mui/material';
+import { ActionsRename } from '@integritymarketing/icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleXmark,
+  faCircleCheck,
+} from '@awesome.me/kit-7ab3488df1/icons/classic/light';
+import { faCheckIncomplete } from '@awesome.me/kit-7ab3488df1/icons/kit/custom';
 
-function ConditionListItem({ label, onEdit, areUwQuestionsComplete, areUwQuestionsExpired }) {
-    return (
-        <Grid
-            container
-            direction="row"
-            sx={{ padding: "16px 24px" }}
-            justifyContent="space-between"
-            alignItems="center"
-            alignSelf="stretch"
-            gap={2}
-        >
-            <Grid item>
-                <Typography variant="h4" sx={{ color: "#052A63" }}>
-                    {label}
-                </Typography>
-            </Grid>
+function ConditionListItem({
+  label,
+  onEdit,
+  areUwQuestionsComplete,
+  areUwQuestionsExpired,
+}) {
+  return (
+    <Grid
+      container
+      direction='row'
+      sx={{ padding: '16px 24px' }}
+      justifyContent='space-between'
+      alignItems='center'
+      alignSelf='stretch'
+      gap={2}
+    >
+      <Grid item>
+        <Typography variant='h4' sx={{ color: '#052A63' }}>
+          {label}
+        </Typography>
+      </Grid>
 
-            <Grid item>
-                <Stack direction="row" gap={3} alignItems="center">
-                    <Box display="flex" gap={1} minWidth={120}>
-                        {areUwQuestionsComplete ? (
-                            <FontAwesomeIcon icon={faCircleCheck} size="lg" color="#717171" />
-                        ) : areUwQuestionsExpired ? (
-                            <FontAwesomeIcon icon={faCircleXmark} size="lg" color="#C81E27" />
-                        ) : (
-                            <FontAwesomeIcon icon={faCheckIncomplete} size="lg" color="#4178FF" />
-                        )}
-                        <Typography
-                            sx={{
-                                color: areUwQuestionsExpired
-                                    ? "#C81E27"
-                                    : areUwQuestionsComplete
-                                    ? "#717171"
-                                    : "#4178FF",
-                            }}
-                        >
-                            {areUwQuestionsComplete ? "Complete" : areUwQuestionsExpired ? "Expired" : "Incomplete"}
-                        </Typography>
-                    </Box>
-                    <Button
-                        size="small"
-                        variant="link"
-                        sx={{
-                            padding: "5px 20px",
-                            borderRadius: "25px",
-                            color: "#6366F1",
-                            border: "none",
-                            "&:hover": {
-                                background: "#fff 0 0 no-repeat padding-box",
-                                borderRadius: "20px",
-                                boxShadow: "0 0 10px 1px rgb(0 0 0 / 20%)",
-                                color: "#1357ff",
-                            },
-                        }}
-                        endIcon={<ActionsRename />}
-                        onClick={onEdit}
-                    >
-                        Edit
-                    </Button>
-                </Stack>
-            </Grid>
-        </Grid>
-    );
+      <Grid item>
+        <Stack direction='row' gap={3} alignItems='center'>
+          <Box display='flex' gap={1} minWidth={120}>
+            {areUwQuestionsExpired ? (
+              <>
+                <FontAwesomeIcon
+                  icon={faCircleXmark}
+                  size='lg'
+                  color='#C81E27'
+                />
+                <Typography sx={{ color: '#C81E27' }}>Expired</Typography>
+              </>
+            ) : areUwQuestionsComplete ? (
+              <>
+                <FontAwesomeIcon
+                  icon={faCircleCheck}
+                  size='lg'
+                  color='#717171'
+                />
+                <Typography sx={{ color: '#717171' }}>Complete</Typography>
+              </>
+            ) : (
+              <>
+                <FontAwesomeIcon
+                  icon={faCheckIncomplete}
+                  size='lg'
+                  color='#4178FF'
+                />
+                <Typography sx={{ color: '#4178FF' }}>Incomplete</Typography>
+              </>
+            )}
+          </Box>
+          <Button
+            size='small'
+            variant='link'
+            sx={{
+              padding: '5px 20px',
+              borderRadius: '25px',
+              color: '#6366F1',
+              border: 'none',
+              '&:hover': {
+                background: '#fff 0 0 no-repeat padding-box',
+                borderRadius: '20px',
+                boxShadow: '0 0 10px 1px rgb(0 0 0 / 20%)',
+                color: '#1357ff',
+              },
+            }}
+            endIcon={<ActionsRename />}
+            onClick={onEdit}
+          >
+            Edit
+          </Button>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
 }
 
 ConditionListItem.propTypes = {
-    label: PropTypes.string.isRequired,
-    onEdit: PropTypes.func.isRequired,
-    areUwQuestionsComplete: PropTypes.bool,
-    areUwQuestionsExpired: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  onEdit: PropTypes.func.isRequired,
+  areUwQuestionsComplete: PropTypes.bool,
+  areUwQuestionsExpired: PropTypes.bool,
 };
 
 export default ConditionListItem;
