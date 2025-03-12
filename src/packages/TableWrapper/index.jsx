bf75c8ed0c208e88f9eb29a150caaac5825f9e5e
rishithainturi <121522091+rishithainturi@users.noreply.github.com>
2025-03-12 07:24:06 -0500

    added line between conditions (#3196)

--- src/packages/TableWrapper/index.jsx
+++ src/packages/TableWrapper/index.jsx
@@ -1,135 +1,160 @@
-import React from "react";
-import PropTypes from "prop-types";
-import MUITable from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableFooter from "@mui/material/TableFooter";
-import TableRow from "@mui/material/TableRow";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import { styled } from "@mui/system";
-import { useReactTable, getCoreRowModel, flexRender, getSortedRowModel } from "@tanstack/react-table";
-import SortArrow from "components/icons/version-2/SortArrow";
+import React from 'react';
+import PropTypes from 'prop-types';
+import MUITable from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableFooter from '@mui/material/TableFooter';
+import TableRow from '@mui/material/TableRow';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import { styled } from '@mui/system';
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+  getSortedRowModel,
+} from '@tanstack/react-table';
+import SortArrow from 'components/icons/version-2/SortArrow';
 
-const Centered = styled("div")`
-    display: flex;
-    gap: 5px;
-    align-items: center;
+const Centered = styled('div')`
+  display: flex;
+  gap: 5px;
+  align-items: center;
 `;
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-        color: "#434A51",
-        cursor: "pointer",
-        fontFamily: "Lato",
-        fontSize: "16px",
-        lineHeight: "20px",
-        borderBottom: "none",
-    },
-    [`&.${tableCellClasses.body}`]: {
-        fontFamily: "Lato",
-        fontSize: "16px",
-        lineHeight: "20px",
-        maxWidth: 250,
-        color: "#434A51",
-        padding: "20px 10px",
-    },
+  [`&.${tableCellClasses.head}`]: {
+    color: '#434A51',
+    cursor: 'pointer',
+    fontFamily: 'Lato',
+    fontSize: '16px',
+    lineHeight: '20px',
+    borderBottom: 'none',
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontFamily: 'Lato',
+    fontSize: '16px',
+    lineHeight: '20px',
+    maxWidth: 250,
+    color: '#434A51',
+    padding: '20px 10px',
+  },
 }));
 
 const StyledTableBody = styled(TableBody)(() => ({
-    backgroundColor: "#FFFFFF",
+  backgroundColor: '#FFFFFF',
 }));
 
 const SMUITable = styled(MUITable)(() => ({
-    ".MuiTableBody-root tr:first-of-type": {
-        borderTopLeftRadius: "10px",
-        borderTopRightRadius: "10px",
-    },
-    ".MuiTableBody-root tr:last-child": {
-        borderBottomLeftRadius: "10px",
-        borderBottomRightRadius: "10px",
-    },
-    ".MuiTableCell-root": {
-        borderBottom: "0px solid #E0E0E0",
-    },
+  '.MuiTableBody-root tr:first-of-type': {
+    borderTopLeftRadius: '10px',
+    borderTopRightRadius: '10px',
+  },
+  '.MuiTableBody-root tr:last-child': {
+    borderBottomLeftRadius: '10px',
+    borderBottomRightRadius: '10px',
+  },
+  '.MuiTableCell-root': {
+    borderBottom: '1px solid #cdd0d5',
+  },
 }));
 
-function Table({ columns, data, footer, initialState, fixedRows = [], overflowHide = false, handleSort }) {
-    const table = useReactTable({
-        columns,
-        data,
-        initialState,
-        getCoreRowModel: getCoreRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-    });
+function Table({
+  columns,
+  data,
+  footer,
+  initialState,
+  fixedRows = [],
+  overflowHide = false,
+  handleSort,
+}) {
+  const table = useReactTable({
+    columns,
+    data,
+    initialState,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+  });
 
-    return (
-        <TableContainer sx={{ width: "100%", borderRadius: "8px", overflowX: overflowHide ? "hidden" : "auto" }}>
-            <SMUITable>
-                <TableHead>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <StyledTableCell
-                                        key={header.id}
-                                        onClick={() => handleSort(header.column.columnDef?.header)}
-                                    >
-                                        <Centered>
-                                            {flexRender(header.column.columnDef?.header, header.getContext())}
-                                            {header.column.columnDef?.enableSorting && (
-                                                <span
-                                                    style={{
-                                                        marginLeft: "8px",
-                                                        marginTop: "4px",
-                                                        cursor: "pointer",
-                                                    }}
-                                                >
-                                                    <SortArrow />
-                                                </span>
-                                            )}
-                                        </Centered>
-                                    </StyledTableCell>
-                                );
-                            })}
-                        </TableRow>
-                    ))}
-                </TableHead>
-                <StyledTableBody>
-                    {fixedRows.map((fixedRow, idx) => (
-                        <TableRow key={`fixed-${idx}`}>{fixedRow}</TableRow>
-                    ))}
-                    {table.getRowModel().rows.map((row) => (
-                        <TableRow key={row.id}>
-                            {row.getVisibleCells().map((cell) => (
-                                <StyledTableCell key={cell.id}>
-                                    {flexRender(cell.column.columnDef?.cell ?? "", cell.getContext())}
-                                </StyledTableCell>
-                            ))}
-                        </TableRow>
-                    ))}
-                </StyledTableBody>
-                {footer && (
-                    <TableFooter>
-                        <TableRow>
-                            <TableCell style={{ border: "none" }} colSpan={columns.length}>
-                                <center>{footer}</center>
-                            </TableCell>
-                        </TableRow>
-                    </TableFooter>
-                )}
-            </SMUITable>
-        </TableContainer>
-    );
+  return (
+    <TableContainer
+      sx={{
+        width: '100%',
+        borderRadius: '8px',
+        overflowX: overflowHide ? 'hidden' : 'auto',
+      }}
+    >
+      <SMUITable>
+        <TableHead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map(header => {
+                return (
+                  <StyledTableCell
+                    key={header.id}
+                    onClick={() => handleSort(header.column.columnDef?.header)}
+                  >
+                    <Centered>
+                      {flexRender(
+                        header.column.columnDef?.header,
+                        header.getContext()
+                      )}
+                      {header.column.columnDef?.enableSorting && (
+                        <span
+                          style={{
+                            marginLeft: '8px',
+                            marginTop: '4px',
+                            cursor: 'pointer',
+                          }}
+                        >
+                          <SortArrow />
+                        </span>
+                      )}
+                    </Centered>
+                  </StyledTableCell>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHead>
+        <StyledTableBody>
+          {fixedRows.map((fixedRow, idx) => (
+            <TableRow key={`fixed-${idx}`}>{fixedRow}</TableRow>
+          ))}
+          {table.getRowModel().rows.map(row => (
+            <TableRow key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <StyledTableCell key={cell.id}>
+                  {flexRender(
+                    cell.column.columnDef?.cell ?? '',
+                    cell.getContext()
+                  )}
+                </StyledTableCell>
+              ))}
+            </TableRow>
+          ))}
+        </StyledTableBody>
+        {footer && (
+          <TableFooter>
+            <TableRow>
+              <TableCell style={{ border: 'none' }} colSpan={columns.length}>
+                <center>{footer}</center>
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        )}
+      </SMUITable>
+    </TableContainer>
+  );
 }
 
 Table.propTypes = {
-    columns: PropTypes.array.isRequired, // Column configuration for the table
-    data: PropTypes.array.isRequired, // Table data
-    footer: PropTypes.node, // Footer content
-    initialState: PropTypes.object, // Initial table state
-    fixedRows: PropTypes.array, // Predefined fixed rows at the top
-    overflowHide: PropTypes.bool, // Whether to hide horizontal overflow
+  columns: PropTypes.array.isRequired, // Column configuration for the table
+  data: PropTypes.array.isRequired, // Table data
+  footer: PropTypes.node, // Footer content
+  initialState: PropTypes.object, // Initial table state
+  fixedRows: PropTypes.array, // Predefined fixed rows at the top
+  overflowHide: PropTypes.bool, // Whether to hide horizontal overflow
 };
 
 export default Table;
