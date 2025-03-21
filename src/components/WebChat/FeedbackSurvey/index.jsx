5c566c954feefefd14bf44215482402d90ab7edc
samrat <samrat.lekkala@integrity.com>
2025-03-21 14:01:00 -0500

    Fix : Added loader styling and Modified FeedbackSurvey component structure

--- src/components/WebChat/FeedbackSurvey/index.jsx
+++ src/components/WebChat/FeedbackSurvey/index.jsx
@@ -85,134 +85,136 @@ const FeedbackSurveyCard = ({
         showFeedbackSurvey ? styles.visible : ''
       }`}
     >
-      <Box className={styles.headerSection}>
-        <img src={logoImage} alt='Feedback Logo' className={styles.logo} />
-        <button className={styles.closeButtonContainer} onClick={onClose}>
-          <FontAwesomeIcon icon={faXmark} size='2xs' color='#fff' />
-        </button>
-      </Box>
-
       {isFeedbackSubmitLoading ? (
-        <div className='loaderContainer'>
+        <div className={styles.loaderContainer}>
           <Spinner />
         </div>
       ) : (
         <>
-          <Box className={styles.surveyIntroContainer}>
-            <p className={styles.surveyTitle}>
-              Ask Integrity™ is still learning - we value your feedback!
-            </p>
-            <p className={styles.surveyInstruction}>
-              Please select any issues that apply to your experience:
-            </p>
+          <Box className={styles.headerSection}>
+            <img src={logoImage} alt='Feedback Logo' className={styles.logo} />
+            <button className={styles.closeButtonContainer} onClick={onClose}>
+              <FontAwesomeIcon icon={faXmark} size='2xs' color='#fff' />
+            </button>
           </Box>
+          <>
+            <Box className={styles.surveyIntroContainer}>
+              <p className={styles.surveyTitle}>
+                Ask Integrity™ is still learning - we value your feedback!
+              </p>
+              <p className={styles.surveyInstruction}>
+                Please select any issues that apply to your experience:
+              </p>
+            </Box>
 
-          <Box className={styles.optionsContainer}>
-            <Box className={styles.options}>
-              <FormGroup className={styles.optionsGroup}>
-                {EXPERIENCE_OPTIONS.map((option, index) => (
-                  <React.Fragment key={option}>
-                    <Box className={styles.checkDivider}>
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={
-                              feedback.selectedOptions[`option${index + 1}`] ||
-                              false
-                            }
-                            onChange={handleCheckboxChange}
-                            name={`option${index + 1}`}
-                            sx={{
-                              color: 'white',
-                              '&.Mui-checked': { color: 'white' },
-                            }}
-                          />
-                        }
-                        label={option}
-                      />
-                    </Box>
-                    <Box className={styles.optionDivider} />
-                  </React.Fragment>
-                ))}
-              </FormGroup>
+            <Box className={styles.optionsContainer}>
+              <Box className={styles.options}>
+                <FormGroup className={styles.optionsGroup}>
+                  {EXPERIENCE_OPTIONS.map((option, index) => (
+                    <React.Fragment key={option}>
+                      <Box className={styles.checkDivider}>
+                        <FormControlLabel
+                          control={
+                            <Checkbox
+                              checked={
+                                feedback.selectedOptions[
+                                  `option${index + 1}`
+                                ] || false
+                              }
+                              onChange={handleCheckboxChange}
+                              name={`option${index + 1}`}
+                              sx={{
+                                color: 'white',
+                                '&.Mui-checked': { color: 'white' },
+                              }}
+                            />
+                          }
+                          label={option}
+                        />
+                      </Box>
+                      <Box className={styles.optionDivider} />
+                    </React.Fragment>
+                  ))}
+                </FormGroup>
 
-              <Box className={styles.checkDivider}>
-                <Box className={styles.additionalFeedback}>
-                  <h4>Additional Feedback</h4>
-                  <TextField
-                    multiline
-                    rows={2}
-                    fullWidth
-                    variant='outlined'
-                    placeholder='Tell us what we can improve'
-                    value={feedback.additionalComment}
-                    onChange={handleTextChange}
-                    InputProps={{ style: { color: 'white' } }}
-                    sx={{
-                      '& .MuiOutlinedInput-root': {
-                        paddingTop: '0.5rem !important',
-                        backgroundColor: '#1D3E71',
-                        width: 'calc(100% - 16px)',
-                        '& fieldset': {
-                          borderColor: 'rgba(113, 136, 168, 0.7)',
+                <Box className={styles.checkDivider}>
+                  <Box className={styles.additionalFeedback}>
+                    <h4>Additional Feedback</h4>
+                    <TextField
+                      multiline
+                      rows={2}
+                      fullWidth
+                      variant='outlined'
+                      placeholder='Tell us what we can improve'
+                      value={feedback.additionalComment}
+                      onChange={handleTextChange}
+                      InputProps={{ style: { color: 'white' } }}
+                      sx={{
+                        '& .MuiOutlinedInput-root': {
+                          paddingTop: '0.5rem !important',
+                          backgroundColor: '#1D3E71',
+                          width: 'calc(100% - 16px)',
+                          '& fieldset': {
+                            borderColor: 'rgba(113, 136, 168, 0.7)',
+                          },
+                          '&:hover fieldset': {
+                            borderColor: 'rgba(113, 136, 168, 0.7)',
+                          },
+                          '& .MuiOutlinedInput-input': {
+                            alignItems: 'flex-start',
+                            verticalAlign: 'top',
+                          },
                         },
-                        '&:hover fieldset': {
-                          borderColor: 'rgba(113, 136, 168, 0.7)',
-                        },
-                        '& .MuiOutlinedInput-input': {
-                          alignItems: 'flex-start',
+                        '& .MuiInputBase-input::placeholder': {
+                          color: 'rgb(255, 255, 255)',
+                          opacity: 1,
+                          fontStyle: 'italic',
+                          lineHeight: '24px',
+                          letterSpacing: '0.15px',
+                          textAlign: 'left',
                           verticalAlign: 'top',
                         },
-                      },
-                      '& .MuiInputBase-input::placeholder': {
-                        color: 'rgb(255, 255, 255)',
-                        opacity: 1,
-                        fontStyle: 'italic',
-                        lineHeight: '24px',
-                        letterSpacing: '0.15px',
-                        textAlign: 'left',
-                        verticalAlign: 'top',
-                      },
-                      width: '92%',
-                      marginBottom: '1rem',
-                      marginTop: '0.5rem',
-                    }}
-                  />
+                        width: '92%',
+                        marginBottom: '1rem',
+                        marginTop: '0.5rem',
+                      }}
+                    />
+                  </Box>
                 </Box>
               </Box>
             </Box>
-          </Box>
 
-          <Box className={styles.buttonContainer}>
-            <Box className={styles.buttonGroup}>
-              <Button
-                variant='text'
-                onClick={onClose}
-                sx={{
-                  color: 'white',
-                  textTransform: 'none',
-                  '&:hover': { background: 'none' },
-                }}
-              >
-                Skip
-              </Button>
-              <Button
-                variant='contained'
-                color='primary'
-                onClick={handleSubmitFeedback}
-                endIcon={
-                  <FontAwesomeIcon
-                    icon={faCircleArrowRight}
-                    color='#fff'
-                    size='lg'
-                  />
-                }
-                disabled={isSubmitDisabled}
-              >
-                Submit
-              </Button>
+            <Box className={styles.buttonContainer}>
+              <Box className={styles.buttonGroup}>
+                <Button
+                  variant='text'
+                  onClick={onClose}
+                  sx={{
+                    color: 'white',
+                    textTransform: 'none',
+                    '&:hover': { background: 'none' },
+                  }}
+                >
+                  Skip
+                </Button>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  onClick={handleSubmitFeedback}
+                  endIcon={
+                    <FontAwesomeIcon
+                      icon={faCircleArrowRight}
+                      color='#fff'
+                      size='lg'
+                    />
+                  }
+                  disabled={isSubmitDisabled}
+                >
+                  Submit
+                </Button>
+              </Box>
             </Box>
-          </Box>
+          </>
         </>
       )}
     </Box>
