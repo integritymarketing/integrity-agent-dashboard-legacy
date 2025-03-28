97fd6c42a0df09eb74ce5d2a28ba83092eb0f51d
samrat68 <samrat.lekkala@integrity.com>
2025-03-28 16:56:57 -0500

    refactor: remove AEP_PlanReviewBanner component and related styles (#3283)
    * refactor: remove AEP_PlanReviewBanner component and related styles
    
    * feat: add AEP_PlanReviewBanner component with styles for Medicare plan reviews
    
    * feat: add AEP_PlanReviewBanner component for Medicare plan reviews

--- src/components/ContactDetailsContainer/PoliciesContainer/PoliciesContainer.jsx
+++ src/components/ContactDetailsContainer/PoliciesContainer/PoliciesContainer.jsx
@@ -1,17 +1,15 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import { useParams } from "react-router-dom";
-import EnrollmentHistoryContainer from "../../EnrollmentHistoryContainer/EnrollmentHistoryContainer";
-import AEP_PlanReviewBanner from "./AEP_PlanReviewBanner";
-import styles from "./PoliciesContainer.module.scss";
+import React from 'react';
+import Box from '@mui/material/Box';
+import { useParams } from 'react-router-dom';
+import EnrollmentHistoryContainer from '../../EnrollmentHistoryContainer/EnrollmentHistoryContainer';
+import styles from './PoliciesContainer.module.scss';
 
 export const PoliciesContainer = () => {
-    const { leadId } = useParams();
+  const { leadId } = useParams();
 
-    return (
-        <Box className={styles.policiesContainer}>
-            <AEP_PlanReviewBanner />
-            <EnrollmentHistoryContainer leadId={leadId} />
-        </Box>
-    );
+  return (
+    <Box className={styles.policiesContainer}>
+      <EnrollmentHistoryContainer leadId={leadId} />
+    </Box>
+  );
 };
