0844a68f2f0519a9cc9bafe4345de134ee1edd7e
samrat68 <samrat.lekkala@integrity.com>
2025-04-03 07:56:19 -0500

    feat: enhance call recordings fetching with optional call log ID (#3305)
    * feat: enhance call recordings fetching with optional call log ID
    
    * feat: update CallRecordingsService to support fetching by call ID and improve code formatting

--- src/hooks/useCallRecordings.jsx
+++ src/hooks/useCallRecordings.jsx
@@ -1,65 +1,72 @@
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import * as Sentry from "@sentry/react";
-import { useClientServiceContext } from "services/clientServiceProvider";
-import { getSignalRConnection } from "hooks/signalRConnection";
-import useUserProfile from "hooks/useUserProfile";
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { useClientServiceContext } from 'services/clientServiceProvider';
+import { getSignalRConnection } from 'hooks/signalRConnection';
+import useUserProfile from 'hooks/useUserProfile';
 
 export default function useCallRecordings() {
-    const { callRecordingsService } = useClientServiceContext();
-    const [callRecordings, setCallRecordings] = useState([]);
-    const { agentId } = useUserProfile();
-    const [fetchTimeoutId, setFetchTimeoutId] = useState(null);
-    const location = useLocation();
+  const { callRecordingsService } = useClientServiceContext();
+  const [callRecordings, setCallRecordings] = useState([]);
+  const { agentId } = useUserProfile();
+  const [fetchTimeoutId, setFetchTimeoutId] = useState(null);
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
 
-    const isLinkToContactPath = () => {
-        return location.pathname.includes("/link-to-contact");
-    };
+  const callLogId = queryParams.get('id');
 
-    async function fetchCallRecordings() {
-        try {
-            const isLinkToContact = isLinkToContactPath();
-            const recordings = await callRecordingsService.getAllCallRecordingsByAgent(isLinkToContact);
+  const isLinkToContactPath = () => {
+    return location.pathname.includes('/link-to-contact');
+  };
 
-            if (recordings.length > 0) {
-                const sortedRecordings = recordings.sort(
-                    (a, b) => new Date(b.callStartTime) - new Date(a.callStartTime)
-                );
-                setCallRecordings(sortedRecordings);
-            } else {
-                setCallRecordings([]);
-            }
-        } catch (error) {
-            Sentry.captureException(error);
-        }
+  async function fetchCallRecordings() {
+    try {
+      const isLinkToContact = isLinkToContactPath();
+      const recordings =
+        await callRecordingsService.getAllCallRecordingsByAgent(
+          isLinkToContact,
+          callLogId
+        );
+
+      if (recordings.length > 0) {
+        const sortedRecordings = recordings.sort(
+          (a, b) => new Date(b.callStartTime) - new Date(a.callStartTime)
+        );
+        setCallRecordings(sortedRecordings);
+      } else {
+        setCallRecordings([]);
+      }
+    } catch (error) {
+      Sentry.captureException(error);
+    }
+  }
+
+  useEffect(() => {
+    function handleActiveCall(status) {
+      if (status) {
+        setFetchTimeoutId(setTimeout(fetchCallRecordings, 5000));
+      } else {
+        setFetchTimeoutId(setTimeout(fetchCallRecordings, 15000));
+      }
     }
 
-    useEffect(() => {
-        function handleActiveCall(status) {
-            if (status) {
-                setFetchTimeoutId(setTimeout(fetchCallRecordings, 5000));
-            } else {
-                setFetchTimeoutId(setTimeout(fetchCallRecordings, 15000));
-            }
-        }
+    if (agentId) {
+      const connection = getSignalRConnection(agentId);
 
-        if (agentId) {
-            const connection = getSignalRConnection(agentId);
+      connection.on('ActiveCall', handleActiveCall);
 
-            connection.on("ActiveCall", handleActiveCall);
+      return () => {
+        connection.off('ActiveCall', handleActiveCall);
+        clearTimeout(fetchTimeoutId);
+      };
+    }
+  }, [agentId, fetchTimeoutId]);
 
-            return () => {
-                connection.off("ActiveCall", handleActiveCall);
-                clearTimeout(fetchTimeoutId);
-            };
-        }
-    }, [agentId, fetchTimeoutId]);
+  useEffect(() => {
+    if (agentId) {
+      fetchCallRecordings();
+    }
+  }, [agentId]);
 
-    useEffect(() => {
-        if (agentId) {
-            fetchCallRecordings();
-        }
-    }, [agentId]);
-
-    return callRecordings;
+  return callRecordings;
 }
--- src/services/callRecordingsService.js
+++ src/services/callRecordingsService.js
@@ -1,71 +1,73 @@
-export const LEADS_API_VERSION = "v2.0";
+export const LEADS_API_VERSION = 'v2.0';
 
 export class CallRecordingsService {
-    constructor(getAccessToken) {
-        this.getAccessToken = getAccessToken;
+  constructor(getAccessToken) {
+    this.getAccessToken = getAccessToken;
+  }
+
+  getAllCallRecordingsByAgent = async (isLinkToContact = false, callId) => {
+    const baseUrl = `${import.meta.env.VITE_COMMUNICATION_API}/Call/Records`;
+    const url = isLinkToContact
+      ? `${baseUrl}/${callId}`
+      : `${baseUrl}?CallStatus=in-progress`;
+
+    const response = await this._clientAPIRequest(url);
+    const data = await response.json();
+    return data;
+  };
+
+  outboundCallFromMedicareCenter = async data => {
+    const response = await this._clientAPIRequest(
+      `${import.meta.env.VITE_COMMUNICATION_API}/Call/CallCustomer`,
+      'POST',
+      data
+    );
+    if (response.ok) {
+      return response;
+    }
+    throw new Error('Update failed.');
+  };
+
+  assignsLeadToInboundCallRecord = async data => {
+    const response = await this._clientAPIRequest(
+      `${import.meta.env.VITE_COMMUNICATION_API}/Call/Records/AssignToLead`,
+      'PUT',
+      data
+    );
+
+    if (response.ok) {
+      return response;
+    }
+    throw new Error(response?.statusText);
+  };
+
+  assignsLeadToOutboundSmsRecord = async data => {
+    const response = await this._clientAPIRequest(
+      `${import.meta.env.VITE_COMMUNICATION_API}/SmsLog/Records/AssignToLead`,
+      'PUT',
+      data
+    );
+
+    if (response.ok) {
+      return response;
+    }
+    throw new Error(response?.statusText);
+  };
+
+  _clientAPIRequest = async (url, method = 'GET', body) => {
+    const accessToken = await this.getAccessToken();
+    const opts = {
+      method,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+    };
+
+    if (body) {
+      opts.body = JSON.stringify(body);
     }
 
-    getAllCallRecordingsByAgent = async (isLinkToContact = false) => {
-        const baseUrl = `${import.meta.env.VITE_COMMUNICATION_API}/Call/Records?`;
-        const url = isLinkToContact ? `${baseUrl}Sort=callStartTime:desc` : `${baseUrl}CallStatus=in-progress`;
-
-        const response = await this._clientAPIRequest(url);
-        const data = await response.json();
-        return data;
-    };
-
-    outboundCallFromMedicareCenter = async (data) => {
-        const response = await this._clientAPIRequest(
-            `${import.meta.env.VITE_COMMUNICATION_API}/Call/CallCustomer`,
-            "POST",
-            data
-        );
-        if (response.ok) {
-            return response;
-        }
-        throw new Error("Update failed.");
-    };
-
-    assignsLeadToInboundCallRecord = async (data) => {
-        const response = await this._clientAPIRequest(
-            `${import.meta.env.VITE_COMMUNICATION_API}/Call/Records/AssignToLead`,
-            "PUT",
-            data
-        );
-
-        if (response.ok) {
-            return response;
-        }
-        throw new Error(response?.statusText);
-    };
-
-    assignsLeadToOutboundSmsRecord = async (data) => {
-        const response = await this._clientAPIRequest(
-            `${import.meta.env.VITE_COMMUNICATION_API}/SmsLog/Records/AssignToLead`,
-            "PUT",
-            data
-        );
-
-        if (response.ok) {
-            return response;
-        }
-        throw new Error(response?.statusText);
-    };
-
-    _clientAPIRequest = async (url, method = "GET", body) => {
-        const accessToken = await this.getAccessToken();
-        const opts = {
-            method,
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-                "Content-Type": "application/json",
-            },
-        };
-
-        if (body) {
-            opts.body = JSON.stringify(body);
-        }
-
-        return fetch(url.toString(), opts);
-    };
+    return fetch(url.toString(), opts);
+  };
 }
