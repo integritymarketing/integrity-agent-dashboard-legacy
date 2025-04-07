e572510d354226fb8df9f7e07e2d5296f5aca3d1
rishithainturi <121522091+rishithainturi@users.noreply.github.com>
2025-04-07 14:15:52 -0500

    Change name to Fully undrwritten IUL (#3343)

--- src/components/CreateNewQuoteContainer/QuickQuoteModals/LifeQuestionCard/constants.js
+++ src/components/CreateNewQuoteContainer/QuickQuoteModals/LifeQuestionCard/constants.js
@@ -1,6 +1,6 @@
 export const LIFE_QUESTION_CARD_LIST = {
   FINAL_EXPENSE: 'Final Expense',
-  INDEXED_UNIVERSAL_LIFE: 'Fully IUL',
+  INDEXED_UNIVERSAL_LIFE: 'Fully Underwritten IUL',
   SIMPLIFIED_INDEXED_UNIVERSAL_LIFE: 'Simplified IUL',
   TERM_LIFE: 'Term Life',
   GUARANTEED_UL: 'Guaranteed UL',
