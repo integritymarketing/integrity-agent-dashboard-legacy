8aaf975d8c081f0e4a1d1f4fda86d1d54500f07a
VenuDamacherla <venu.damacherla@integrity.com>
2025-04-08 13:42:10 -0500

    fix: reorder life insurance product constants for consistency

--- src/components/CreateNewQuoteContainer/QuickQuoteModals/LifeQuestionCard/constants.js
+++ src/components/CreateNewQuoteContainer/QuickQuoteModals/LifeQuestionCard/constants.js
@@ -2,7 +2,7 @@ export const LIFE_QUESTION_CARD_LIST = {
   FINAL_EXPENSE: 'Final Expense',
   INDEXED_UNIVERSAL_LIFE: 'Fully Underwritten IUL',
   SIMPLIFIED_INDEXED_UNIVERSAL_LIFE: 'Simplified IUL',
-  TERM_LIFE: 'Term Life',
-  GUARANTEED_UL: 'Guaranteed UL',
   ANNUITIES: 'Annuities',
+  GUARANTEED_UL: 'Guaranteed UL',
+  TERM_LIFE: 'Term Life',
 };
