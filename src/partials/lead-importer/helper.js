099588a01284940071f69ce8992a657ade1b1133
Stephen Kuehl <unstephenk@gmail.com>
2025-04-16 14:08:30 -0500

    SPW-287036-Client-import-is-failing-when-a-lead-ONLY-has-a-phone-number (#3401)

--- src/partials/lead-importer/helper.js
+++ src/partials/lead-importer/helper.js
@@ -1,22 +1,21 @@
 import moment from "moment";
 
-import { formatPhoneNumber } from "utils/phones";
+import {formatPhoneNumber} from "utils/phones";
 
-export const getFormattedData = ({ phone, isTobaccoUser, height, weight, partB, partA, birthdate, ...data }) => {
-    const formattedData = {
-        ...data,
-        phone: String(formatPhoneNumber(phone)).replace(/\D/g, ""),
-        ...(isTobaccoUser !== null && isTobaccoUser !== undefined
-            ? { isTobaccoUser: isTobaccoUser.toLowerCase() === "yes" }
-            : {}),
-        ...(height !== null && height !== undefined ? { height: Number(height) } : {}),
-        ...(weight !== null && weight !== undefined ? { weight: Number(weight) } : {}),
-        ...(birthdate !== null && birthdate !== undefined
-            ? { birthdate: moment(birthdate, "MM/DD/YYYY").toDate() }
-            : {}),
-        ...(partA !== null && partA !== undefined ? { partA: moment(partA, "MM/DD/YYYY").toDate() } : {}),
-        ...(partB !== null && partB !== undefined ? { partB: moment(partB, "MM/DD/YYYY").toDate() } : {}),
-    };
-
-    return formattedData;
+export const getFormattedData = ({phone, isTobaccoUser, height, weight, partB, partA, birthdate, email, ...data}) => {
+  return {
+    ...data,
+    phone: phone === "" ? null : String(formatPhoneNumber(phone)).replace(/\D/g, ""),
+    email: email === "" ? null : email,
+    ...(isTobaccoUser !== null && isTobaccoUser !== undefined
+      ? {isTobaccoUser: isTobaccoUser.toLowerCase() === "yes"}
+      : {}),
+    ...(height !== null && height !== undefined ? {height: Number(height)} : {}),
+    ...(weight !== null && weight !== undefined ? {weight: Number(weight)} : {}),
+    ...(birthdate !== null && birthdate !== undefined
+      ? {birthdate: moment(birthdate, "MM/DD/YYYY").toDate()}
+      : {}),
+    ...(partA !== null && partA !== undefined ? {partA: moment(partA, "MM/DD/YYYY").toDate()} : {}),
+    ...(partB !== null && partB !== undefined ? {partB: moment(partB, "MM/DD/YYYY").toDate()} : {}),
+  };
 };
