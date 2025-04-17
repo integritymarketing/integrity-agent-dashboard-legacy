06ba5e345625f26577fab75cd60df4e5a208f27b
samrat68 <samrat.lekkala@integrity.com>
2025-04-17 17:22:42 -0500

    Feat : Added Page Not Found error message (#3404)

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,52 +1,27 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import Container from "components/ui/container";
-import GlobalNav from "partials/global-nav-v2";
-import SimpleFooter from "partials/simple-footer";
-import Modal from "components/ui/modal";
-import ContactInfo from "partials/contact-info";
-
-const useHelpLinkWithModal = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  return [
-    (props) => (
-      <button
-        className="link link--inherit link--body"
-        type="button"
-        onClick={() => setModalOpen(true)}
-        {...props}
-      ></button>
-    ),
-    () => (
-      <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
-        <ContactInfo />
-      </Modal>
-    ),
-  ];
-};
+import React from 'react';
+import { HeaderUnAuthenticated } from 'components/HeaderUnAuthenticated';
+import { FooterUnAuthenticated } from 'components/FooterUnAuthenticated';
+import { NoResultsError } from '@integritymarketing/clients-ui-kit';
+import { GenericError } from 'components/icons/errorImages';
+import { ContainerUnAuthenticated } from 'components/ContainerUnAuthenticated';
 
 const NotFound = () => {
-  const [HelpLink, HelpModal] = useHelpLinkWithModal();
-  const navigate = useNavigate();
+  const errorData = {
+    title: 'Page Not Found',
+    subtitle: 'The requested source could not be found',
+    image: <GenericError />,
+    buttonText: 'Go Back',
+    onButtonClick: () => {
+      window.history.back();
+    },
+  };
   return (
-    <div className="content-frame bg-neutral-gradient text-invert">
-      <GlobalNav menuHidden={true} className="mb-auto" />
-      <Container id="main-content">
-        <div className="hdg hdg--2">404</div>
-        <div className="hdg hdg--2 mb-4">Page not found.</div>
-        <p className="text-body text-body--large mb-4">
-          We weren’t able to find the page you are looking for. <br />
-          Please check the URL or <HelpLink>contact support</HelpLink> for
-          further assistance.
-        </p>
-        <div>
-          <button className="btn btn--invert" onClick={() => navigate(-1)}>
-            Go Back
-          </button>
-        </div>
-        <HelpModal />
-      </Container>
-      <SimpleFooter className="global-footer--simple" />
+    <div className='content-frame v2'>
+      <HeaderUnAuthenticated />
+      <ContainerUnAuthenticated>
+        <NoResultsError {...errorData} />
+      </ContainerUnAuthenticated>
+      <FooterUnAuthenticated />
     </div>
   );
 };
